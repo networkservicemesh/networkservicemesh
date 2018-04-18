da7d9887a3010bd4f8e45f6ef43978f7a731980f
Kyle Mestery <mestery@mestery.com>
2018-04-18 11:25:39 -0500

    Diagnose travis ci problem
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -3,4 +3,9 @@ set -ex
 pushd ~/
 wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
 unzip protoc-3.5.1-linux-x86_64.zip
+
+echo "GOPATH=${GOPATH}"
+echo "GOBIN=${GOBIN}"
+ls ${GOPATH}
+ls ${GOBIN}
 popd
