1c96e5f42bffc7feb664a2936e305b971007cf87
Kyle Mestery <mestery@mestery.com>
2018-04-18 11:14:49 -0500

    Use the linux version of protoc

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 pushd ~/
-wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
-unzip protoc-3.5.1-osx-x86_64.zip
+wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+unzip protoc-3.5.1-linux-x86_64.zip
 popd
