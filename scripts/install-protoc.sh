402f2d25f386c43d25b0ec9c60d3cbcd826f1e2e
Kyle Mestery <mestery@mestery.com>
2018-04-18 12:54:14 -0500

    Cleanup debug in the install-protoc.sh script
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -3,11 +3,4 @@ set -ex
 pushd ~/
 wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
 unzip protoc-3.5.1-linux-x86_64.zip
-
-echo "GOPATH=${GOPATH}"
-echo "GOBIN=${GOBIN}"
-ls ${GOPATH}
-ls ${GOBIN}
-echo "Checking ${GOPATH}/bin"
-ls ${GOPATH}/bin
 popd
