c159b37adc5539bc51a13e0d8f1fbfc60d96b069
Kyle Mestery <mestery@mestery.com>
2018-04-24 16:04:53 -0700

    Move go fmt into the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 # This script builds the networkservicemesh
 #
 
+test -z $(go fmt ./...) || (echo "Run go fmt ./... and recommit your code";exit 1)
 go get -u github.com/golang/protobuf/protoc-gen-go
 go generate ./...
 go install ./...
