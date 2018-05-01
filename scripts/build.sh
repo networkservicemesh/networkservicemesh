88ffdf40959e27a4ae304c43ffe3707d06bb3a03
Frederick F. Kautz IV <fkautz@redhat.com>
2018-05-01 12:07:45 -0700

    Fixing test path for recursive go dep
    Signed-off-by: Frederick F. Kautz IV <fkautz@redhat.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 # This script builds the networkservicemesh
 #
 
+set -e
+
 export RACE_ENABLED="-race"
 while test $# -gt 0; do
 	case "$1" in
@@ -16,7 +18,7 @@ while test $# -gt 0; do
 	shift
 done
 
-[ -d vendor/ligato/networkservicemesh/ ] && (echo "Run: rm -rf vendor/ligato/networkservicemesh;dep ensure";exit 1)
+[ -d vendor/github.com/ligato/networkservicemesh/ ] && (echo "Run: rm -rf vendor/github.com/ligato/networkservicemesh;dep ensure";exit 1)
 test -z $(go fmt ./...) || (echo "Run go fmt ./... and recommit your code";exit 1)
 go get -u github.com/golang/protobuf/protoc-gen-go
 go generate ./...
