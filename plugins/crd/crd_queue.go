185ba477dc0c75854c804f01d4b45040c239348b
Kyle Mestery <mestery@mestery.com>
2018-05-30 18:52:34 -0500

    Add a custom resource definition plugin (#54)
    * Add a custom resource definition plugin
    
    This adds a CRD plugin into networkservicemesh. This takes care of adding
    custom resources for networkserviceendpoints, networkservicechannels, and
    networkservices. This includes validators for the resources as well, and
    makes use of the existing generated go code from our protobufs as the
    payload.
    
    
    * Update function comment
    
    This fixes an issue noted by the linter around the comment for AfterInit().
    
    
    * Address initial set of comments from code review
    
    This fixes the initial set of comments from the code review and walkthrough
    done on 5/29.
    
    
    * Move CRD group back to networkservicemesh.io
    
    CRDs need to be formatted with a "." in the group name, otherwise when creating
    them you get an error like this:
    
    time="2018-05-30 13:58:58.32253" level=error msg="Error loading core: plugin CRD: AfterInit error 'CustomResourceDefinition.apiextensions.k8s.io \"networkserviceendpoints.networkservicemesh\" is invalid: spec.group: Invalid value: \"networkservicemesh\": should be a domain with at least one dot', took 37.918062ms" loc="core/event_loop.go(29)" logger=agentcore
    
    
    * CRD work queue updates
    
    Last set of updates from the code review yesterday. This updates the work queue
    after an extensive review.
    
    
    * Move where the worker queue readers are run
    
    These used to be in their own go routine, move them into the informer
    go routines instead. Having them as separate go routines meant there
    was a possible startup race condition.
    
    
    * Make sure to use the correct workqueue in the worker functions

---
+++ plugins/crd/crd_queue.go
@@ -0,0 +1,270 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// //go:generate protoc -I ./model/pod --go_out=plugins=grpc:./model/pod ./model/pod/pod.proto
+
+package netmeshplugincrd
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/tools/cache"
+)
+
+// This file contains the work queue backends for each of the CRDs we create in the
+// plugin AfterInit() call.
+
+func networkserviceWork(plugin *Plugin) {
+	for {
+		// We read a message off the queue ...
+		key, shutdown := queueNS.Get()
+
+		// If the queue has been shut down, we should exit the work queue here
+		if shutdown {
+			plugin.informerStopCh <- struct{}{}
+			return
+		}
+
+		// Convert the queue item into a string. If it's not a string, we'll
+		// simply discard it as invalid data and log a message.
+		var strKey string
+		var ok bool
+		if strKey, ok = key.(string); !ok {
+			runtime.HandleError(fmt.Errorf("key in queue should be of type string but got %T. discarding", key))
+			return
+		}
+
+		// We define a function here to process a queue item, so that we can
+		// use 'defer' to make sure the message is marked as Done on the queue
+		func(key string) {
+			var obj interface{}
+			var err error
+
+			defer queueNS.Done(key)
+
+			// Attempt to split the 'key' into namespace and object name
+			namespace, name, err := cache.SplitMetaNamespaceKey(strKey)
+
+			if err != nil {
+				plugin.Log.Errorf("Error splitting meta namespace key into parts: %s", err.Error())
+				runtime.HandleError(fmt.Errorf("Error splitting meta namespace key into parts: %s", err.Error()))
+				// This is a soft-error, we merely want to forget it and mark it done on the queue
+				queueNS.Forget(key)
+				return
+			}
+
+			plugin.Log.Infof("Read item '%s/%s' off workqueue. Processing...", namespace, name)
+
+			// Retrieve the latest version in the cache of this alert
+			obj, err = plugin.sharedFactoryNS.Networkservice().V1().NetworkServices().Lister().NetworkServices(namespace).Get(name)
+
+			if err != nil {
+				plugin.Log.Errorf("Error getting object '%s/%s' from api: %s", namespace, name, err.Error())
+				runtime.HandleError(fmt.Errorf("Error getting object '%s/%s' from api: %s", namespace, name, err.Error()))
+				// This is a hard-error, we'll raise the flag so the plugin catches this and shuts down
+				queueNS.Forget(key)
+				plugin.queueError <- true
+				return
+			}
+
+			plugin.Log.Infof("Got most up to date version of '%s/%s'. Syncing...", namespace, name)
+			plugin.Log.Infof("Object found: %s", obj)
+			plugin.Log.Infof("Finished processing '%s/%s' successfully! Removing from queue.", namespace, name)
+
+			// As we managed to process this successfully, we can forget it
+			// from the work queue altogether.
+			queueNS.Forget(key)
+		}(strKey)
+	}
+}
+
+// networkserviceEnqeue will add an object 'obj' into the workqueue. The object being added
+// must be of type metav1.Object, metav1.ObjectAccessor or cache.ExplicitKey.
+func networkserviceEnqueue(obj interface{}) {
+	// DeletionHandlingMetaNamespaceKeyFunc will convert an object into a
+	// 'namespace/name' string. We do this because our item may be processed
+	// much later than now, and so we want to ensure it gets a fresh copy of
+	// the resource when it starts. Also, this allows us to keep adding the
+	// same item into the work queue without duplicates building up.
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("error obtaining key for object being enqueue: %s", err.Error()))
+		return
+	}
+	// Add the item to the queue
+	queueNS.Add(key)
+}
+
+func networkservicechannelWork(plugin *Plugin) {
+	for {
+		// We read a message off the queue
+		key, shutdown := queueNSC.Get()
+
+		// If the queue has been shut down, we should exit the work queue here
+		if shutdown {
+			plugin.informerStopCh <- struct{}{}
+			return
+		}
+
+		// Convert the queue item into a string. If it's not a string, we'll
+		// simply discard it as invalid data and log a message.
+		var strKey string
+		var ok bool
+		if strKey, ok = key.(string); !ok {
+			runtime.HandleError(fmt.Errorf("key in queue should be of type string but got %T. discarding", key))
+			return
+		}
+
+		// We define a function here to process a queue item, so that we can
+		// use 'defer' to make sure the message is marked as Done on the queue
+		func(key string) {
+			var obj interface{}
+			var err error
+
+			defer queueNSC.Done(key)
+
+			// Attempt to split the 'key' into namespace and object name
+			namespace, name, err := cache.SplitMetaNamespaceKey(strKey)
+
+			if err != nil {
+				plugin.Log.Errorf("Error splitting meta namespace key into parts: %s", err.Error())
+				runtime.HandleError(fmt.Errorf("error splitting meta namespace key into parts: %s", err.Error()))
+				// This is a soft-error, we merely want to forget it and mark it done on the queue
+				queueNSC.Forget(key)
+				return
+			}
+
+			plugin.Log.Infof("Read item '%s/%s' off workqueue. Processing...", namespace, name)
+
+			// Retrieve the latest version in the cache of this alert
+			obj, err = plugin.sharedFactoryNS.Networkservice().V1().NetworkServiceChannels().Lister().NetworkServiceChannels(namespace).Get(name)
+
+			if err != nil {
+				plugin.Log.Errorf("Error getting object '%s/%s' from api: %s", namespace, name, err.Error())
+				runtime.HandleError(fmt.Errorf("Error getting object '%s/%s' from api: %s", namespace, name, err.Error()))
+				// This is a hard-error, we'll raise the flag so the plugin catches this and shuts down
+				queueNSC.Forget(key)
+				plugin.queueError <- true
+				return
+			}
+
+			plugin.Log.Infof("Got most up to date version of '%s/%s'. Syncing...", namespace, name)
+			plugin.Log.Infof("Object found: %s", obj)
+			plugin.Log.Infof("Finished processing '%s/%s' successfully! Removing from queue.", namespace, name)
+
+			// As we managed to process this successfully, we can forget it
+			// from the work queue altogether.
+			queueNSC.Forget(key)
+		}(strKey)
+	}
+}
+
+// networkserviceChannelEnqueue will add an object 'obj' into the workqueue. The object being added
+// must be of type metav1.Object, metav1.ObjectAccessor or cache.ExplicitKey.
+func networkservicechannelEnqueue(obj interface{}) {
+	// DeletionHandlingMetaNamespaceKeyFunc will convert an object into a
+	// 'namespace/name' string. We do this because our item may be processed
+	// much later than now, and so we want to ensure it gets a fresh copy of
+	// the resource when it starts. Also, this allows us to keep adding the
+	// same item into the work queue without duplicates building up.
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("error obtaining key for object being enqueue: %s", err.Error()))
+		return
+	}
+	// Add the item to the queue
+	queueNSC.Add(key)
+}
+
+func networkserviceendpointWork(plugin *Plugin) {
+	for {
+		// We read a message off the queue
+		key, shutdown := queueNSE.Get()
+
+		// If the queue has been shut down, we should exit the work queue here
+		if shutdown {
+			plugin.informerStopCh <- struct{}{}
+			return
+		}
+
+		// Convert the queue item into a string. If it's not a string, we'll
+		// simply discard it as invalid data and log a message.
+		var strKey string
+		var ok bool
+		if strKey, ok = key.(string); !ok {
+			runtime.HandleError(fmt.Errorf("key in queue should be of type string but got %T. discarding", key))
+			return
+		}
+
+		// We define a function here to process a queue item, so that we can
+		// use 'defer' to make sure the message is marked as Done on the queue
+		func(key string) {
+			var obj interface{}
+			var err error
+
+			defer queueNSE.Done(key)
+
+			// Attempt to split the 'key' into namespace and object name
+			namespace, name, err := cache.SplitMetaNamespaceKey(strKey)
+
+			if err != nil {
+				plugin.Log.Errorf("Error splitting meta namespace key into parts: %s", err.Error())
+				runtime.HandleError(fmt.Errorf("error splitting meta namespace key into parts: %s", err.Error()))
+				// This is a soft-error, we merely want to forget it and mark it done on the queue
+				queueNSE.Forget(key)
+				return
+			}
+
+			plugin.Log.Infof("Read item '%s/%s' off workqueue. Processing...", namespace, name)
+
+			// Retrieve the latest version in the cache of this alert
+			obj, err = plugin.sharedFactoryNS.Networkservice().V1().NetworkServiceEndpoints().Lister().NetworkServiceEndpoints(namespace).Get(name)
+
+			if err != nil {
+				plugin.Log.Errorf("Error getting object '%s/%s' from api: %s", namespace, name, err.Error())
+				runtime.HandleError(fmt.Errorf("Error getting object '%s/%s' from api: %s", namespace, name, err.Error()))
+				// This is a hard-error, we'll raise the flag so the plugin catches this and shuts down
+				queueNSE.Forget(key)
+				plugin.queueError <- true
+				return
+			}
+
+			plugin.Log.Infof("Got most up to date version of '%s/%s'. Syncing...", namespace, name)
+			plugin.Log.Infof("Object found: %s", obj)
+			plugin.Log.Infof("Finished processing '%s/%s' successfully! Removing from queue.", namespace, name)
+
+			// As we managed to process this successfully, we can forget it
+			// from the work queue altogether.
+			queueNSE.Forget(key)
+		}(strKey)
+	}
+}
+
+// networkserviceEndpointEnqueue will add an object 'obj' into the workqueue. The object being added
+// must be of type metav1.Object, metav1.ObjectAccessor or cache.ExplicitKey.
+func networkserviceendpointEnqueue(obj interface{}) {
+	// DeletionHandlingMetaNamespaceKeyFunc will convert an object into a
+	// 'namespace/name' string. We do this because our item may be processed
+	// much later than now, and so we want to ensure it gets a fresh copy of
+	// the resource when it starts. Also, this allows us to keep adding the
+	// same item into the work queue without duplicates building up.
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("error obtaining key for object being enqueue: %s", err.Error()))
+		return
+	}
+	// Add the item to the queue
+	queueNSE.Add(key)
+}
---
+++ plugins/crd/plugin_impl_crd.go
@@ -0,0 +1,330 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// //go:generate protoc -I ./model/pod --go_out=plugins=grpc:./model/pod ./model/pod/pod.proto
+
+package netmeshplugincrd
+
+import (
+	"fmt"
+	"reflect"
+	"sync"
+	"time"
+
+	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextcs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
+
+	"github.com/ligato/cn-infra/config"
+	"github.com/ligato/cn-infra/flavors/local"
+	"github.com/ligato/cn-infra/health/statuscheck"
+	"github.com/ligato/cn-infra/logging"
+	"github.com/ligato/networkservicemesh/pkg/apis/networkservicemesh.io/v1"
+	client "github.com/ligato/networkservicemesh/pkg/client/clientset/versioned"
+	factory "github.com/ligato/networkservicemesh/pkg/client/informers/externalversions"
+)
+
+// Plugin watches K8s resources and causes all changes to be reflected in the ETCD
+// data store.
+type Plugin struct {
+	Deps
+
+	pluginStopCh    chan struct{}
+	wg              sync.WaitGroup
+	k8sClientConfig *rest.Config
+	k8sClientset    *kubernetes.Clientset
+	apiclientset    *apiextcs.Clientset
+	crdClient       client.Interface
+	StatusMonitor   statuscheck.StatusReader
+
+	// Used to signal hard errors while processing the queue
+	queueError chan bool
+	// informerStopCh can be used to stop all the informer, as well as control loops
+	// within the application.
+	informerStopCh chan struct{}
+	// sharedFactory is a shared informer factory that is used a a cache for
+	// items in the API server. It saves each informer listing and watching the
+	// same resources independently of each other, thus providing more up to
+	// date results with less 'effort'
+	sharedFactoryNS  factory.SharedInformerFactory
+	sharedFactoryNSE factory.SharedInformerFactory
+	sharedFactoryNSC factory.SharedInformerFactory
+}
+
+var (
+	// queue is a queue of resources to be processed. It performs exponential
+	// backoff rate limiting, with a minimum retry period of 5 seconds and a
+	// maximum of 1 minute.
+	queueNS  = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
+	queueNSC = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
+	queueNSE = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
+)
+
+// Deps defines dependencies of netmesh plugin.
+type Deps struct {
+	local.PluginInfraDeps
+	// Kubeconfig with k8s cluster address and access credentials to use.
+	KubeConfig config.PluginConfig
+}
+
+// Init builds K8s client-set based on the supplied kubeconfig and initializes
+// all reflectors.
+func (plugin *Plugin) Init() error {
+	var err error
+	plugin.Log.SetLevel(logging.DebugLevel)
+	plugin.pluginStopCh = make(chan struct{})
+
+	kubeconfig := plugin.KubeConfig.GetConfigName()
+	plugin.Log.WithField("kubeconfig", kubeconfig).Info("Loading kubernetes client config")
+	plugin.k8sClientConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return fmt.Errorf("Failed to build kubernetes client config: %s", err)
+	}
+
+	plugin.k8sClientset, err = kubernetes.NewForConfig(plugin.k8sClientConfig)
+	if err != nil {
+		return fmt.Errorf("Failed to build kubernetes client: %s", err)
+	}
+
+	plugin.informerStopCh = make(chan struct{})
+	plugin.queueError = make(chan bool, 1)
+
+	return nil
+}
+
+// Create the CRD resource, ignore error if it already exists
+func createCRD(plugin *Plugin, FullName, Group, Version, Plural, Name string) error {
+	crd := &apiextv1beta1.CustomResourceDefinition{
+		ObjectMeta: meta.ObjectMeta{Name: FullName},
+		Spec: apiextv1beta1.CustomResourceDefinitionSpec{
+			Group:   Group,
+			Version: Version,
+			Scope:   apiextv1beta1.NamespaceScoped,
+			Names: apiextv1beta1.CustomResourceDefinitionNames{
+				Plural: Plural,
+				Kind:   Name,
+			},
+		},
+	}
+
+	_, cserr := plugin.apiclientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+	if cserr != nil && apierrors.IsAlreadyExists(cserr) {
+		plugin.Log.Infof("Created CRD %s succesfully, though it already existed", Name)
+		return nil
+	} else if cserr != nil {
+		plugin.Log.Infof("Error creating CRD %s: %s", Name, cserr)
+	} else {
+		plugin.Log.Infof("Created CRD %s succesfully", Name)
+	}
+
+	return cserr
+}
+
+func informerNetworkServices(plugin *Plugin) {
+	// We use a shared informer from the informer factory, to save calls to the
+	// API as we grow our application and so state is consistent between our
+	// control loops. We set a resync period of 30 seconds, in case any
+	// create/replace/update/delete operations are missed when watching
+	plugin.sharedFactoryNS = factory.NewSharedInformerFactory(plugin.crdClient, time.Second*30)
+
+	informer := plugin.sharedFactoryNS.Networkservice().V1().NetworkServices().Informer()
+	// We add a new event handler, watching for changes to API resources.
+	informer.AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: networkserviceEnqueue,
+			UpdateFunc: func(old, cur interface{}) {
+				if !reflect.DeepEqual(old, cur) {
+					networkserviceEnqueue(cur)
+				}
+			},
+			DeleteFunc: networkserviceEnqueue,
+		},
+	)
+
+	// Start the informer. This will cause it to begin receiving updates from
+	// the configured API server and firing event handlers in response.
+	plugin.sharedFactoryNS.Start(plugin.informerStopCh)
+	plugin.Log.Info("Started NetworkService informer factory.")
+
+	// Wait for the informer cache to finish performing it's initial sync of
+	// resources
+	if !cache.WaitForCacheSync(plugin.informerStopCh, informer.HasSynced) {
+		plugin.Log.Error("Error waiting for informer cache to sync")
+	}
+
+	plugin.Log.Info("NetworkService Informer is ready")
+
+	// Read forever from the work queue
+	networkserviceWork(plugin)
+}
+
+func informerNetworkServiceChannels(plugin *Plugin) {
+	// We use a shared informer from the informer factory, to save calls to the
+	// API as we grow our application and so state is consistent between our
+	// control loops. We set a resync period of 30 seconds, in case any
+	// create/replace/update/delete operations are missed when watching
+	plugin.sharedFactoryNSC = factory.NewSharedInformerFactory(plugin.crdClient, time.Second*30)
+
+	informer := plugin.sharedFactoryNSC.Networkservice().V1().NetworkServiceChannels().Informer()
+	// we add a new event handler, watching for changes to API resources.
+	informer.AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: networkservicechannelEnqueue,
+			UpdateFunc: func(old, cur interface{}) {
+				if !reflect.DeepEqual(old, cur) {
+					networkservicechannelEnqueue(cur)
+				}
+			},
+			DeleteFunc: networkservicechannelEnqueue,
+		},
+	)
+
+	// Start the informer. This will cause it to begin receiving updates from
+	// the configured API server and firing event handlers in response.
+	plugin.sharedFactoryNSC.Start(plugin.informerStopCh)
+	plugin.Log.Info("Started NetworkServiceChannel informer factory.")
+
+	// Wait for the informer cache to finish performing it's initial sync of
+	// resources
+	if !cache.WaitForCacheSync(plugin.informerStopCh, informer.HasSynced) {
+		plugin.Log.Errorf("Error waiting for informer cache to sync")
+	}
+
+	plugin.Log.Info("NetworkServiceChannel Informer is ready")
+
+	// Read forever from the work queue
+	networkservicechannelWork(plugin)
+}
+
+func informerNetworkServiceEndpoints(plugin *Plugin) {
+	// We use a shared informer from the informer factory, to save calls to the
+	// API as we grow our application and so state is consistent between our
+	// control loops. We set a resync period of 30 seconds, in case any
+	// create/replace/update/delete operations are missed when watching
+	plugin.sharedFactoryNSE = factory.NewSharedInformerFactory(plugin.crdClient, time.Second*30)
+
+	informer := plugin.sharedFactoryNSE.Networkservice().V1().NetworkServiceEndpoints().Informer()
+	// we add a new event handler, watching for changes to API resources.
+	informer.AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: networkserviceendpointEnqueue,
+			UpdateFunc: func(old, cur interface{}) {
+				if !reflect.DeepEqual(old, cur) {
+					networkserviceendpointEnqueue(cur)
+				}
+			},
+			DeleteFunc: networkserviceendpointEnqueue,
+		},
+	)
+
+	// Start the informer. This will cause it to begin receiving updates from
+	// the configured API server and firing event handlers in response.
+	plugin.sharedFactoryNSE.Start(plugin.informerStopCh)
+	plugin.Log.Info("Started NetworkServiceEndpoints informer factory.")
+
+	// Wait for the informer cache to finish performing it's initial sync of
+	// resources
+	if !cache.WaitForCacheSync(plugin.informerStopCh, informer.HasSynced) {
+		plugin.Log.Errorf("Error waiting for informer cache to sync")
+	}
+
+	plugin.Log.Info("NetworkServiceEndpoint Informer is ready")
+
+	// Read forever from the work queue
+	networkserviceendpointWork(plugin)
+}
+
+// AfterInit This will create all of the CRDs for NetworkServiceMesh.
+func (plugin *Plugin) AfterInit() error {
+	var err error
+	var crdname string
+
+	// Create clientset and create our CRD, this only needs to run once
+	plugin.apiclientset, err = apiextcs.NewForConfig(plugin.k8sClientConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create an instance of our own API client
+	plugin.crdClient, err = client.NewForConfig(plugin.k8sClientConfig)
+	if err != nil {
+		plugin.Log.Errorf("Error creating CRD client: %s", err.Error())
+		panic(err.Error())
+	}
+
+	crdname = reflect.TypeOf(v1.NetworkServiceEndpoint{}).Name()
+	err = createCRD(plugin, v1.FullNSMEPName,
+		v1.NSMGroup,
+		v1.NSMGroupVersion,
+		v1.NSMEPPlural,
+		crdname)
+
+	if err != nil {
+		plugin.Log.Error("Error initializing NetworkServiceEndpoint CRD")
+		return err
+	}
+
+	crdname = reflect.TypeOf(v1.NetworkServiceChannel{}).Name()
+	err = createCRD(plugin, v1.FullNSMChannelName,
+		v1.NSMGroup,
+		v1.NSMGroupVersion,
+		v1.NSMChannelPlural,
+		crdname)
+
+	if err != nil {
+		plugin.Log.Error("Error initializing NetworkServiceChannel CRD")
+		return err
+	}
+
+	crdname = reflect.TypeOf(v1.NetworkService{}).Name()
+	err = createCRD(plugin, v1.FullNSMName,
+		v1.NSMGroup,
+		v1.NSMGroupVersion,
+		v1.NSMPlural,
+		crdname)
+
+	if err != nil {
+		plugin.Log.Error("Error initializing NetworkService CRD")
+		return err
+	}
+
+	go informerNetworkServices(plugin)
+	go informerNetworkServiceChannels(plugin)
+	go informerNetworkServiceEndpoints(plugin)
+	go handleQueueErrors(plugin)
+
+	return nil
+}
+
+// handleQueueErrors monitors the queueError channel for errors from the
+// dequeueing functions and closes the plugin down if one is received.
+func handleQueueErrors(plugin *Plugin) {
+	<-plugin.queueError
+	plugin.Log.Error("Error processing queues, shutting plugin down")
+
+	plugin.Close()
+}
+
+// Close stops all reflectors.
+func (plugin *Plugin) Close() error {
+	close(plugin.pluginStopCh)
+	plugin.wg.Wait()
+	return nil
+}
