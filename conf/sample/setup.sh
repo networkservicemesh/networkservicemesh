261fd74fa3518a644cae45ae0db1ff479c9c2e04
Kyle Mestery <mestery@mestery.com>
2018-06-04 18:05:56 -0500

    Add sample configuration and scripts to test CRDs (#60)
    * Add sample configuration and scripts to test CRDs
    
    This commit adds some sample yaml configuration files to create all the CRDs
    in use by Network Service Mesh. It also adds some simple shell scripts to
    test creation and deletion of these resources.
    
    
    * Correct wording in setup/teardown scripts
    
    
    * Add docs directory
    
    Add some instructions on running Network Service Mesh with Minikube as well.

---
+++ conf/sample/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Sample script for creating Network Service Mesh CRDs.
+#
+# NOTE: This assumes minikube for now during the label step.
+
+# First, label the nodes
+kubectl label --overwrite nodes minikube app=networkservice-node
+
+# Now create the daemonset
+kubectl create -f conf/sample/networkservice-daemonset.yaml
+
+# Create the channel
+kubectl create -f conf/sample/networkservice-channel.yaml
+
+# Create the endpoints
+kubectl create -f conf/sample/networkservice-endpoint.yaml
+
+# Finally, create the network service
+kubectl create -f conf/sample/networkservice.yaml
+
+# Dump some info
+kubectl get pods,crd,NetworkService,NetworkServiceChannel,NetworkServiceEndpoint
