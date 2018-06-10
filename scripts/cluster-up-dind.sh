135b9e871457e1ab4b6afd767141f5ea89f77ae5
Kyle Mestery <mestery@mestery.com>
2018-06-09 20:18:07 -0500

    Update copyrights to include Cisco
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- scripts/cluster-up-dind.sh
+++ scripts/cluster-up-dind.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copyright (c) 2016-2017 Bitnami
+# Copyright (c) 2018 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
