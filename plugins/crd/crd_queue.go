networkservicemesh/networkservicemesh

commit 060979ebe42eb7345e15ac295373617a0f85e807

Refactor queue handling code (#85)
* Refactor queue handling code

Closes #82

This commit refactors the workqueue code so it can be more effectively shared
by all CRD handlers. It also moves the workqueue definitions themselves into
the crd_queue.go file where they more naturally belong.

Signed-off-by: Kyle Mestery <mestery@mestery.com>

* Fix some go formatting errors

Signed-off-by: Kyle Mestery <mestery@mestery.com>

plugins/crd/crd_queue.go
@@ -18,26 +18,38 @@ package netmeshplugincrd
 
 import (
 	"fmt"
+	"time"
 
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
 )
 
-// How many times do we retry processing an item
-const QUEUE_RETRY_COUNT = 5
+// QueueRetryCount is the max number of times to retry processing a failed item
+// from the workqueue.
+const QueueRetryCount = 5
+
+var (
+	// queue is a queue of resources to be processed. It performs exponential
+	// backoff rate limiting, with a minimum retry period of 5 seconds and a
+	// maximum of 1 minute.
+	queueNS  = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
+	queueNSC = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
+	queueNSE = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
+)
 
 // This file contains the work queue backends for each of the CRDs we create in the
 // plugin AfterInit() call.
 
-func networkserviceWork(plugin *Plugin) {
+func workforever(plugin *Plugin, queue workqueue.RateLimitingInterface, stopCH chan struct{}) {
 	for {
 		// We read a message off the queue ...
-		key, shutdown := queueNS.Get()
+		key, shutdown := queue.Get()
 
 		// If the queue has been shut down, we should exit the work queue here
 		if shutdown {
 			plugin.Log.Error("shutdown signaled, closing stopChNS")
-			plugin.stopChNS <- struct{}{}
+			stopCH <- struct{}{}
 			return
 		}
 
@@ -53,7 +65,7 @@ func networkserviceWork(plugin *Plugin) {
 		// We define a function here to process a queue item, so that we can
 		// use 'defer' to make sure the message is marked as Done on the queue
 		func(key string) {
-			defer queueNS.Done(key)
+			defer queue.Done(key)
 
 			// Attempt to split the 'key' into namespace and object name
 			namespace, name, err := cache.SplitMetaNamespaceKey(strKey)
@@ -61,7 +73,7 @@ func networkserviceWork(plugin *Plugin) {
 			if err != nil {
 				// This is a soft-error
 				plugin.Log.Errorf("Error splitting meta namespace key into parts: %s", err.Error())
-				queueNS.Forget(key)
+				queue.Forget(key)
 				return
 			}
 
@@ -73,15 +85,15 @@ func networkserviceWork(plugin *Plugin) {
 			item, exists, err := plugin.informerNS.GetIndexer().GetByKey(strKey)
 
 			if err != nil {
-				if queueNS.NumRequeues(key) < QUEUE_RETRY_COUNT {
+				if queue.NumRequeues(key) < QueueRetryCount {
 					plugin.Log.Errorf("Requeueing after error processing item with key %s, error %v", key, err)
-					queueNS.AddRateLimited(key)
-					return
-				} else {
-					plugin.Log.Errorf("Failed processing item with key %s, error %v, no more retries", key, err)
-					queueNS.Forget(key)
+					queue.AddRateLimited(key)
 					return
 				}
+
+				plugin.Log.Errorf("Failed processing item with key %s, error %v, no more retries", key, err)
+				queue.Forget(key)
+				return
 			}
 
 			// Verify if this was a delete vs. an add/update
@@ -95,7 +107,7 @@ func networkserviceWork(plugin *Plugin) {
 
 			// As we managed to process this successfully, we can forget it
 			// from the work queue altogether.
-			queueNS.Forget(key)
+			queue.Forget(key)
 		}(strKey)
 	}
 }
@@ -117,77 +129,6 @@ func networkserviceEnqueue(obj interface{}) {
 	queueNS.Add(key)
 }
 
-func networkservicechannelWork(plugin *Plugin) {
-	for {
-		// We read a message off the queue
-		key, shutdown := queueNSC.Get()
-
-		// If the queue has been shut down, we should exit the work queue here
-		if shutdown {
-			plugin.Log.Error("shutdown signaled, closing stopChNSC")
-			plugin.stopChNSC <- struct{}{}
-			return
-		}
-
-		// Convert the queue item into a string. If it's not a string, we'll
-		// simply discard it as invalid data and log a message.
-		var strKey string
-		var ok bool
-		if strKey, ok = key.(string); !ok {
-			runtime.HandleError(fmt.Errorf("key in queue should be of type string but got %T. discarding", key))
-			return
-		}
-
-		// We define a function here to process a queue item, so that we can
-		// use 'defer' to make sure the message is marked as Done on the queue
-		func(key string) {
-			defer queueNSC.Done(key)
-
-			// Attempt to split the 'key' into namespace and object name
-			namespace, name, err := cache.SplitMetaNamespaceKey(strKey)
-
-			if err != nil {
-				// This is a soft-error
-				plugin.Log.Errorf("Error splitting meta namespace key into parts: %s", err.Error())
-				queueNSC.Forget(key)
-				return
-			}
-
-			plugin.Log.Infof("Read item '%s/%s' off workqueue. Processing...", namespace, name)
-
-			// Retrieve the latest version in the cache of this NetworkService. By using
-			// GetByKey() we are able to determine if the item exists, and thus if it was
-			// added or deleted from the queue, and process appropriately
-			item, exists, err := plugin.informerNSC.GetIndexer().GetByKey(strKey)
-
-			if err != nil {
-				if queueNSC.NumRequeues(key) < QUEUE_RETRY_COUNT {
-					plugin.Log.Errorf("Requeueing after error processing item with key %s, error %v", key, err)
-					queueNSC.AddRateLimited(key)
-					return
-				} else {
-					plugin.Log.Errorf("Failed processing item with key %s, error %v, no more retries", key, err)
-					queueNSC.Forget(key)
-					return
-				}
-			}
-
-			// Verify if this was a delete vs. an add/update
-			if !exists {
-				plugin.Log.Infof("Object (%s) deleted from queue", name)
-			} else {
-				plugin.Log.Infof("Got most up to date version of '%s/%s'. Syncing...", namespace, name)
-				plugin.Log.Infof("Object found: %s", item)
-				plugin.Log.Infof("Finished processing '%s/%s' successfully! Removing from queue.", namespace, name)
-			}
-
-			// As we managed to process this successfully, we can forget it
-			// from the work queue altogether.
-			queueNSC.Forget(key)
-		}(strKey)
-	}
-}
-
 // networkserviceChannelEnqueue will add an object 'obj' into the workqueue. The object being added
 // must be of type metav1.Object, metav1.ObjectAccessor or cache.ExplicitKey.
 func networkservicechannelEnqueue(obj interface{}) {
@@ -203,77 +144,6 @@ func networkservicechannelEnqueue(obj interface{}) {
 	}
 	// Add the item to the queue
 	queueNSC.Add(key)
-}
-
-func networkserviceendpointWork(plugin *Plugin) {
-	for {
-		// We read a message off the queue
-		key, shutdown := queueNSE.Get()
-
-		// If the queue has been shut down, we should exit the work queue here
-		if shutdown {
-			plugin.Log.Error("shutdown signaled, closing stopChNSE")
-			plugin.stopChNSE <- struct{}{}
-			return
-		}
-
-		// Convert the queue item into a string. If it's not a string, we'll
-		// simply discard it as invalid data and log a message.
-		var strKey string
-		var ok bool
-		if strKey, ok = key.(string); !ok {
-			runtime.HandleError(fmt.Errorf("key in queue should be of type string but got %T. discarding", key))
-			return
-		}
-
-		// We define a function here to process a queue item, so that we can
-		// use 'defer' to make sure the message is marked as Done on the queue
-		func(key string) {
-			defer queueNSE.Done(key)
-
-			// Attempt to split the 'key' into namespace and object name
-			namespace, name, err := cache.SplitMetaNamespaceKey(strKey)
-
-			if err != nil {
-				// This is a soft-error
-				plugin.Log.Errorf("Error splitting meta namespace key into parts: %s", err.Error())
-				queueNSE.Forget(key)
-				return
-			}
-
-			plugin.Log.Infof("Read item '%s/%s' off workqueue. Processing...", namespace, name)
-
-			// Retrieve the latest version in the cache of this NetworkService. By using
-			// GetByKey() we are able to determine if the item exists, and thus if it was
-			// added or deleted from the queue, and process appropriately
-			item, exists, err := plugin.informerNSE.GetIndexer().GetByKey(strKey)
-
-			if err != nil {
-				if queueNSE.NumRequeues(key) < QUEUE_RETRY_COUNT {
-					plugin.Log.Errorf("Requeueing after error processing item with key %s, error %v", key, err)
-					queueNSE.AddRateLimited(key)
-					return
-				} else {
-					plugin.Log.Errorf("Failed processing item with key %s, error %v, no more retries", key, err)
-					queueNSE.Forget(key)
-					return
-				}
-			}
-
-			// Verify if this was a delete vs. an add/update
-			if !exists {
-				plugin.Log.Infof("Object (%s) deleted from queue", name)
-			} else {
-				plugin.Log.Infof("Got most up to date version of '%s/%s'. Syncing...", namespace, name)
-				plugin.Log.Infof("Object found: %s", item)
-				plugin.Log.Infof("Finished processing '%s/%s' successfully! Removing from queue.", namespace, name)
-			}
-
-			// As we managed to process this successfully, we can forget it
-			// from the work queue altogether.
-			queueNSE.Forget(key)
-		}(strKey)
-	}
 }
 
 // networkserviceEndpointEnqueue will add an object 'obj' into the workqueue. The object being added

plugins/crd/plugin_impl_crd.go
@@ -30,7 +30,6 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/workqueue"
 
 	"github.com/ligato/cn-infra/config"
 	"github.com/ligato/cn-infra/flavors/local"
@@ -72,15 +71,6 @@ type Plugin struct {
 	informerNSE cache.SharedIndexInformer
 	informerNSC cache.SharedIndexInformer
 }
-
-var (
-	// queue is a queue of resources to be processed. It performs exponential
-	// backoff rate limiting, with a minimum retry period of 5 seconds and a
-	// maximum of 1 minute.
-	queueNS  = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
-	queueNSC = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
-	queueNSE = workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute))
-)
 
 // Deps defines dependencies of netmesh plugin.
 type Deps struct {
@@ -278,7 +268,7 @@ func informerNetworkServices(plugin *Plugin) {
 	plugin.Log.Info("NetworkService Informer is ready")
 
 	// Read forever from the work queue
-	networkserviceWork(plugin)
+	workforever(plugin, queueNS, plugin.stopChNS)
 }
 
 func informerNetworkServiceChannels(plugin *Plugin) {
@@ -316,7 +306,7 @@ func informerNetworkServiceChannels(plugin *Plugin) {
 	plugin.Log.Info("NetworkServiceChannel Informer is ready")
 
 	// Read forever from the work queue
-	networkservicechannelWork(plugin)
+	workforever(plugin, queueNSC, plugin.stopChNSC)
 }
 
 func informerNetworkServiceEndpoints(plugin *Plugin) {
@@ -354,7 +344,7 @@ func informerNetworkServiceEndpoints(plugin *Plugin) {
 	plugin.Log.Info("NetworkServiceEndpoint Informer is ready")
 
 	// Read forever from the work queue
-	networkserviceendpointWork(plugin)
+	workforever(plugin, queueNSE, plugin.stopChNSE)
 }
 
 // AfterInit This will create all of the CRDs for NetworkServiceMesh.

commit 6afb79061ff7d772d7fd205b825f9ce0f8096d0d

Fix informer factory usage
Commit 060979e introduced a problem when refactoring the workqueue
code. It solely used the NetworkService informer, which meant that
we were not properly dequeueing NetworkServerChannels or
NetworkServiceEndpoints. This commit corrects this bug.

Signed-off-by: Kyle Mestery <mestery@mestery.com>

plugins/crd/crd_queue.go
@@ -41,7 +41,7 @@ var (
 // This file contains the work queue backends for each of the CRDs we create in the
 // plugin AfterInit() call.
 
-func workforever(plugin *Plugin, queue workqueue.RateLimitingInterface, stopCH chan struct{}) {
+func workforever(plugin *Plugin, queue workqueue.RateLimitingInterface, informer cache.SharedIndexInformer, stopCH chan struct{}) {
 	for {
 		// We read a message off the queue ...
 		key, shutdown := queue.Get()
@@ -82,7 +82,7 @@ func workforever(plugin *Plugin, queue workqueue.RateLimitingInterface, stopCH c
 			// Retrieve the latest version in the cache of this NetworkService. By using
 			// GetByKey() we are able to determine if the item exists, and thus if it was
 			// added or deleted from the queue, and process appropriately
-			item, exists, err := plugin.informerNS.GetIndexer().GetByKey(strKey)
+			item, exists, err := informer.GetIndexer().GetByKey(strKey)
 
 			if err != nil {
 				if queue.NumRequeues(key) < QueueRetryCount {

plugins/crd/plugin_impl_crd.go
@@ -258,7 +258,7 @@ func informerNetworkServices(plugin *Plugin) {
 	plugin.Log.Info("NetworkService Informer is ready")
 
 	// Read forever from the work queue
-	workforever(plugin, queueNS, plugin.stopChNS)
+	workforever(plugin, queueNS, plugin.informerNS, plugin.stopChNS)
 }
 
 func informerNetworkServiceChannels(plugin *Plugin) {
@@ -296,7 +296,7 @@ func informerNetworkServiceChannels(plugin *Plugin) {
 	plugin.Log.Info("NetworkServiceChannel Informer is ready")
 
 	// Read forever from the work queue
-	workforever(plugin, queueNSC, plugin.stopChNSC)
+	workforever(plugin, queueNSC, plugin.informerNSC, plugin.stopChNSC)
 }
 
 func informerNetworkServiceEndpoints(plugin *Plugin) {
@@ -334,7 +334,7 @@ func informerNetworkServiceEndpoints(plugin *Plugin) {
 	plugin.Log.Info("NetworkServiceEndpoint Informer is ready")
 
 	// Read forever from the work queue
-	workforever(plugin, queueNSE, plugin.stopChNSE)
+	workforever(plugin, queueNSE, plugin.informerNSE, plugin.stopChNSE)
 }
 
 // AfterInit This will create all of the CRDs for NetworkServiceMesh.