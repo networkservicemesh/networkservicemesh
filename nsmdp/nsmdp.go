2ca64193f6e2c0bd022a23d6c0b50788b2b5f214
Serguei Bezverkhi <sbezverk@cisco.com>
2018-06-20 17:12:11 -0400

    Refactor device plugin to register with kubelet and dynamically start client's gRCP servers (#102)
    * Initial code load
    
    
    * Addressing failure in CI
    
    
    * Adjusting deplpoyment yaml
    
    
    * Addign information collection after CI failure
    
    
    * Applying rshared workaround
    
    
    * Adding plugin's logger
    
    
    * Addressing comments
    
    
    * Addressing comments

--- nsmdp/nsmdp.go
+++ nsmdp/nsmdp.go
@@ -20,89 +20,125 @@
 package nsmdp
 
 import (
-	"context"
-	"crypto/rand"
 	"fmt"
+	"net"
 	"os"
+	"path"
+	"strconv"
+	"time"
 
-	"github.com/ligato/networkservicemesh/deviceplugin"
+	"github.com/ligato/cn-infra/logging"
+
+	"golang.org/x/net/context"
+	"golang.org/x/sys/unix"
+	"google.golang.org/grpc"
 	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1beta1"
 )
 
 const (
-	SocketBaseDir = "/var/lib/networkservicemesh/"
-)
-
-type NSMDevicePlugin struct {
-	*deviceplugin.DevicePlugin
-	devs       map[string]*NSMDevice
-	updatedevs chan *NSMDevice
-	stop       chan interface{}
-}
-
-type NSMDevice struct {
-	*pluginapi.Device
-	token string
-}
-
-const (
+	socketBaseDir   = "/var/lib/networkservicemesh/"
 	resourceName    = "nsm.ligato.io/socket"
-	serverSock      = pluginapi.DevicePluginPath + "nsm.ligato.io.sock"
+	serverSock      = "nsm.ligato.io.sock"
 	initDeviceCount = 10
+	socketMask      = 0077
+	folderMask      = 0777
 )
 
-func NewNSMDevicePlugin() *NSMDevicePlugin {
-	n := &NSMDevicePlugin{
-		DevicePlugin: deviceplugin.NewDevicePlugin(serverSock, resourceName),
-		devs:         make(map[string]*NSMDevice),
-		stop:         make(chan interface{}),
-		updatedevs:   make(chan *NSMDevice, 10),
-	}
-	for i := uint(0); i < initDeviceCount; i++ {
-		dev := &NSMDevice{
-			Device: &pluginapi.Device{
-				ID:     "NSM_" + string(i),
-				Health: pluginapi.Healthy,
-			},
-			token: generateToken(),
-		}
-		n.updatedevs <- dev
-	}
-	return n
+type nsmSocket struct {
+	device      *pluginapi.Device
+	socketPath  string
+	stopChannel chan bool
+	allocated   bool
 }
 
-func generateToken() string {
-	b := make([]byte, 16)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)
+type nsmClientEndpoints struct {
+	nsmSockets map[string]nsmSocket
+	logger     logging.PluginLogger
 }
 
-func (n *NSMDevicePlugin) Stop() error {
-	err := n.DevicePlugin.Stop()
+// Define functions needed to meet the Kubernetes DevicePlugin API
+func (n *nsmClientEndpoints) GetDevicePluginOptions(context.Context, *pluginapi.Empty) (*pluginapi.DevicePluginOptions, error) {
+	n.logger.Infof("GetDevicePluginOptions was called.")
+	return &pluginapi.DevicePluginOptions{}, nil
+}
+
+func startClientServer(client *nsmSocket, logger logging.PluginLogger) {
+	listenEndpoint := client.socketPath
+	fi, err := os.Stat(listenEndpoint)
+	if err == nil && (fi.Mode()&os.ModeSocket) != 0 {
+		os.Remove(listenEndpoint)
+	}
+	if err != nil && !os.IsNotExist(err) {
+		logger.Errorf("failure stat of socket file %s with error: %+v", client.socketPath, err)
+		client.allocated = false
+		return
+	}
+
+	unix.Umask(socketMask)
+	sock, err := net.Listen("unix", listenEndpoint)
 	if err != nil {
-		return err
+		logger.Errorf("failure to listen on socket %s with error: %+v", client.socketPath, err)
+		client.allocated = false
+		return
 	}
-	close(n.stop)
-	return nil
+	grpcServer := grpc.NewServer([]grpc.ServerOption{}...)
 
+	logger.Infof("Starting Client gRPC server listening on socket: %s", serverSock)
+	go grpcServer.Serve(sock)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	conn, err := dial(ctx, listenEndpoint)
+	if err != nil {
+		logger.Errorf("failure to communicate with the socket %s with error: %+v", client.socketPath, err)
+		client.allocated = false
+	}
+	defer conn.Close()
+	defer cancel()
+	logger.Infof("Client Server socket: %s is operational", listenEndpoint)
+
+	// Wait for shutdown
+	select {
+	case <-client.stopChannel:
+		logger.Infof("Server for socket %s received shutdown request", client.socketPath)
+	}
+	client.allocated = false
+	client.stopChannel <- true
 }
 
-func (n *NSMDevicePlugin) Allocate(ctx context.Context, reqs *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
+func (n *nsmClientEndpoints) Allocate(ctx context.Context, reqs *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
+	n.logger.Info(" Allocate was called.")
 	responses := pluginapi.AllocateResponse{}
 	for _, req := range reqs.ContainerRequests {
 		var mounts []*pluginapi.Mount
 		for _, id := range req.DevicesIDs {
-			_, ok := n.devs[id]
-			if !ok {
-				return nil, fmt.Errorf("invalid allocation request: unknown device: %s", id)
+			if _, ok := n.nsmSockets[id]; ok {
+				if n.nsmSockets[id].allocated {
+					// Socket has been previsously used, since we did not get notification from
+					// kubelet when POD using this socket went down, gRPC client's server
+					// needs to be stopped.
+					n.nsmSockets[id].stopChannel <- true
+					// Wait for confirmation
+					<-n.nsmSockets[id].stopChannel
+					close(n.nsmSockets[id].stopChannel)
+				}
+				mount := &pluginapi.Mount{
+					ContainerPath: socketBaseDir,
+					HostPath:      path.Join(socketBaseDir, fmt.Sprintf("nsm-%s", id)),
+					ReadOnly:      false,
+				}
+				n.nsmSockets[id] = nsmSocket{
+					device:      &pluginapi.Device{ID: id, Health: pluginapi.Healthy},
+					socketPath:  path.Join(mount.HostPath, serverSock),
+					stopChannel: make(chan bool),
+					allocated:   true,
+				}
+				if err := os.MkdirAll(mount.HostPath, folderMask); err == nil {
+					// Starting Client's gRPC server and managed to create its host path.
+					client := n.nsmSockets[id]
+					go startClientServer(&client, n.logger)
+					mounts = append(mounts, mount)
+				}
 			}
-			mount := &pluginapi.Mount{
-				ContainerPath: SocketBaseDir + id,
-				HostPath:      SocketBaseDir + id,
-			}
-			os.MkdirAll(mount.HostPath, 0777)
-			// FIXME - Create socket files for Pod to NSM communication
-			mounts = append(mounts, mount)
 		}
 		response := pluginapi.ContainerAllocateResponse{
 			Mounts: mounts,
@@ -112,15 +148,113 @@ func (n *NSMDevicePlugin) Allocate(ctx context.Context, reqs *pluginapi.Allocate
 	return &responses, nil
 }
 
-func (n *NSMDevicePlugin) ListAndWatch(e *pluginapi.Empty, s pluginapi.DevicePlugin_ListAndWatchServer) error {
+func (n *nsmClientEndpoints) ListAndWatch(e *pluginapi.Empty, s pluginapi.DevicePlugin_ListAndWatchServer) error {
+	n.logger.Infof("ListAndWatch was called with s: %+v", s)
 	for {
-		select {
-		case <-n.stop:
-			return nil
-		case dev := <-n.updatedevs:
-			n.devs[dev.ID] = dev
-			devs := []*pluginapi.Device{dev.Device}
-			s.Send(&pluginapi.ListAndWatchResponse{Devices: devs})
+		resp := new(pluginapi.ListAndWatchResponse)
+		for _, dev := range n.nsmSockets {
+			resp.Devices = append(resp.Devices, dev.device)
+		}
+		if err := s.Send(resp); err != nil {
+			n.logger.Errorf("Failed to send response to kubelet: %v\n", err)
+		}
+		time.Sleep(5 * time.Second)
+	}
+	return nil
+}
+
+func (n *nsmClientEndpoints) PreStartContainer(context.Context, *pluginapi.PreStartContainerRequest) (*pluginapi.PreStartContainerResponse, error) {
+	n.logger.Infof("PreStartContainer was called.")
+	return &pluginapi.PreStartContainerResponse{}, nil
+}
+
+// Register registers
+func Register(kubeletEndpoint string) error {
+	conn, err := grpc.Dial(kubeletEndpoint, grpc.WithInsecure(),
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}))
+	defer conn.Close()
+	if err != nil {
+		return fmt.Errorf("device-plugin: cannot connect to kubelet service: %v", err)
+	}
+	client := pluginapi.NewRegistrationClient(conn)
+	reqt := &pluginapi.RegisterRequest{
+		Version:      pluginapi.Version,
+		Endpoint:     serverSock,
+		ResourceName: resourceName,
+	}
+
+	_, err = client.Register(context.Background(), reqt)
+	if err != nil {
+		return fmt.Errorf("device-plugin: cannot register to kubelet service: %v", err)
+	}
+	return nil
+}
+
+func dial(ctx context.Context, unixSocketPath string) (*grpc.ClientConn, error) {
+	c, err := grpc.DialContext(ctx, unixSocketPath, grpc.WithInsecure(), grpc.WithBlock(),
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func startDeviceServer(nsm *nsmClientEndpoints) error {
+	// Initial socket clean up
+	listenEndpoint := path.Join(pluginapi.DevicePluginPath, serverSock)
+	fi, err := os.Stat(listenEndpoint)
+	if err == nil && (fi.Mode()&os.ModeSocket) != 0 {
+		if err := os.Remove(listenEndpoint); err != nil {
+			return err
 		}
 	}
+
+	sock, err := net.Listen("unix", listenEndpoint)
+	if err != nil {
+		return err
+	}
+	grpcServer := grpc.NewServer([]grpc.ServerOption{}...)
+	pluginapi.RegisterDevicePluginServer(grpcServer, nsm)
+
+	nsm.logger.Infof("Starting Device Plugin's gRPC server listening on socket: %s", serverSock)
+	go grpcServer.Serve(sock)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	conn, err := dial(ctx, listenEndpoint)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	defer cancel()
+	nsm.logger.Infof("Socket: %s is operational", listenEndpoint)
+	return nil
+}
+
+func newNSMClientEndpoints(logger logging.PluginLogger) *nsmClientEndpoints {
+	nsm := &nsmClientEndpoints{
+		nsmSockets: map[string]nsmSocket{},
+		logger:     logger,
+	}
+	for i := 0; i < initDeviceCount; i++ {
+		nsm.nsmSockets[strconv.Itoa(i)] = nsmSocket{device: &pluginapi.Device{ID: strconv.Itoa(i), Health: pluginapi.Healthy}}
+	}
+	return nsm
+}
+
+// NewNSMDevicePlugin registers and starts Kubelet's device plugin
+func NewNSMDevicePlugin(logger logging.PluginLogger) error {
+	nsm := newNSMClientEndpoints(logger)
+	if err := startDeviceServer(nsm); err != nil {
+		return err
+	}
+	// Registers with Kubelet.
+	err := Register(pluginapi.KubeletSocket)
+
+	return err
 }
