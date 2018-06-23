e58e78ce28f81cf20e3b862b07f1d510641420b1
Ed Warnicke <hagbard@gmail.com>
2018-06-23 16:08:33 -0500

    Utilities to make idempotent plugins easy (#113)
    * Vendoring for upcoming util/idempotent
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Utilities to make idempotent plugins easy
    
    Embedding
    
    idempotent.Impl
    
    In a plugin and writing the Plugin Init() and Close()
    as:
    
    func (plugin *Plugin) Init() error {
         return plugin.IdempotentInit(plugin.init)
    }
    
    func (plugin *Plugin) Close() error {
         return plugin.IdempotentClose(plugin.close)
    }
    
    will make sure that the plugin Init() and Close()
    are idempotent.  Additionally, refCounting is performed
    so that the Plugin isn't *actually* closed until
    Close() has been called as many times as Init()
    
    This allows safe sharing of a Plugin.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Make ReInit after a Final close an error.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Replace IsIdempotent with IsClosed()
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Re-add IsIdempotent() to the interface
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Run go fmt
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

---
+++ utils/idempotent/idempotent_impl.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package core manages the lifecycle of all plugins (start, graceful
+// shutdown) and defines the core lifecycle SPI. The core lifecycle SPI
+// must be implemented by each plugin.
+
+package idempotent
+
+import (
+	"errors"
+	"sync"
+)
+
+const (
+	ReinitErrorStr = "True Close() has already occurred, plugin can no longer be Init() ed"
+)
+
+// IdemPotentImpl implements methods for wrapping Init() and Close() such that
+// the actual Init() and Close() are idempotent.
+type Impl struct {
+	refCount      int
+	refCountMutex sync.Mutex
+	initErr       error
+	closeErr      error
+}
+
+// IsIdempotent returns true if the object is idempotent
+// Its mostly used as a marker to match the IdemPotent interface
+func (i *Impl) IsIdempotent() bool {
+	return true
+}
+
+// IsClosed returns true if an Idempotent plugin is *truely* closed
+func (i *Impl) IsClosed() bool {
+	i.refCountMutex.Lock()
+	defer i.refCountMutex.Unlock()
+	return i.refCount < 0
+}
+
+// IdempotentInit increments the refCount and calls init precisely once
+func (i *Impl) IdempotentInit(init func() error) error {
+	i.refCountMutex.Lock()
+	defer i.refCountMutex.Unlock()
+	if i.refCount < 0 { // i.refCount < 0 means we are terminally closed and no longer Init-able
+		return errors.New(ReinitErrorStr)
+	}
+	i.refCount++
+	if i.refCount == 1 {
+		i.initErr = init()
+	}
+	return i.initErr
+}
+
+// IdemPotentClose decrements the refCount and calls close precisely once
+// when refCount is equal to zero.
+func (i *Impl) IdempotentClose(close func() error) (err error) {
+	i.refCountMutex.Lock()
+	defer i.refCountMutex.Unlock()
+	if i.refCount < 0 { // i.refCount < 0 means we are terminally closed and no longer Init-able
+		return i.closeErr
+	}
+	i.refCount--
+	if i.refCount == 0 {
+		i.closeErr = close()
+		i.refCount-- // Make sure refcount < 0
+	}
+	return i.closeErr
+}
