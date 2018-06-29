07220233adaa6df652cb9459998af4d534c5509a
Kyle Mestery <mestery@mestery.com>
2018-06-29 11:43:25 -0500

    Add channels and endpoints into the objectstore (#126)
    * Add channels and endpoints into the objectstore
    
    This commit adds an objectstore backend for channels and endpoints,
    including Create/Delete/List methods for each as well. The objectstore
    will be complete after this commit, and can cache all CRD objects
    for NSM clients to utilize.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>
    
    * Add objectstore unit testing
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>
    
    * Remove redundant definitions
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

---
+++ plugins/objectstore/networkservicechannels.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package objectstore
+
+import (
+	"sync"
+
+	"github.com/ligato/networkservicemesh/netmesh/model/netmesh"
+)
+
+// NetworkServiceChannelsStore map stores all discovered Network Service Channel
+// Objects with a key composed of a name and a namespace
+type networkServiceChannelsStore struct {
+	networkServiceChannel map[meta]*netmesh.NetworkService_NetmeshChannel
+	sync.RWMutex
+}
+
+// NewNetworkServiceChannelsStore instantiates a new instance of a global
+// NetworkServiceChannels store. It must be initialized before any controllers start.
+func newNetworkServiceChannelsStore() *networkServiceChannelsStore {
+	return &networkServiceChannelsStore{
+		networkServiceChannel: map[meta]*netmesh.NetworkService_NetmeshChannel{}}
+}
+
+// Add method adds descovered NetworkServiceChannel if it does not
+// already exit in the store.
+func (n *networkServiceChannelsStore) Add(ns *netmesh.NetworkService_NetmeshChannel) {
+	n.Lock()
+	defer n.Unlock()
+
+	key := meta{
+		name:      ns.Metadata.Name,
+		namespace: ns.Metadata.Namespace,
+	}
+	if _, ok := n.networkServiceChannel[key]; !ok {
+		// Not in the store, adding it.
+		n.networkServiceChannel[key] = ns
+	}
+}
+
+// Delete method deletes removed NetworkServiceChannel object from the store.
+func (n *networkServiceChannelsStore) Delete(key meta) {
+	n.Lock()
+	defer n.Unlock()
+
+	if _, ok := n.networkServiceChannel[key]; ok {
+		delete(n.networkServiceChannel, key)
+	}
+}
+
+// List method lists all known NetworkServiceChannel objects.
+func (n *networkServiceChannelsStore) List() []*netmesh.NetworkService_NetmeshChannel {
+	n.Lock()
+	defer n.Unlock()
+	networkServiceChannels := []*netmesh.NetworkService_NetmeshChannel{}
+	for _, ns := range n.networkServiceChannel {
+		networkServiceChannels = append(networkServiceChannels, ns)
+	}
+
+	return networkServiceChannels
+}
