b8fde6e8bab859fb1d2e2fcd30b45989bd1a179d
Ian Wells <iawells@cisco.com>
2018-07-05 19:41:47 -0700

    Correct example conf/sample scripts
    Both shell scripts were sensitive to the directory in which they were
    run - now they aren't - and the setup script would fail when the
    service didn't start, which - based on the MINIKUBE directions - it
    wouldn't.
    
    Signed-Off-By: Ian Wells <iawells@cisco.com>

--- conf/sample/setup.sh
+++ conf/sample/setup.sh
@@ -4,20 +4,41 @@
 #
 # NOTE: This assumes minikube for now during the label step.
 
-# First, label the nodes
+here="$(dirname "$0")"
+
+# First, label the nodes.  These are the nodes on which the service will run.
 kubectl label --overwrite nodes minikube app=networkservice-node
 
-# Now create the daemonset
-kubectl create -f conf/sample/networkservice-daemonset.yaml
+# Now create the daemonset.  This will run the containers.
+# It will also create the service types.
+kubectl create -f "$here/networkservice-daemonset.yaml"
+
+# Creating the daemonset creates the CRDs for the NSM-specific resources.
+# This can take a little time.
+for f in $(seq 1 20); do
+    sleep 3 # 60s all told, plus command time
+    if kubectl get crd networkservices.networkservicemesh.io >& /dev/null; then
+        ready=1
+        break
+    fi
+done
+
+if [ -z "$ready" ] ; then
+    echo "DaemonSet did not come up."
+    echo "One common reason is that the NSM container image is not in"
+    echo "the registry that your k8s is using.  Please ensure it has"
+    echo "been uploaded there."
+    exit 1
+fi
 
 # Create the channel
-kubectl create -f conf/sample/networkservice-channel.yaml
+kubectl create -f "$here/networkservice-channel.yaml"
 
 # Create the endpoints
-kubectl create -f conf/sample/networkservice-endpoint.yaml
+kubectl create -f "$here/networkservice-endpoint.yaml"
 
 # Finally, create the network service
-kubectl create -f conf/sample/networkservice.yaml
+kubectl create -f "$here/networkservice.yaml"
 
 # Dump some info
 kubectl get pods,crd,NetworkService,NetworkServiceChannel,NetworkServiceEndpoint
--- conf/sample/teardown.sh
+++ conf/sample/teardown.sh
@@ -3,14 +3,16 @@
 # Sample script for deleting Network Service Mesh CRDs.
 #
 
+here="$(dirname "$0")"
+
 # First, delete the network service
-kubectl delete -f conf/sample/networkservice.yaml
+kubectl delete -f "$here/networkservice.yaml"
 
 # Delete the endpoints
-kubectl delete -f conf/sample/networkservice-endpoint.yaml
+kubectl delete -f "$here/networkservice-endpoint.yaml"
 
 # Delete the channel
-kubectl delete -f conf/sample/networkservice-channel.yaml
+kubectl delete -f "$here/networkservice-channel.yaml"
 
 # Now delete the daemonset
-kubectl delete -f conf/sample/networkservice-daemonset.yaml
+kubectl delete -f "$here/networkservice-daemonset.yaml"
