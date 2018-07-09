17cabe0869711d0599af56b800914675347c4fce
Kyle Mestery <mestery@mestery.com>
2018-07-09 15:58:38 -0500

    Update bounding directory for code generation
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- scripts/update-codegen.sh
+++ scripts/update-codegen.sh
@@ -20,7 +20,7 @@ echo "Generating other deepcopy funcs"
 "${GOPATH}"/bin/deepcopy-gen \
   --input-dirs ./netmesh/model/netmesh --input-dirs ./pkg/nsm/apis/common \
   --go-header-file "${SCRIPT_ROOT}/conf/boilerplate.txt" \
-  --bounding-dirs ./netmesh/model/netmesh --bounding-dirs ./pkg/nsm/apis/common \
+  --bounding-dirs ./pkg/nsm/apis/netmesh --bounding-dirs ./pkg/nsm/apis/common \
   -O zz_generated.deepcopy \
   -o "${GOPATH}/src"
 
