659e1683e1cd94dd634c42a7d55d3fa5d42cf7bb
Ed Warnicke <hagbard@gmail.com>
2018-07-16 12:26:28 -0500

    Add CheckDeps, InitDeps, CloseDeps helper (#170)
    * Add CheckDeps, InitDeps, CloseDeps helpers
    
    CheckDeps - check for common errors in plugin Deps
    
    The Deps pattern in Plugins is a powerful way to allow
    users to wire in alternate implementations for things the Plugin
    uses.  In addition to the flexibility it offers, it also makes
    it much much easier to Mock and test.
    
    Unfortunately, there are several mistakes its easy to make with Deps:
    
    - Making a Deps field private (lowercase) which precludes wiring in
      an alternate implementation
    - Failing to set a Default value for Deps in options.go
    
    CheckDeps checks for all of these mistakes.  It is suitable to
    be run in the NewPlugin(...) in options.go as a final check before
    returning a new Plugin to insure its ready to Go :)
    
    *******
    
    InitDeps - Runs Init() on all Deps implemented with Plugins
    
    Deps should be interfaces, they do not have to be Plugins
    InitDeps loops through all of the fields in Deps, and if they
    are Plugins, it Init()'s them.
    
    CloseDeps - Runs Close() on all Deps implemented with Plugins
    
    Closes all Deps of the given plugin if they themselves
    are idempotent.Plugins
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Fixes in response to review comments
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * Minor requested fix to comments
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    * One more comment to resolve
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

---
+++ utils/helper/deptools/init_test.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deptools_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ligato/networkservicemesh/utils/helper/deptools"
+	. "github.com/onsi/gomega"
+)
+
+func TestInit(t *testing.T) {
+	RegisterTestingT(t)
+	p := &PluginWithDeps{
+		Deps: NonOptionalDeps{
+			One: &Plugin{},
+		},
+	}
+	Expect(deptools.Init(p)).To(Succeed())
+	Expect(p.Deps.One.Running()).To(BeTrue())
+}
+
+func TestDepsUnSetDeps(t *testing.T) {
+	RegisterTestingT(t)
+	p := &PluginWithDeps{}
+	Expect(deptools.Init(p)).ToNot(Succeed())
+}
+
+func TestInitNoDeps(t *testing.T) {
+	RegisterTestingT(t)
+	p := &Plugin{}
+	Expect(deptools.Init(p)).To(Succeed())
+}
+
+func TestInitNonPluginDep(t *testing.T) {
+	RegisterTestingT(t)
+	p := &PluginWithDeps{
+		Deps: NonOptionalDeps{
+			One: &Runable{},
+		},
+	}
+	Expect(deptools.Init(p)).To(Succeed())
+	Expect(p.Deps.One.Running()).To(BeFalse())
+}
+
+type PluginErrorOnInit struct {
+	Plugin
+}
+
+func (*PluginErrorOnInit) Init() error { return fmt.Errorf("PluginErrorOnInit always fails on Init") }
+
+func TestInitErrorOnInit(t *testing.T) {
+	RegisterTestingT(t)
+	p := &PluginWithDeps{
+		Deps: NonOptionalDeps{
+			One: &PluginErrorOnInit{},
+		},
+	}
+	Expect(deptools.Init(p)).ToNot(Succeed())
+	Expect(p.Deps.One.Running()).To(BeFalse())
+}
