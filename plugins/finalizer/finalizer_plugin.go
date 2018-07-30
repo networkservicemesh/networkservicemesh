0ae6b1b1e994ace993f632cb7d7fdc5c4f8c0d1a
Serguei Bezverkhi <sbezverk@cisco.com>
2018-07-30 14:20:21 -0400

    Dataplane cleanup part 3 (#209)
    * Completing dataplane cleanup
    
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>
    
    * Addressing comment
    
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- plugins/finalizer/finalizer_plugin.go
+++ plugins/finalizer/finalizer_plugin.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// //go:generate protoc -I ./model/pod --go_out=plugins=grpc:./model/pod ./model/pod/pod.proto
-
 package finalizer
 
 import (
