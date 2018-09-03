9b42b410a43cef506245ab293b677de2be83ed6d
Serguei Bezverkhi <sbezverk@cisco.com>
2018-09-03 00:06:07 -0400

    Addressing comments
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- controllers/sriov-controller/dpapi-controller.go
+++ controllers/sriov-controller/dpapi-controller.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -20,6 +34,7 @@ const (
 	serverBasePath          = pluginapi.DevicePluginPath
 	updateChannelBufferSize = 10
 	containerConfigFilePath = "/var/lib/networkservicemesh"
+	nsmVFsPrefix            = "NSM_VFS_"
 )
 
 type registrationState int
@@ -267,7 +282,7 @@ func (s *serviceInstanceController) Allocate(ctx context.Context, reqs *pluginap
 	if strings.HasPrefix(networkServiceName, "sriov_") {
 		networkServiceName = strings.Split(networkServiceName, "sriov_")[1]
 	}
-	key := "NSM_VFS_" + networkServiceName
+	key := nsmVFsPrefix + networkServiceName
 	// Creating config file which will be passed to the POD. The config file name is composed
 	// from Network Service name + vfio group ID taken from the first device in Allocate Request.
 	// Example: If Network Service is vlan10 and AllocateRequest has /dev/vfio/67, then the config
