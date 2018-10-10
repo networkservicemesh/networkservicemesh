6115d994c37a4aab566ab86a5e9a80ff6458c85b
Serguei Bezverkhi <sbezverk@cisco.com>
2018-10-10 17:16:19 -0400

    Adding dataplane methods
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/nsm/apis/netmesh/netmesh.pb.go
+++ pkg/nsm/apis/netmesh/netmesh.pb.go
@@ -19,7 +19,6 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-// +k8s:openapi-gen=true
 type NetworkServiceEndpoint struct {
 	NetworkServiceName   string                   `protobuf:"bytes,1,opt,name=network_service_name,json=networkServiceName,proto3" json:"network_service_name,omitempty"`
 	NetworkServiceHost   string                   `protobuf:"bytes,2,opt,name=network_service_host,json=networkServiceHost,proto3" json:"network_service_host,omitempty"`
@@ -90,7 +89,6 @@ func (m *NetworkServiceEndpoint) GetLocalMechanisms() []*common.LocalMechanism {
 	return nil
 }
 
-// +k8s:openapi-gen=true
 type NetworkService struct {
 	NetworkServiceName   string   `protobuf:"bytes,1,opt,name=network_service_name,json=networkServiceName,proto3" json:"network_service_name,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
