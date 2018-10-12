fe032f1520f5090677d099352af64027ff792a0b
Kyle Mestery <mestery@mestery.com>
2018-10-12 08:56:30 -0500

    Copy images from outer to inner Docker
    We're using kubeadm-dind-cluster now to run CI tests in travis-ci. This
    means we first build the Docker images in the outer Docker, and then
    spinup a Kubernetes cluster in the outer Docker, with it's own inner
    Docker.
    
    This PR ensures we're copying the images from the outer Docker into the
    inner Docker of each Kubernetes node so we run tests against the freshly
    built images rather than the ones from Docker Hub.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- scripts/cluster-up-dind.sh
+++ scripts/cluster-up-dind.sh
@@ -29,7 +29,12 @@ rm -f ${DIND_CLUSTER_SH}
 wget ${DIND_URL}
 chmod +x ${DIND_CLUSTER_SH}
 ./${DIND_CLUSTER_SH} up
+
 export PATH="${HOME}/.kubeadm-dind-cluster:${PATH}"
 # Wait for Kubernetes to be up and ready
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
+
+# Load the freshly built Docker images from the outer Docker into the inner Docker
+./scripts/ci-dind-images.sh
+
 # vim: sw=4 ts=4 et si
