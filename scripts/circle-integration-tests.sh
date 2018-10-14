38eed36915fed1cd0e2ba4f30a3a1e1a157de5b0
Andrey Platov <andrey@xored.com>
2018-10-14 12:14:21 +0200

    run integration tests
    Signed-off-by: Andrey Platov <andrey@xored.com>

---
+++ scripts/circle-integration-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright (c) 2016-2017 Bitnami
+# Copyright (c) 2018 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xe
+
+. scripts/integration-tests.sh
+
+# run_tests returns an error on failure
+run_tests
+exit_code=$?
+if [ "${exit_code}" == "0" ] ; then
+    echo "TESTS: PASS"
+else
+    error_collection
+    echo "TESTS: FAIL"
+fi
+
+set +x
+exit ${exit_code}
+
+# vim: sw=4 ts=4 et si
