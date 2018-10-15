7accdf159bbcd1cac31011c0e15be50b24d2d083
Ed Warnicke <hagbard@gmail.com>
2018-10-15 15:11:11 -0500

    Switch to using MonitorMechanism to get Mechanisms (#340)
    This is built on PR #336, #337, #338
    
    Only the last commit is from this PR.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- plugins/dataplaneregistrar/dataplaneregistrar_reg.go
+++ plugins/dataplaneregistrar/dataplaneregistrar_reg.go
@@ -115,10 +115,8 @@ func (r *dataplaneRegistrarServer) RequestDataplaneRegistration(ctx context.Cont
 	}
 	// Instantiating dataplane object with parameters from the request and creating a new object in the Object store
 	dataplane := &objectstore.Dataplane{
-		RegisteredName:   req.DataplaneName,
-		SocketLocation:   req.DataplaneSocket,
-		RemoteMechanisms: req.RemoteMechanisms,
-		LocalMechanisms:  req.LocalMechanisms,
+		RegisteredName: req.DataplaneName,
+		SocketLocation: req.DataplaneSocket,
 	}
 	r.objectStore.ObjectCreated(dataplane)
 	// Starting per dataplane go routine which will open grpc client connection on dataplane advertised socket
