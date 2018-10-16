45b175f67f589130f1d4614d614926a5c573b4a7
Ed Warnicke <hagbard@gmail.com>
2018-10-16 08:45:45 -0500

    And run CR update script for commons.
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- pkg/nsm/apis/netmesh/netmesh.pb.go
+++ pkg/nsm/apis/netmesh/netmesh.pb.go
@@ -19,6 +19,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
+// +k8s:openapi-gen=true
 type NetworkServiceEndpoint struct {
 	NetworkServiceName   string                   `protobuf:"bytes,1,opt,name=network_service_name,json=networkServiceName" json:"network_service_name,omitempty"`
 	NetworkServiceHost   string                   `protobuf:"bytes,2,opt,name=network_service_host,json=networkServiceHost" json:"network_service_host,omitempty"`
@@ -89,6 +90,7 @@ func (m *NetworkServiceEndpoint) GetLocalMechanisms() []*common.LocalMechanism {
 	return nil
 }
 
+// +k8s:openapi-gen=true
 type NetworkService struct {
 	NetworkServiceName   string   `protobuf:"bytes,1,opt,name=network_service_name,json=networkServiceName" json:"network_service_name,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
