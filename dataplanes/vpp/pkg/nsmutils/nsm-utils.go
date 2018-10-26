be3759982c587d50059e80aa2908ba42a9e44106
lobkovilya <lobkovilya@yandex.ru>
2018-10-26 20:26:40 +0700

    Extend Mechanism interface with Validation method (#385)
    Signed-off-by: lobkovilya <lobkovilya@yandex.ru>

--- dataplanes/vpp/pkg/nsmutils/nsm-utils.go
+++ dataplanes/vpp/pkg/nsmutils/nsm-utils.go
@@ -30,40 +30,27 @@ const (
 	NSMkeyIPv4PrefixLength = "ipv4prefixlength"
 )
 
-// keys is a map of all keys which are supported in the connection Parameters map
-type keyProperties struct {
-	mandatory bool
-	validator func(value string) error
+// Keys is a map of all Keys which are supported in the connection Parameters map
+type KeyProperties struct {
+	Mandatory bool
+	Validator func(value string) error
 }
-type keys map[string]keyProperties
+type Keys map[string]KeyProperties
 
 // ValidateParameters checks all required amd optional parameters
 // and attempts to check them
-func ValidateParameters(parameters map[string]string) error {
-
-	keyList := keys{
-		NSMkeyNamespace: keyProperties{
-			mandatory: true,
-			validator: namespace},
-		NSMkeyIPv4: keyProperties{
-			mandatory: false,
-			validator: ipv4},
-		NSMkeyIPv4PrefixLength: keyProperties{
-			mandatory: false,
-			validator: ipv4prefixlength},
-	}
-
-	// Check for any Unknown keys if found return error
+func ValidateParameters(parameters map[string]string, keyList Keys) error {
+	// Check for any Unknown Keys if found return error
 	for key := range parameters {
 		if _, ok := keyList[key]; !ok {
-			// Found a key in parameters which is not in the list of supported keys
+			// Found a key in parameters which is not in the list of supported Keys
 			return fmt.Errorf("found unknown key %s in the parameters", key)
 		}
 	}
 
 	// Check mandatory parameters first
 	for key, properties := range keyList {
-		if properties.mandatory {
+		if properties.Mandatory {
 			if _, ok := parameters[key]; !ok {
 				return fmt.Errorf("missing mandatory %s key", key)
 			}
@@ -72,28 +59,23 @@ func ValidateParameters(parameters map[string]string) error {
 
 	// Check sanity for all passed parameters
 	for key, value := range parameters {
-		if err := keyList[key].validator(value); err != nil {
+		if err := keyList[key].Validator(value); err != nil {
 			return fmt.Errorf("key %s has invalid value %s, error: %+v", key, value, err)
 		}
 	}
-	// Check presence of both ipv4 address and prefix length
-	_, v1 := parameters[NSMkeyIPv4]
-	_, v2 := parameters[NSMkeyIPv4PrefixLength]
-	if v1 != v2 {
-		return fmt.Errorf("both parameter \"ipv4\" and \"ipv4prefixlength\" must either present or missing")
-	}
+
 	return nil
 }
 
-// keys validator functions, for each new keys there should be a validator function.
-func namespace(value string) error {
+// Keys validator functions, for each new Keys there should be a validator function.
+func Namespace(value string) error {
 	if !strings.HasPrefix(value, "pid:") {
 		return fmt.Errorf("malformed namespace %s, must start with \"pid:\" following by the process id of a container", value)
 	}
 	return nil
 }
 
-func ipv4(value string) error {
+func Ipv4(value string) error {
 	ip := net.ParseIP(value)
 	if ip == nil {
 		return fmt.Errorf("invalid value %s of ipv4 parameter", value)
@@ -103,7 +85,7 @@ func ipv4(value string) error {
 	return nil
 }
 
-func ipv4prefixlength(value string) error {
+func Ipv4prefixlength(value string) error {
 	prefixLength, err := strconv.Atoi(value)
 	if err != nil {
 		return err
