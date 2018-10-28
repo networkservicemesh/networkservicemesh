3692073c8f60fae9bc025498a7f74c2b2df775fc
Andrey Platov <andrey@xored.com>
2018-10-28 19:40:31 +0100

    real build and run script
    Signed-off-by: Andrey Platov <andrey@xored.com>

--- controlplane/scripts/build-and-run.sh
+++ controlplane/scripts/build-and-run.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
 docker build -t nsmd/nsmd -f build/Dockerfile.nsmd ..
-docker run -it -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nsmd
 docker build -t nsmd/nse -f build/Dockerfile.nse ..
-docker run -it -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nse
 docker build -t nsmd/nsc -f build/Dockerfile.nsc ..
-docker run -it -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nsc
-docker run --network=host --privileged=true --volume=/var/run:/var/run --volume=/var/lib:/var/lib --volume=/lib/modules:/lib/modules --ipc=host --pid=host -it networkservicemesh/vpp:latest
-docker run --network=host --privileged=true --volume=/var/run:/var/run --volume=/var/lib:/var/lib --volume=/lib/modules:/lib/modules --volume=/var/lib/networkservicemesh:/var/lib/networkservicemesh/ --ipc=host --pid=host -it networkservicemesh/vpp-daemon:latest
+
+echo "Starting nsmd..."
+docker run -d -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nsmd >containers.txt
+echo "Starting nse..."
+docker run -d -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nse >>containers.txt
+echo "Starting vpp..."
+docker run --network=host --privileged=true --volume=/var/run:/var/run --volume=/var/lib:/var/lib --volume=/lib/modules:/lib/modules --ipc=host --pid=host -d networkservicemesh/vpp >>containers.txt
+echo "Starting vpp-daemon..."
+docker run --network=host --privileged=true --volume=/var/run:/var/run --volume=/var/lib:/var/lib --volume=/lib/modules:/lib/modules --volume=/var/lib/networkservicemesh:/var/lib/networkservicemesh/ --ipc=host --pid=host -d networkservicemesh/vpp-daemon >>containers.txt
+
+echo "vpp-daemon takes time (unnecessarily) to register with nsmd, so let it connect. waiting 60 seconds..."
+sleep 60
+
+echo "Running nsm client..."
+docker run -d -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nsc
+
+echo "Showing nsmd logs..."
+docker logs $(sed '1q;d' containers.txt)
+
+echo "Showing nse logs..."
+docker logs $(sed '2q;d' containers.txt)
+
+echo "Showing vpp-daemon logs..."
+docker logs $(sed '4q;d' containers.txt)
+
+echo "Starting nsc..."
+docker run -d -v "/var/lib/networkservicemesh:/var/lib/networkservicemesh" nsmd/nsc >>containers.txt
+
+echo "Showing nsc logs..."
+docker logs $(sed '5q;d' containers.txt)
+
+echo "Showing nse interfaces..."
+docker exec $(sed '2q;d' containers.txt) ifconfig -a
+
+echo "Showing nsc interfaces..."
+docker exec $(sed '5q;d' containers.txt) ifconfig -a
+
+echo "Ping nse from nsc interfaces..."
+docker exec $(sed '5q;d' containers.txt) ping 2.2.2.3
+
+echo "Kill and remove containers..."
+docker kill $(<containers.txt)
+docker rm $(<containers.txt)
