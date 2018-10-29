cda46b3b1ff5f6d7911062430d1b18d5d7dff4a7
Andrey Platov <andrey@xored.com>
2018-10-29 03:14:21 +0100

    make any-any cross-connects (#395)
    Signed-off-by: Andrey Platov <andrey@xored.com>
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- dataplanes/vpp/pkg/nsmvpp/nsm_vpp_noimpl.go
+++ dataplanes/vpp/pkg/nsmvpp/nsm_vpp_noimpl.go
@@ -7,19 +7,19 @@ import (
 	"github.com/ligato/networkservicemesh/pkg/nsm/apis/common"
 )
 
-type UnimplementedMechanism struct {
+type unimplementedMechanism struct {
 	Type common.LocalMechanismType
 }
 
 // CreateLocalConnect return error for unimplemented mechanism
-func (m UnimplementedMechanism) CreateLocalConnect(apiCh govppapi.Channel, srcParameters, dstParameters map[string]string) (string, error) {
-	return "", fmt.Errorf("%s mechanism not implemented", common.LocalMechanismType_name[int32(m.Type)])
+func (m unimplementedMechanism) createInterface(apiCh govppapi.Channel, parameters map[string]string) (uint32, error) {
+	return 0, fmt.Errorf("%s mechanism not implemented", common.LocalMechanismType_name[int32(m.Type)])
 }
 
-func (m UnimplementedMechanism) DeleteLocalConnect(apiCh govppapi.Channel, connID string) error {
+func (m unimplementedMechanism) deleteInterface(apiCh govppapi.Channel, intf uint32) error {
 	return fmt.Errorf("%s mechanism not implemented", common.LocalMechanismType_name[int32(m.Type)])
 }
 
-func (m UnimplementedMechanism) Validate(parameters map[string]string) error {
+func (m unimplementedMechanism) validate(parameters map[string]string) error {
 	return nil
 }
