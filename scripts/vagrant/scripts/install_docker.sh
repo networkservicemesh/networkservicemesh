a1cb91ae005bf0535033acc811660f7cc15f69f1
mathieu rohon <mathieu.rohon@gmail.com>
2018-11-08 23:31:08 +0100

    Vagrant: Add custom docker provisioner (#438)
    Force installation of docker-ce 18.06, the latest version
    supported by kubeadm.
    
    Signed-off-by: Mathieu Rohon <mathieu.rohon@orange.com>

---
+++ scripts/vagrant/scripts/install_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copy the vagrant docker provisioner, but force the docker version to 18.06
+# see:
+# - https://github.com/hashicorp/vagrant/blob/master/plugins/provisioners/docker/cap/debian/docker_install.rb
+
+apt-get update -qq -y
+apt-get install -qq -y --force-yes curl apt-transport-https
+apt-get purge -qq -y lxc-docker* || true
+curl -sSL https://get.docker.com/ | VERSION=18.06 sh
