4db0063d07d249ef57fbb941ef73e275a2edf752
Frederick F. Kautz IV <fkautz@redhat.com>
2018-11-11 14:31:26 -0800

    Integration tests now point to new code
    Signed-off-by: Frederick F. Kautz IV <fkautz@redhat.com>

--- .kubeadm.mk
+++ .kubeadm.mk
@@ -18,6 +18,7 @@ kubeadm-start: kubeadm-reset
 	@mkdir -p "$(HOME)/.kube"
 	@sudo cp -i /etc/kubernetes/admin.conf "$(HOME)/.kube/config"
 	@sudo chown "$(id -u):$(id -g)" "$(HOME)/.kube/config"
+	@kubectl taint nodes --all node-role.kubernetes.io/master-
 	@kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 	@kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
