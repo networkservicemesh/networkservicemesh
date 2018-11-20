840353795d2dd1426b534037b9e68711900967d3
yf-yang <36890796+yf-yang@users.noreply.github.com>
2018-11-20 14:10:57 -0800

    Fake monitors for skydive development (#520)
    * fake monitor client and server
    
    Signed-off-by: Mathieu Rohon <mathieu.rohon@orange.com>
    
    * move fake monitor for skydive development to examples/skydive
    
    Signed-off-by: yf-yang <directoryyf@gmail.com>

---
+++ .monitor.mk
@@ -0,0 +1,10 @@
+.PHONY: mon
+mon: mon-server mon-client
+
+.PHONY: mon-server
+mon-server:
+	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-server ./examples/skydive/monitor-server.go
+
+.PHONY: mon-client
+mon-client:
+	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-client ./examples/skydive/monitor-client.go
