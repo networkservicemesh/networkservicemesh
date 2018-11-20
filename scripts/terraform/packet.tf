615b8150b8c336504392dbd12b1cf6d7ebc3cfaf
Frederick F. Kautz IV <fkautz@pseudocode.cc>
2018-11-19 19:22:57 -0800

    Migrate CI to kubeadm (#500)
    * Migrate CI to kubeadm
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@redhat.com>
    
    * Fix destroy-cluster missing a variable
    
    Signed-off-by: Frederick F. Kautz IV <fkautz@redhat.com>

---
+++ scripts/terraform/packet.tf
@@ -0,0 +1,28 @@
+variable "auth_token" {}
+variable "project_id" {}
+variable "public_key" {}
+variable "master_hostname" {}
+variable "public_key_name" {}
+
+provider "packet"{
+    auth_token = "${var.auth_token}"
+}
+
+resource "packet_ssh_key" "key" {
+    name = "${var.public_key_name}"
+    public_key = "${file(var.public_key)}"
+}
+
+resource "packet_device" "master"{
+    hostname = "${var.master_hostname}"
+    facility = "sjc1"
+    plan = "baremetal_0"
+    operating_system = "ubuntu_16_04"
+    billing_cycle = "hourly"
+    project_id = "${var.project_id}"
+    depends_on = ["packet_ssh_key.key"]
+}
+
+output "master.public_ip" {
+    value = "${packet_device.master.network.0.address}"
+}
