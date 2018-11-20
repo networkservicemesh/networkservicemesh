2a34f17d5156620610abce6bde8559a26699a465
yf-yang <directoryyf@gmail.com>
2018-11-20 12:10:13 -0800

    move fake monitor for skydive development to examples/skydive
    Signed-off-by: yf-yang <directoryyf@gmail.com>

--- .monitor.mk
+++ .monitor.mk
@@ -3,8 +3,8 @@ mon: mon-server mon-client
 
 .PHONY: mon-server
 mon-server:
-	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-server ./controlplane/cmd/monitor-server.go
+	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-server ./examples/skydive/monitor-server.go
 
 .PHONY: mon-client
 mon-client:
-	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-client ./controlplane/cmd/monitor-client.go
+	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-client ./examples/skydive/monitor-client.go
