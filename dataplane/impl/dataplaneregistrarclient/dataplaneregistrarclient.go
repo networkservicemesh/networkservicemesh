7097dddc0e5a5013bee9eb604859cfae0d4ecf20
Ed Warnicke <hagbard@gmail.com>
2018-11-21 20:03:23 -0600

    Get much more aggressive on the retry interval
    30 seconds is to long, trying 100ms
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- dataplane/impl/dataplaneregistrarclient/dataplaneregistrarclient.go
+++ dataplane/impl/dataplaneregistrarclient/dataplaneregistrarclient.go
@@ -51,7 +51,7 @@ type OnDisConnectFunc func() error
 func (dr *dataplaneRegistration) register(ctx context.Context) {
 	logrus.Info("Registering with NetworkServiceManager")
 	logrus.Infof("Retry interval: %s", dr.registrar.registrationRetryInterval)
-	tools.WaitForPortAvailable(context.Background(), "unix", dr.registrar.registrarSocket, dr.registrar.registrationRetryInterval)
+	tools.WaitForPortAvailable(context.Background(), "unix", dr.registrar.registrarSocket, 100*time.Millisecond)
 	ticker := time.NewTicker(dr.registrar.registrationRetryInterval)
 	for ; true; <-ticker.C {
 		select {
