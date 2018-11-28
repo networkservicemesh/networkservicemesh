7ceb08e117d7eb1fbcd83beaf84173bb3beaf48a
Nikolay Nikolaev <nnikolay@vmware.com>
2018-11-28 19:03:45 +0200

    controlplane: implement a simple Ednpoint selection. Implements 534 (#539)
    * controlplane: expand Selector.SelectEndpoint to get nsLabels
    
    The service request's Connection contains the NS name and the desired
    labels. Passs these to the Selector so it can use them during the NSE
    selection phase.
    The current RoundRobin slector, just omits them though.
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * controlplane: add MatchSelector
    
    The MatchSelector implements a simple label matching NSE selection.
    The mmethod of operation is:
     1) find a Match that has has all its SourceSelector labels in the reqeusted service
     2) filter the list of the NSEs against the Match.Routes by matching NSE.Labels
        agianst their DestinationSelector.
     3) invoke the RoundRobin selector on the list of candidate NSEs
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * controlplane: switch model to use MatchSelector
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * controlplane: Slector interface gets connection.Connection argument
    
    Pass the request Connection argument down to the Selector implementation for more
    flexibility.
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- controlplane/pkg/selector/selector.go
+++ controlplane/pkg/selector/selector.go
@@ -1,7 +1,10 @@
 package selector
 
-import "github.com/ligato/networkservicemesh/controlplane/pkg/apis/registry"
+import (
+	"github.com/ligato/networkservicemesh/controlplane/pkg/apis/local/connection"
+	"github.com/ligato/networkservicemesh/controlplane/pkg/apis/registry"
+)
 
 type Selector interface {
-	SelectEndpoint(ns *registry.NetworkService, networkServiceEndpoints []*registry.NetworkServiceEndpoint) *registry.NetworkServiceEndpoint
+	SelectEndpoint(requestConnection *connection.Connection, ns *registry.NetworkService, networkServiceEndpoints []*registry.NetworkServiceEndpoint) *registry.NetworkServiceEndpoint
 }
