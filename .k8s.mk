d14323ea8af8d8722b7ddcdf7304abea484bef97
Frederick F. Kautz IV <fkautz@pseudocode.cc>
2018-12-06 12:09:26 -0800

    Remove kubeadm provider (#592)
    Signed-off-by: Frederick F. Kautz IV <fkautz@redhat.com>

--- .k8s.mk
+++ .k8s.mk
@@ -27,7 +27,6 @@ ifeq ($(CLUSTER_RULES_PREFIX),)
 CLUSTER_RULES_PREFIX := vagrant
 endif
 include .vagrant.mk
-include .kubeadm.mk
 include .packet.mk
 
 # .null.mk allows you to skip the vagrant machinery with:
