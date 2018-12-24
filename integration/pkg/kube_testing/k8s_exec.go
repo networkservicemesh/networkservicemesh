c8d6933846334de55fa6f893f308dba9d18af65d
Andrey Sobolev <haiodo@gmail.com>
2018-12-25 01:50:36 +0700

    Initial part of Integeration tests on pure GO with usage of Kubernetes API directly.
    * Local NSC -> NSMD+Dataplane <- NSE
    * NSMD deployment test on different nodes.
    * NSMD-k8s registry test.
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

---
+++ integration/pkg/kube_testing/k8s_exec.go
@@ -0,0 +1,59 @@
+package kube_testing
+
+import (
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
+	"strings"
+)
+
+type Writer struct {
+	Str string
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+	str := string(p)
+	if len(str) > 0 {
+		w.Str += str
+	}
+	return len(str), nil
+}
+
+func (o *K8s) Exec(pod *v1.Pod, container string, command ...string) (string, string, error) {
+	execRequest := o.clientset.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(pod.Name).
+		Namespace(pod.Namespace).
+		SubResource("exec").
+		VersionedParams(&v1.PodExecOptions{
+			Container: container,
+			Command:   command,
+			Stdin:     true,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       false,
+		}, scheme.ParameterCodec)
+
+	if len(container) > 0 {
+		execRequest = execRequest.Param("container", container)
+	}
+
+	exec, err := remotecommand.NewSPDYExecutor(o.config, "POST", execRequest.URL())
+	if err != nil {
+		return "", "", err
+	}
+
+	stdIn := strings.NewReader("")
+	stdOut := new(Writer)
+	stdErr := new(Writer)
+
+	options := remotecommand.StreamOptions{
+		Stdin:  stdIn,
+		Stdout: stdOut,
+		Stderr: stdErr,
+		Tty:    false,
+	}
+	err = exec.Stream(options)
+
+	return stdOut.Str, stdErr.Str, err
+}
---
+++ integration/pkg/kube_testing/pods/alpine.go
@@ -0,0 +1,38 @@
+package pods
+
+import (
+	"k8s.io/api/core/v1"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func AlpinePod(name string, node *v1.Node) *v1.Pod {
+	ht := new(v1.HostPathType)
+	*ht = v1.HostPathDirectoryOrCreate
+
+	pod := &v1.Pod{
+		ObjectMeta: v12.ObjectMeta{
+			Name: name,
+		},
+		TypeMeta: v12.TypeMeta{
+			Kind: "Deployment",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:            "alpine-img",
+					Image:           "alpine:latest",
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Command: []string{
+						"tail", "-f", "/dev/null",
+					},
+				},
+			},
+		},
+	}
+	if node != nil {
+		pod.Spec.NodeSelector = map[string]string{
+			"kubernetes.io/hostname": node.Labels["kubernetes.io/hostname"],
+		}
+	}
+	return pod
+}
