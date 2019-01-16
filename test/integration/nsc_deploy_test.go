ee47686cc0aa204db2a96df8b1f8458bdb86c085
Andrey Sobolev <haiodo@users.noreply.github.com>
2019-01-16 15:01:55 +0700

    Add timeouts for integration test pod deployments (#655)
     * Have timeouts for integration test pod deployments
     * Simplify code a bit and add one more test for integration testing framework
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

---
+++ test/integration/nsc_deploy_test.go
@@ -0,0 +1,42 @@
+package nsmd_integration_tests
+
+import (
+	"github.com/ligato/networkservicemesh/test/kube_testing"
+	"github.com/ligato/networkservicemesh/test/kube_testing/pods"
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+)
+
+func TestDeployPodIntoInvalidEnv(t *testing.T) {
+	RegisterTestingT(t)
+
+	if testing.Short() {
+		t.Skip("Skip, please run without -short")
+		return
+	}
+
+	logrus.Print("Running NSMD Deploy test")
+
+	k8s, err := kube_testing.NewK8s()
+	defer k8s.Cleanup()
+
+	Expect(err).To(BeNil())
+
+	k8s.Prepare("nsmd", "nsc") // Be sure where is no NSMD
+	nodes := k8s.GetNodesWait(1, time.Second*60)
+
+	if len(nodes) < 1 {
+		logrus.Printf("At least two kubernetes nodes are required for this test")
+		Expect(len(nodes)).To(Equal(1))
+		return
+	}
+
+	nsmdPodNode1, err := k8s.CreatePodsRaw(10*time.Second, false, pods.NSCPod("nsc1", &nodes[0], map[string]string{}))
+	Expect(len(nsmdPodNode1)).To(Equal(1))
+
+	k8s.DeletePods("default", nsmdPodNode1...)
+
+}
