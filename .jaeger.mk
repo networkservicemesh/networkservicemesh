837b25837b5c17ee836d7271093ff9ad515fbd35
Ed Warnicke <hagbard@gmail.com>
2019-01-18 21:57:54 -0600

    Test commit to check CI
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- .jaeger.mk
+++ .jaeger.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 .PHONY: jaeger-port-forward
 jaeger-port-forward:
 	@kubectl port-forward svc/jaeger 16686:16686
