0abaa56c1587a02b4df0ea94db70b6b25e36be63
Ilya Lobkov <lobkovilya@yandex.ru>
2019-01-22 18:51:43 +0700

    Issue #664: Integration tests for CrossConnectMonitor (#689)
    * Create tests to cover behavior related to cross-connect monitor
    
    Signed-off-by: Ilya Lobkov <lobkovilya@yandex.ru>
    
    * Fix imports
    
    Signed-off-by: Ilya Lobkov <lobkovilya@yandex.ru>
    
    * Add testing.Short for CrossConnectMonitor tests
    
    Signed-off-by: Ilya Lobkov <lobkovilya@yandex.ru>

--- test/integration/death_handling_test.go
+++ test/integration/death_handling_test.go
@@ -1,9 +1,8 @@
 package nsmd_integration_tests
 
 import (
-	"fmt"
+	"github.com/networkservicemesh/networkservicemesh/test/integration/nsmd_test_utils"
 	"github.com/networkservicemesh/networkservicemesh/test/kube_testing"
-	"github.com/networkservicemesh/networkservicemesh/test/kube_testing/pods"
 	. "github.com/onsi/gomega"
 	"github.com/sirupsen/logrus"
 	"k8s.io/api/core/v1"
@@ -11,60 +10,6 @@ import (
 	"testing"
 	"time"
 )
-
-func deployNsmdAndDataplane(k8s *kube_testing.K8s, node *v1.Node) (nsmd *v1.Pod, dataplane *v1.Pod) {
-	startTime := time.Now()
-
-	nsmdName := fmt.Sprintf("nsmd-%s", node.Name)
-	dataplaneName := fmt.Sprintf("nsmd-dataplane-%s", node.Name)
-	corePods := k8s.CreatePods(pods.NSMDPod(nsmdName, node), pods.VPPDataplanePod(dataplaneName, node))
-	logrus.Printf("Started NSMD/Dataplane: %v on node %s", time.Since(startTime), node.Name)
-	nsmd = corePods[0]
-	dataplane = corePods[1]
-
-	Expect(nsmd.Name).To(Equal(nsmdName))
-	Expect(dataplane.Name).To(Equal(dataplaneName))
-
-	k8s.WaitLogsContains(dataplane, "", "Sending MonitorMechanisms update", defaultTimeout)
-	k8s.WaitLogsContains(nsmd, "nsmd", "Dataplane added", defaultTimeout)
-	k8s.WaitLogsContains(nsmd, "nsmdp", "ListAndWatch was called with", defaultTimeout)
-
-	return
-}
-
-func deployIcmp(k8s *kube_testing.K8s, node *v1.Node) (icmp *v1.Pod) {
-	startTime := time.Now()
-
-	logrus.Infof("Starting ICMP Responder NSE on node: %s", node.Name)
-	icmp = k8s.CreatePod(pods.ICMPResponderPod("icmp-responder-nse1", node,
-		map[string]string{
-			"ADVERTISE_NSE_NAME":   "icmp-responder",
-			"ADVERTISE_NSE_LABELS": "app=icmp-responder",
-			"IP_ADDRESS":           "10.20.1.0/24",
-		},
-	))
-	Expect(icmp.Name).To(Equal("icmp-responder-nse1"))
-
-	k8s.WaitLogsContains(icmp, "", "NSE: channel has been successfully advertised, waiting for connection from NSM...", defaultTimeout)
-
-	logrus.Printf("ICMP Responder started done: %v", time.Since(startTime))
-	return
-}
-
-func deployNsc(k8s *kube_testing.K8s, node *v1.Node) (nsc *v1.Pod) {
-	startTime := time.Now()
-	nsc = k8s.CreatePod(pods.NSCPod("nsc1", node,
-		map[string]string{
-			"OUTGOING_NSC_LABELS": "app=icmp",
-			"OUTGOING_NSC_NAME":   "icmp-responder",
-		},
-	))
-	Expect(nsc.Name).To(Equal("nsc1"))
-
-	k8s.WaitLogsContains(nsc, "nsc", "nsm client: initialization is completed successfully", defaultTimeout)
-	logrus.Printf("NSC started done: %v", time.Since(startTime))
-	return
-}
 
 func TestNscDiesSingleNode(t *testing.T) {
 	RegisterTestingT(t)
@@ -119,21 +64,10 @@ func testDie(t *testing.T, killSrc bool, nodesCount int) {
 	k8s.Prepare("nsmd", "nsc", "nsmd-dataplane", "icmp-responder-nse")
 	logrus.Printf("Cleanup done: %v", time.Since(s1))
 
-	nodes := k8s.GetNodesWait(nodesCount, time.Second*60)
-	Expect(len(nodes) >= nodesCount).To(Equal(true),
-		"At least one kubernetes node are required for this test")
+	nodes := nsmd_test_utils.SetupNodes(k8s, nodesCount)
 
-	nsmds := []*v1.Pod{}
-	dataplanes := []*v1.Pod{}
-
-	for i := 0; i < nodesCount; i++ {
-		nsmd, dataplane := deployNsmdAndDataplane(k8s, &nodes[i])
-		nsmds = append(nsmds, nsmd)
-		dataplanes = append(dataplanes, dataplane)
-	}
-
-	icmp := deployIcmp(k8s, &nodes[nodesCount-1])
-	nsc := deployNsc(k8s, &nodes[0])
+	icmp := nsmd_test_utils.DeployIcmp(k8s, nodes[nodesCount-1].Node)
+	nsc := nsmd_test_utils.DeployNsc(k8s, nodes[0].Node, "nsc1")
 
 	failures := InterceptGomegaFailures(func() {
 		ipResponse, errOut, err := k8s.Exec(nsc, nsc.Spec.Containers[0].Name, "ip", "addr")
@@ -177,16 +111,16 @@ func testDie(t *testing.T, killSrc bool, nodesCount int) {
 	if len(failures) > 0 {
 		logrus.Errorf("Failues: %v", failures)
 		for k := 0; k < nodesCount; k++ {
-			nsmdLogs, _ := k8s.GetLogs(nsmds[k], "nsmd")
+			nsmdLogs, _ := k8s.GetLogs(nodes[k].Nsmd, "nsmd")
 			logrus.Errorf("===================== NSMD %d output since test is failing %v\n=====================", k, nsmdLogs)
 
-			nsmdk8sLogs, _ := k8s.GetLogs(nsmds[k], "nsmd-k8s")
+			nsmdk8sLogs, _ := k8s.GetLogs(nodes[k].Nsmd, "nsmd-k8s")
 			logrus.Errorf("===================== NSMD K8S %d output since test is failing %v\n=====================", k, nsmdk8sLogs)
 
-			nsmdpLogs, _ := k8s.GetLogs(nsmds[k], "nsmdp")
+			nsmdpLogs, _ := k8s.GetLogs(nodes[k].Nsmd, "nsmdp")
 			logrus.Errorf("===================== NSMD K8S %d output since test is failing %v\n=====================", k, nsmdpLogs)
 
-			dataplaneLogs, _ := k8s.GetLogs(dataplanes[k], "")
+			dataplaneLogs, _ := k8s.GetLogs(nodes[k].Dataplane, "")
 			logrus.Errorf("===================== Dataplane %d output since test is failing %v\n=====================", k, dataplaneLogs)
 		}
 
---
+++ test/integration/nsmd_test_utils/test_utils.go
@@ -0,0 +1,90 @@
+package nsmd_test_utils
+
+import (
+	"fmt"
+	"github.com/networkservicemesh/networkservicemesh/test/kube_testing"
+	"github.com/networkservicemesh/networkservicemesh/test/kube_testing/pods"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+	"time"
+)
+
+const defaultTimeout = 60 * time.Second
+
+type NodeConf struct {
+	Nsmd      *v1.Pod
+	Dataplane *v1.Pod
+	Node      *v1.Node
+}
+
+func SetupNodes(k8s *kube_testing.K8s, nodesCount int) []*NodeConf {
+	nodes := k8s.GetNodesWait(nodesCount, defaultTimeout)
+	Expect(len(nodes) >= nodesCount).To(Equal(true),
+		"At least one kubernetes node are required for this test")
+
+	confs := []*NodeConf{}
+	for i := 0; i < nodesCount; i++ {
+		nsmd, dataplane := deployNsmdAndDataplane(k8s, &nodes[i])
+		confs = append(confs, &NodeConf{
+			Nsmd:      nsmd,
+			Dataplane: dataplane,
+			Node:      &nodes[i],
+		})
+	}
+	return confs
+}
+
+func deployNsmdAndDataplane(k8s *kube_testing.K8s, node *v1.Node) (nsmd *v1.Pod, dataplane *v1.Pod) {
+	startTime := time.Now()
+
+	nsmdName := fmt.Sprintf("nsmd-%s", node.Name)
+	dataplaneName := fmt.Sprintf("nsmd-dataplane-%s", node.Name)
+	corePods := k8s.CreatePods(pods.NSMDPod(nsmdName, node), pods.VPPDataplanePod(dataplaneName, node))
+	logrus.Printf("Started NSMD/Dataplane: %v on node %s", time.Since(startTime), node.Name)
+	nsmd = corePods[0]
+	dataplane = corePods[1]
+
+	Expect(nsmd.Name).To(Equal(nsmdName))
+	Expect(dataplane.Name).To(Equal(dataplaneName))
+
+	k8s.WaitLogsContains(dataplane, "", "Sending MonitorMechanisms update", defaultTimeout)
+	k8s.WaitLogsContains(nsmd, "nsmd", "Dataplane added", defaultTimeout)
+	k8s.WaitLogsContains(nsmd, "nsmdp", "ListAndWatch was called with", defaultTimeout)
+
+	return
+}
+
+func DeployIcmp(k8s *kube_testing.K8s, node *v1.Node) (icmp *v1.Pod) {
+	startTime := time.Now()
+
+	logrus.Infof("Starting ICMP Responder NSE on node: %s", node.Name)
+	icmp = k8s.CreatePod(pods.ICMPResponderPod("icmp-responder-nse1", node,
+		map[string]string{
+			"ADVERTISE_NSE_NAME":   "icmp-responder",
+			"ADVERTISE_NSE_LABELS": "app=icmp-responder",
+			"IP_ADDRESS":           "10.20.1.0/24",
+		},
+	))
+	Expect(icmp.Name).To(Equal("icmp-responder-nse1"))
+
+	k8s.WaitLogsContains(icmp, "", "NSE: channel has been successfully advertised, waiting for connection from NSM...", defaultTimeout)
+
+	logrus.Printf("ICMP Responder started done: %v", time.Since(startTime))
+	return
+}
+
+func DeployNsc(k8s *kube_testing.K8s, node *v1.Node, name string) (nsc *v1.Pod) {
+	startTime := time.Now()
+	nsc = k8s.CreatePod(pods.NSCPod(name, node,
+		map[string]string{
+			"OUTGOING_NSC_LABELS": "app=icmp",
+			"OUTGOING_NSC_NAME":   "icmp-responder",
+		},
+	))
+	Expect(nsc.Name).To(Equal(name))
+
+	k8s.WaitLogsContains(nsc, "nsc", "nsm client: initialization is completed successfully", defaultTimeout)
+	logrus.Printf("NSC started done: %v", time.Since(startTime))
+	return
+}
