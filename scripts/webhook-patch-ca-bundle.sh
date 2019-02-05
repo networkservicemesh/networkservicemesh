5f3647ae13cd19af33ac69ac147eb79bc7c6b6ac
Andrey Platov <andrey@xored.com>
2019-02-04 20:10:33 -0800

    making shellcheck happy
    Signed-off-by: Andrey Platov <andrey@xored.com>

--- scripts/webhook-patch-ca-bundle.sh
+++ scripts/webhook-patch-ca-bundle.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ROOT=$(cd $(dirname $0)/../../; pwd)
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-export CA_BUNDLE=$(kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"')
+CA_BUNDLE=$(kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"')
+export CA_BUNDLE
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
