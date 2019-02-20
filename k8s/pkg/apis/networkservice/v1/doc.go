bf36b4ed6e7c1a2c26ae0b1a604e8ad7c780c858
Nikolay Nikolaev <nnikolay@vmware.com>
2019-02-20 19:08:55 +0200

    Update proto generation for go modules (#749)
    * Update go generate to remove vendor paths
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Remove vendor references in CircleCI
    
    Update protoc installation in CircleCI
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Update re-generated proto files
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- k8s/pkg/apis/networkservice/v1/doc.go
+++ k8s/pkg/apis/networkservice/v1/doc.go
@@ -1,5 +1,4 @@
 // +k8s:deepcopy-gen=package
 // +groupName=networkservicemesh.io
 //go:generate deepcopy-gen --go-header-file ../../../../../conf/boilerplate2.txt -O zz_generated.deepcopy
-//go:generate ../../../../../vendor/k8s.io/code-generator/generate-groups.sh all github.com/networkservicemesh/networkservicemesh/k8s/pkg/networkservice github.com/networkservicemesh/networkservicemesh/k8s/pkg/apis networkservice:v1  --output-base ../../../../../../../../ --go-header-file ../../../../../conf/boilerplate2.txt
 package v1
