e91cbd2f6f87b7085c94fb3bd504e8fbee1569d9
Nikolay Nikolaev <nnikolay@vmware.com>
2019-02-20 09:35:34 +0200

    tests: integration: disable broken tests when run globaly
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- .integration.mk
+++ .integration.mk
@@ -19,7 +19,7 @@ k8s-integration-config:
 
 .PHONY: k8s-integration-tests
 k8s-integration-tests: k8s-integration-config
-	@GO111MODULE=on BROKEN_TESTS_ENABLED=on go test -v ./test/... -failfast
+	@GO111MODULE=on go test -v ./test/... -failfast
 
 .PHONY: k8s-integration-%-test
 k8s-integration-%-test: k8s-integration-config
