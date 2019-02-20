291f53a72bc3fa10b5321485afc0f9f481dfd610
Nikolay Nikolaev <nnikolay@vmware.com>
2019-02-20 12:35:52 +0200

    lower pod*Timeouts
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- test/kube_testing/k8s.go
+++ test/kube_testing/k8s.go
@@ -20,10 +20,10 @@ import (
 )
 
 const (
-	podStartTimeout  = 5 * time.Minute
-	podDeleteTimeout = 5 * time.Minute
-	podExecTimeout	 = 3 * time.Minute
-	podGetLogTimeout = 3 * time.Minute
+	podStartTimeout  = 1 * time.Minute
+	podDeleteTimeout = 1 * time.Minute
+	podExecTimeout   = 1 * time.Minute
+	podGetLogTimeout = 1 * time.Minute
 )
 
 type PodDeployResult struct {
@@ -86,7 +86,7 @@ func (l *K8s) createAndBlock(client kubernetes.Interface, config *rest.Config, n
 		}(pod)
 	}
 
-	if !waitTimeout(fmt.Sprintf("createAndBlock with pods: %v", pods ), &wg, timeout) {
+	if !waitTimeout(fmt.Sprintf("createAndBlock with pods: %v", pods), &wg, timeout) {
 		logrus.Errorf("Failed to deploy pod, trying to get any information")
 		results := []*PodDeployResult{}
 		for _, p := range pods {
@@ -144,7 +144,7 @@ func blockUntilPodReady(client kubernetes.Interface, timeout time.Duration, sour
 			break
 		}
 
-		if time.Since(st) > timeout / 2 {
+		if time.Since(st) > timeout/2 {
 			logrus.Infof("Pod deploy half time passed: %v", pod)
 		}
 
@@ -429,7 +429,7 @@ func (o *K8s) WaitLogsContains(pod *v1.Pod, container string, pattern string, ti
 			logrus.Printf("Error on get logs: %v retrying", error)
 		}
 		if !strings.Contains(logs, pattern) {
-			<- time.Tick(100 * time.Millisecond)
+			<-time.Tick(100 * time.Millisecond)
 		} else {
 			break
 		}
