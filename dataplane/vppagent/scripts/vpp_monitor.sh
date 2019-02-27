a3b1fb3d75af840fe4e6470d04cbf415416e0027
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-02-27 21:54:31 +0700

    Fix integration tests failures (#738)
    Signed-off-by: Stepan Anokhin <stepan.anokhin@gmail.com>

--- dataplane/vppagent/scripts/vpp_monitor.sh
+++ dataplane/vppagent/scripts/vpp_monitor.sh
@@ -11,18 +11,21 @@ fi
 # setup postmortem data location
 readonly DEFAULT_POSTMORTEM_DATA_LOCATION=/var/tmp/nsm-postmortem/vpp-dataplane
 readonly POSTMORTEM_DATA_LOCATION=${POSTMORTEM_DATA_LOCATION:-"$DEFAULT_POSTMORTEM_DATA_LOCATION"}
+readonly GDB_COMMANDS_FILE=/usr/bin/vpp_monitor_commands.gdb
 
 # make sure postmortem data location exists
 mkdir -p "$POSTMORTEM_DATA_LOCATION"
 
-# attach to the vpp with a gdb monitor
-# and store a core dump in case of crash
+# prepare gdb config
 {
     echo "set confirm off"
     echo "set backtrace limit 200"
     echo "handle SIGINT pass nostop"
-    echo "python"
-    cat "/usr/bin/vpp_monitor_handlers.py"
-    echo "end"
+    echo "source /usr/bin/vpp_monitor_handlers.py"
+    echo "attach $(supervisorctl -c /etc/supervisord/supervisord.conf pid vpp)"
     echo "cont"
-} | gdb attach "$(pgrep -f "vpp -c")"
+} > "$GDB_COMMANDS_FILE"
+
+# attach to the vpp with a gdb monitor
+# and store a core dump in case of crash
+gdb -x "$GDB_COMMANDS_FILE"
