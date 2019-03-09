959ae3a69b8da4d467f358038071d7ba51d9f0fa
Nikolay Nikolaev <nnikolay@vmware.com>
2019-03-09 13:42:09 +0200

    Adding initial support for kind.sigs.k8s.io
    K8s in Docker is a standard way to run K8s tests. We use this tool
    here as an alternative cluster provider. Kind is lighter than bringing
    up full VMs.
    
    The setup consists of master and 2 nodes. It successfuly runs
    make k8s-integration test, so can be used in the CI.
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

---
+++ .mk/kind.mk
@@ -0,0 +1,47 @@
+# Copyright 2019 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+KIND_CLUSTER_NAME="nsm"
+KIND_IMAGE_PATH=./scripts/vagrant/images/
+
+.PHONY: kind-config
+kind-config:
+	@which kind >/dev/null 2>&1 || \
+		GO111MODULE=off go get -u sigs.k8s.io/kind
+
+.PHONY: kind-start
+kind-start: kind-config
+	@kind get clusters | grep nsm  >/dev/null 2>&1 || \
+		( kind create cluster --name="$(KIND_CLUSTER_NAME)" --config ./scripts/kind.yaml && \
+		KUBECONFIG="$$(kind get kubeconfig-path --name="$(KIND_CLUSTER_NAME)")" \
+		kubectl taint node $(KIND_CLUSTER_NAME)-control-plane node-role.kubernetes.io/master:NoSchedule- )
+
+.PHONY: kind-stop
+kind-stop:
+	@kind delete cluster --name="$(KIND_CLUSTER_NAME)"
+
+.PHONY: kind-restart
+kind-restart: kind-stop kind-start
+	@echo "kind cluster restarted"
+
+.PHONY: kind-%-load-images
+kind-%-load-images:
+	@if [ -e "$(KIND_IMAGE_PATH)/$*.tar" ]; then \
+		echo "Loading image $*.tar to kind"; \
+		kind load image-archive --name="$(KIND_CLUSTER_NAME)" $(KIND_IMAGE_PATH)$*.tar ; \
+	else \
+		echo "Cannot load $*.tar: scripts/vagrant/images/$*.tar does not exist.  Try running 'make k8s-$*-save'"; \
+		exit 1; \
+	fi
