0c9b92426bb092713d93bea94082c436a720c011
Nikolay Nikolaev <nnikolay@vmware.com>
2019-03-11 01:47:02 +0200

    WIP: Fixes for CircleCI and Packet (#813)
    Fixes for CircleCI and Packet

--- scripts/terraform/packet.tf
+++ scripts/terraform/packet.tf
@@ -5,37 +5,39 @@ variable "master_hostname" {}
 variable "worker1_hostname" {}
 variable "public_key_name" {}
 
-provider "packet"{
-    auth_token = "${var.auth_token}"
+provider "packet" {
+  auth_token = "${var.auth_token}"
 }
 
 resource "packet_ssh_key" "key" {
-    name = "${var.public_key_name}"
-    public_key = "${file(var.public_key)}"
+  name       = "${var.public_key_name}"
+  public_key = "${file(var.public_key)}"
 }
 
-resource "packet_device" "master"{
-    hostname = "${var.master_hostname}"
-    facility = "sjc1"
-    plan = "baremetal_0"
-    operating_system = "ubuntu_16_04"
-    billing_cycle = "hourly"
-    project_id = "${var.project_id}"
-    depends_on = ["packet_ssh_key.key"]
+resource "packet_device" "master" {
+  hostname         = "${var.master_hostname}"
+  facilities       = ["sjc1"]
+  plan             = "t1.small.x86"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${var.project_id}"
+  depends_on       = ["packet_ssh_key.key"]
 }
-resource "packet_device" "worker1"{
-    hostname = "${var.worker1_hostname}"
-    facility = "sjc1"
-    plan = "baremetal_0"
-    operating_system = "ubuntu_16_04"
-    billing_cycle = "hourly"
-    project_id = "${var.project_id}"
-    depends_on = ["packet_ssh_key.key"]
+
+resource "packet_device" "worker1" {
+  hostname         = "${var.worker1_hostname}"
+  facilities       = ["sjc1"]
+  plan             = "t1.small.x86"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${var.project_id}"
+  depends_on       = ["packet_ssh_key.key"]
 }
 
 output "master.public_ip" {
-    value = "${packet_device.master.access_public_ipv4}"
+  value = "${packet_device.master.access_public_ipv4}"
 }
+
 output "worker1.public_ip" {
-    value = "${packet_device.worker1.access_public_ipv4}"
+  value = "${packet_device.worker1.access_public_ipv4}"
 }
