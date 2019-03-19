f5513a1c1d96da85aa3c0baf0cc3df2811fa2f32
lobkovilya <lobkovilya@yandex.ru>
2019-03-19 14:18:40 +0700

    Fix tests
    Signed-off-by: Ilya Lobkov <lobkovilya@yandex.ru>

--- controlplane/pkg/nsmd/constants.go
+++ controlplane/pkg/nsmd/constants.go
@@ -1,8 +1,7 @@
 package nsmd
 
 const (
-	NsmServerSocketEnv  = "NSM_SERVER_SOCKET"
-	NsmClientSocketEnv  = "NSM_CLIENT_SOCKET"
-	WorkspaceEnv        = "WORKSPACE"
-	ExcludedPrefixesEnv = "EXCLUDED_PREFIXES"
+	NsmServerSocketEnv = "NSM_SERVER_SOCKET"
+	NsmClientSocketEnv = "NSM_CLIENT_SOCKET"
+	WorkspaceEnv       = "WORKSPACE"
 )
