581a23e60e9b06ef9545528b125b863d39037763
Artem Belov <artem.belov@xored.com>
2019-03-21 15:31:06 +0700

    Speedup tests pods deleting
    Parallelise pods deleting: Run delete command for all pods and then wait for all of them completed
    
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- test/kube_testing/k8s.go
+++ test/kube_testing/k8s.go
@@ -14,7 +14,7 @@ import (
 	"github.com/sirupsen/logrus"
 	arv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -294,6 +294,7 @@ func (o *K8s) deletePodForce(pod *v1.Pod) error {
 // Delete POD with completion check
 // Make force delete on timeout
 func (o *K8s) deletePods(pods ...*v1.Pod) error {
+	var ctx []context.Context
 	for _, pod := range pods {
 		delOpt := &metaV1.DeleteOptions{}
 		err := o.clientset.CoreV1().Pods(pod.Namespace).Delete(pod.Name, delOpt)
@@ -301,18 +302,21 @@ func (o *K8s) deletePods(pods ...*v1.Pod) error {
 			return err
 		}
 
-		ctx, cancel := context.WithTimeout(context.Background(), podDeleteTimeout)
+		c, cancel := context.WithTimeout(context.Background(), podDeleteTimeout)
+		ctx = append(ctx, c)
 		defer cancel()
-		err = blockUntilPodWorking(o.clientset, ctx, pod)
+	}
+
+	for i, pod := range pods {
+		err := blockUntilPodWorking(o.clientset, ctx[i], pod)
 		if err != nil {
 			err = o.deletePodForce(pod)
 			logrus.Warnf(`The POD "%s" may continue to run on the cluster`, pod.Name)
 			if err != nil {
-				logrus.Error(err)
+				logrus.Warn(err)
 			}
 		}
 	}
-
 	return nil
 }
 
@@ -356,10 +360,9 @@ func (o *K8s) CleanupCRDs() {
 }
 
 func (l *K8s) Cleanup() {
-	for _, result := range l.pods {
-		err := l.deletePods(result)
-		Expect(err).To(BeNil())
-	}
+	err := l.deletePods(l.pods...)
+	Expect(err).To(BeNil())
+	l.CleanupCRDs()
 	l.pods = nil
 	l.CleanupCRDs()
 	l.CleanupConfigMaps()
