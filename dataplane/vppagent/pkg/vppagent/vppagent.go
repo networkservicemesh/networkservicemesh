21a3549148a551e4ce4886b26aed3ad7d7c3791c
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-03-26 17:47:54 +0700

    Fix rebase issue (#753)
    Signed-off-by: Stepan Anokhin <stepan.anokhin@gmail.com>

--- dataplane/vppagent/pkg/vppagent/vppagent.go
+++ dataplane/vppagent/pkg/vppagent/vppagent.go
@@ -35,6 +35,7 @@ import (
 	"github.com/networkservicemesh/networkservicemesh/dataplane/vppagent/pkg/converter"
 	"github.com/networkservicemesh/networkservicemesh/dataplane/vppagent/pkg/memif"
 	"github.com/networkservicemesh/networkservicemesh/pkg/tools"
+	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
