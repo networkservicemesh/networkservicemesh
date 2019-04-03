e42829c37262b3ebfbc1e18dbc7253ca380df377
Nikolay Nikolaev <nnikolay@vmware.com>
2019-04-03 10:28:59 -0700

    CI multi cluster (#883)
    * CI: deploy 2 clusters and run parallel tests
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * CI: parametrize container build
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * CI: cache the terraform binary
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * CI: parallelise K8s deploy
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- scripts/terraform/download-postmortem-data.sh
+++ scripts/terraform/download-postmortem-data.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 # shellcheck disable=SC2086
 
+cluster_id=${PACKET_CLUSTER_ID:-1}
+master_ip="$(terraform output master${cluster_id}.public_ip)"
+worker_ip="$(terraform output worker${cluster_id}_1.public_ip)"
+
 SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 SOURCE_LOC="/var/tmp/nsm-postmortem/"
 
 mkdir -p ~/postmortem
 
-scp ${SSH_OPTS} -r root@"$(terraform output master.public_ip)":"$SOURCE_LOC" ~/postmortem || true
-scp ${SSH_OPTS} -r root@"$(terraform output worker1.public_ip)":"$SOURCE_LOC" ~/postmortem || true
+scp ${SSH_OPTS} -r root@${master_ip}:"$SOURCE_LOC" ~/postmortem${cluster_id} || true
+scp ${SSH_OPTS} -r root@${worker_ip}:"$SOURCE_LOC" ~/postmortem${cluster_id} || true
--- scripts/terraform/packet.tf
+++ scripts/terraform/packet.tf
@@ -1,8 +1,10 @@
 variable "auth_token" {}
 variable "project_id" {}
 variable "public_key" {}
-variable "master_hostname" {}
-variable "worker1_hostname" {}
+variable "master1_hostname" {}
+variable "worker1_1_hostname" {}
+variable "master2_hostname" {}
+variable "worker2_1_hostname" {}
 variable "public_key_name" {}
 
 provider "packet" {
@@ -14,8 +16,8 @@ resource "packet_ssh_key" "key" {
   public_key = "${file(var.public_key)}"
 }
 
-resource "packet_device" "master" {
-  hostname         = "${var.master_hostname}"
+resource "packet_device" "master1" {
+  hostname         = "${var.master1_hostname}"
   facilities       = ["sjc1"]
   plan             = "t1.small.x86"
   operating_system = "ubuntu_16_04"
@@ -24,8 +26,8 @@ resource "packet_device" "master" {
   depends_on       = ["packet_ssh_key.key"]
 }
 
-resource "packet_device" "worker1" {
-  hostname         = "${var.worker1_hostname}"
+resource "packet_device" "worker1_1" {
+  hostname         = "${var.worker1_1_hostname}"
   facilities       = ["sjc1"]
   plan             = "t1.small.x86"
   operating_system = "ubuntu_16_04"
@@ -34,10 +36,38 @@ resource "packet_device" "worker1" {
   depends_on       = ["packet_ssh_key.key"]
 }
 
-output "master.public_ip" {
-  value = "${packet_device.master.access_public_ipv4}"
+output "master1.public_ip" {
+  value = "${packet_device.master1.access_public_ipv4}"
 }
 
-output "worker1.public_ip" {
-  value = "${packet_device.worker1.access_public_ipv4}"
+output "worker1_1.public_ip" {
+  value = "${packet_device.worker1_1.access_public_ipv4}"
+}
+
+resource "packet_device" "master2" {
+  hostname         = "${var.master2_hostname}"
+  facilities       = ["sjc1"]
+  plan             = "t1.small.x86"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${var.project_id}"
+  depends_on       = ["packet_ssh_key.key"]
+}
+
+resource "packet_device" "worker2_1" {
+  hostname         = "${var.worker2_1_hostname}"
+  facilities       = ["sjc1"]
+  plan             = "t1.small.x86"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${var.project_id}"
+  depends_on       = ["packet_ssh_key.key"]
+}
+
+output "master2.public_ip" {
+  value = "${packet_device.master2.access_public_ipv4}"
+}
+
+output "worker2_1.public_ip" {
+  value = "${packet_device.worker2_1.access_public_ipv4}"
 }
