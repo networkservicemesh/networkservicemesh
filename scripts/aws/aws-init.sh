727ecadecfcccc3a6ac4e645fc91d07d999af93e
artem-belov <47595664+artem-belov@users.noreply.github.com>
2019-04-04 19:52:14 +0700

    AWS support (#885)
    * Deploy Amazon AWS kubernates cluster
    
    * Destroy Amazon AWS active services script
    
    * Specify AWS instances names
    
    * Add make parameters; Create quickstart doc file
    
    * Disable yaml linter in the aws vpc template file
    
    * Fix documentation
    
    * Start create cluster script
    
    * Add create AWS cluster go script
    
    * Delete AWS EKS cluster go script
    
    * Fix AWS init script; Change make file from Python to Go
    
    * Return non zero result on errors
    
    Signed-off-by: Artem Belov <artem.belov@xored.com>

---
+++ scripts/aws/aws-init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p "$HOME/.aws"
+mkdir -p "$HOME/bin"
+
+echo "[default]
+region = us-east-2" > "$HOME/.aws/config"
+
+echo "[default]
+aws_access_key_id = $NSM_AWS_ACCESS_KEY_ID
+aws_secret_access_key = $NSM_AWS_SECRET_ACCESS_KEY" > "$HOME/.aws/credentials"
+
+if [ ! -f "$HOME/bin/aws-iam-authenticator" ]; then
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     curl -o "$HOME/bin/aws-iam-authenticator" https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/aws-iam-authenticator;;
+        Darwin*)    curl -o "$HOME/bin/aws-iam-authenticator" https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/darwin/amd64/aws-iam-authenticator;;
+        *)          echo "Unknown uname $unameOut" && exit 1
+    esac
+fi
+chmod +x "$HOME/bin/aws-iam-authenticator"
+export PATH="$HOME/bin:$PATH"
---
+++ scripts/aws/cubernetes-cluster.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func printUsage( ) {
+	fmt.Printf("Usage: go run ./... <command>\n" +
+		"AWS support commands:\n" +
+		"	Create		Create EKS cluster and configure kubernetes\n" +
+		"	Delete		Destroy EKS cluster\n")
+
+}
+
+func main () {
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+
+	switch os.Args[1] {
+		case "Create":
+			createAWSKubernetesCluster()
+		case "Delete":
+			deleteAWSKubernetesCluster()
+		default:
+			printUsage()
+	}
+}
