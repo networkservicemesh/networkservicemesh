df2ede480f18f25883de00bc5c9847b4067bcb2e
lobkovilya <lobkovilya@yandex.ru>
2019-04-05 18:50:42 +0700

    Fix example of vppagent-icmp-responder-nse, add test for memif
    Signed-off-by: Ilya Lobkov <lobkovilya@yandex.ru>

---
+++ test/integration/basic_memif_test.go
@@ -0,0 +1,58 @@
+package nsmd_integration_tests
+
+import (
+	"github.com/networkservicemesh/networkservicemesh/test/integration/nsmd_test_utils"
+	"github.com/networkservicemesh/networkservicemesh/test/kube_testing"
+	"github.com/networkservicemesh/networkservicemesh/test/kube_testing/pods"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestSimpleMemifConnection(t *testing.T) {
+	RegisterTestingT(t)
+
+	if testing.Short() {
+		t.Skip("Skip, please run without -short")
+		return
+	}
+
+	k8s, err := kube_testing.NewK8s()
+	defer k8s.Cleanup()
+
+	Expect(err).To(BeNil())
+
+	k8s.PrepareDefault()
+
+	nodes := nsmd_test_utils.SetupNodesConfig(k8s, 1, defaultTimeout, []*pods.NSMgrPodConfig{})
+
+	envIcmp := map[string]string{
+		"ADVERTISE_NSE_NAME":   "icmp-responder",
+		"ADVERTISE_NSE_LABELS": "app=icmp",
+		"IP_ADDRESS":           "10.20.1.0/24",
+	}
+	vppagentIcmp := k8s.CreatePod(pods.VppagentICMPResponderPod("vppagent-icmp-responder", nodes[0].Node, envIcmp))
+	Expect(vppagentIcmp.Name).To(Equal("vppagent-icmp-responder"))
+
+	envNsc := map[string]string{
+		"OUTGOING_NSC_LABELS": "app=icmp",
+		"OUTGOING_NSC_NAME":   "icmp-responder",
+	}
+	vppagentNsc := k8s.CreatePod(pods.VppagentNSC("vppagent-nsc", nodes[0].Node, envNsc))
+	Expect(vppagentNsc.Name).To(Equal("vppagent-nsc"))
+
+	nseAvailable := false
+	attempts := 30
+	for ; attempts > 0; <-time.Tick(300 * time.Millisecond) {
+		response, _, _ := k8s.Exec(vppagentNsc, vppagentNsc.Spec.Containers[0].Name, "vppctl", "ping", "10.20.1.2", "repeat", "2")
+		if response != "" && !strings.Contains(response, "100% packet loss") {
+			nseAvailable = true
+			logrus.Info("Ping successful")
+			break
+		}
+		attempts--
+	}
+	Expect(nseAvailable).To(Equal(true))
+}
---
+++ test/kube_testing/pods/vppagent_icmp_responder.go
@@ -0,0 +1,49 @@
+package pods
+
+import (
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func VppagentICMPResponderPod(name string, node *v1.Node, env map[string]string) *v1.Pod {
+	var envVars []v1.EnvVar
+	for k, v := range env {
+		envVars = append(envVars,
+			v1.EnvVar{
+				Name:  k,
+				Value: v,
+			})
+	}
+
+	pod := &v1.Pod{
+		ObjectMeta: v12.ObjectMeta{
+			Name: name,
+		},
+		TypeMeta: v12.TypeMeta{
+			Kind: "Deployment",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:            "icmp-responder-nse",
+					Image:           "networkservicemesh/vppagent-icmp-responder-nse:latest",
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Resources: v1.ResourceRequirements{
+						Limits: v1.ResourceList{
+							"networkservicemesh.io/socket": resource.NewQuantity(1, resource.DecimalSI).DeepCopy(),
+						},
+					},
+					Env: envVars,
+				},
+			},
+		},
+	}
+
+	if node != nil {
+		pod.Spec.NodeSelector = map[string]string{
+			"kubernetes.io/hostname": node.Labels["kubernetes.io/hostname"],
+		}
+	}
+	return pod
+}
