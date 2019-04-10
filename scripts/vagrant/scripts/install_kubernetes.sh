c11f6286c9621bd546c8d526340132829c3d0582
Nikolay Nikolaev <nnikolay@vmware.com>
2019-04-10 15:36:50 +0300

    vagrant: cleanup k8s install
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- scripts/vagrant/scripts/install_kubernetes.sh
+++ scripts/vagrant/scripts/install_kubernetes.sh
@@ -14,22 +14,3 @@ apt-get install -y kubernetes-cni kubelet kubeadm kubectl
 swapoff -a
 # keep swap off after reboot
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-# sed -i '/ExecStart=/a Environment="KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-sed -i '0,/ExecStart=/s//Environment="KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs"\n&/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-
-# Setup Hugepages
-#sed -i '9,/KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs/KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs --feature-gates HugePages=false/'
-
-# kubeproxy seems to needs these to actually work properly...
-sysctl net.bridge.bridge-nf-call-iptables=1
-{
-    echo ip_vs_rr 
-    echo ip_vs_wrr
-    echo ip_vs_sh 
-    echo ip_vs 
-} >> /etc/modprobe
-modprobe ip_vs_rr
-modprobe ip_vs_wrr 
-modprobe ip_vs_sh 
-modprobe ip_vs
-lsmod | grep ip_vs
