dbd440ae3cc346478d36cb81050d52236977b004
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-04-15 17:44:33 +0700

    Clean VCS cache on failre
    Signed-off-by: Stepan Anokhin <stepan.anokhin@gmail.com>

--- scripts/go-mod-download.sh
+++ scripts/go-mod-download.sh
@@ -5,6 +5,7 @@ attempt=1;
 
 until (( attempt > limit )) || go mod download; do
     attempt=$(( attempt + 1 ));
+    rm -rf "$GOPATH"/pkg/mod/cache/vcs/* # wipe out the vcs cache to overwrite corrupted repos
     (( attempt <= limit )) && echo "Trying again, attempt $attempt";
 done
 
