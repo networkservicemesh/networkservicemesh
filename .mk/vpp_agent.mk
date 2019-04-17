ed43ec6d634721948e9784f0fb84a2a93100d49f
Nikolay Nikolaev <nnikolay@vmware.com>
2019-04-17 08:55:47 +0300

    vpp update to v2.0.1 (#911)
    Cleanup unneeded kafka.conf deleteion.
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- .mk/vpp_agent.mk
+++ .mk/vpp_agent.mk
@@ -1,9 +1,9 @@
 ARCH ?= $(shell uname -m)
 ifeq (${ARCH}, x86_64)
-  export VPP_AGENT=ligato/vpp-agent:v2.0.0
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.0.0
+  export VPP_AGENT=ligato/vpp-agent:v2.0.1
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.0.1
 endif
 ifeq (${ARCH}, aarch64)
-  export VPP_AGENT=ligato/vpp-agent-arm64:v2.0.0
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent-arm64:v2.0.0
+  export VPP_AGENT=ligato/vpp-agent-arm64:v2.0.1
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent-arm64:v2.0.1
 endif
