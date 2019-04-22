0ca9b5d441dc3563b386ac87d0186ee2fc211e18
Vladimir Popov <vladimir.popov@xored.com>
2019-04-22 18:09:29 +0700

    Fix issue with nse_run script
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- examples/cmd/nse/nse_run.sh
+++ examples/cmd/nse/nse_run.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
 
-"/bin/${NSE_IMAGE}"
+kill_nse() {
+    kill -s $1 "${NSE}"
+}
+
+sighup () {
+    kill_nse "HUP"
+}
+
+sigint () {
+    kill_nse "INT"
+}
+
+sigterm () {
+    kill_nse "TERM"
+}
+
+sigquit () {
+    kill_nse "QUIT"
+}
+
+trap sighup SIGHUP
+trap sigint SIGINT
+trap sigterm SIGTERM
+trap sigquit SIGQUIT
+
+"/bin/${NSE_IMAGE}" &
+
+NSE=$!
+wait "${NSE}"
+wait "${NSE}"
