dccb9eebcc91d8e59ef6223cf585391c33f6c16e
Artem Belov <artem.belov@xored.com>
2019-04-26 21:16:33 +0700

    Fix aws-iam-authenticator access during tests
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- scripts/aws/create-kubernetes-cluster.go
+++ scripts/aws/create-kubernetes-cluster.go
@@ -227,8 +227,6 @@ func createEksWorkerNodes(cfClient *cloudformation.CloudFormation, nodesStackNam
 
 	s := string(sf)
 
-	subnetId := strings.Split(*clusterStackOutputs.SubnetIds, ",")[0]
-
 	_, err = cfClient.CreateStack(&cloudformation.CreateStackInput{
 		StackName:    nodesStackName,
 		TemplateBody: &s,
