8cb6239e4e0875b59a7c01401127c7d142fa103e
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-04-27 00:54:39 +0300

    Expand bench tests (#998)
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- test/integration/const.go
+++ test/integration/const.go
@@ -6,4 +6,8 @@ const (
 	ciDelayCoefficient = 1
 	defaultTimeout     = 2 * time.Minute * ciDelayCoefficient
 	fastTimeout        = defaultTimeout / 5
+	nscDefaultName     = "nsc"
+	icmpDefaultName    = "icmp-responder"
+	nscCount           = 5
+	nscMaxCount        = 10
 )
