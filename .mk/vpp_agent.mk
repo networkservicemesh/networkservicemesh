2c4b4ebdf0aef236ba1c4880c96c3a4ff154e071
Nikolay Nikolaev <nnikolay@vmware.com>
2019-05-10 11:08:49 +0300

    vpp-agent: bump version to 2.1.0
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- .mk/vpp_agent.mk
+++ .mk/vpp_agent.mk
@@ -1,9 +1,9 @@
 ARCH ?= $(shell uname -m)
 ifeq (${ARCH}, x86_64)
-  export VPP_AGENT=ligato/vpp-agent:v2.0.2
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.0.2
+  export VPP_AGENT=ligato/vpp-agent:v2.1.0
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.1.0
 endif
 ifeq (${ARCH}, aarch64)
-  export VPP_AGENT=ligato/vpp-agent-arm64:v2.0.2
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent-arm64:v2.0.2
+  export VPP_AGENT=ligato/vpp-agent-arm64:v2.1.0
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent-arm64:v2.1.0
 endif
