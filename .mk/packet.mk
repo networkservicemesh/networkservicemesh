21202ab024eaf2b73e76767952c79d8e846edcec
Artem Belov <artem.belov@xored.com>
2019-05-17 18:41:48 +0700

    Fix packet ssh commands
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- .mk/packet.mk
+++ .mk/packet.mk
@@ -67,8 +67,8 @@ packet-download-postmortem:
 packet-print-kubelet-log:
 	@pushd scripts/terraform && \
 	echo "Master node kubelet log:" && \
-	scp ${SSH_OPTS} root@`terraform output master${PACKET_CLUSTER_ID}.public_ip` "journalctl -u kubelet" && \
+	ssh ${SSH_OPTS} root@`terraform output master${PACKET_CLUSTER_ID}.public_ip` "journalctl -u kubelet" && \
 	echo "Woker node kubelet log:" && \
-	scp ${SSH_OPTS} root@`terraform output worker${PACKET_CLUSTER_ID}_1.public_ip` "journalctl -u kubelet" && \
+	ssh ${SSH_OPTS} root@`terraform output worker${PACKET_CLUSTER_ID}_1.public_ip` "journalctl -u kubelet" && \
 	popd
 
