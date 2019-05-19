c634168ff68274853ab78d77a85c70d3485c3c7b
denis-tingajkin <denis.tingajkin@xored.com>
2019-05-20 02:18:06 +0700

    Add check on nil for RemoteDestination
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- controlplane/pkg/nsm/nse_requests.go
+++ controlplane/pkg/nsm/nse_requests.go
@@ -13,39 +13,39 @@ import (
 func (srv *networkServiceManager) createRemoteNSMRequest(endpoint *registry.NSERegistration, requestConnection nsm.NSMConnection, dataplane *model.Dataplane, existingConnection *model.ClientConnection) *remote_networkservice.NetworkServiceRequest {
 	// We need to obtain parameters for remote mechanism
 	remoteM := append([]*remote_connection.Mechanism{}, dataplane.RemoteMechanisms...)
-	var message *remote_networkservice.NetworkServiceRequest
 
 	// Try Heal only if endpoint are same as for existing connection.
 	if existingConnection != nil && endpoint == existingConnection.Endpoint {
 		remoteDst := existingConnection.Xcon.GetRemoteDestination()
-		message = &remote_networkservice.NetworkServiceRequest{
+		if remoteDst != nil {
+			return &remote_networkservice.NetworkServiceRequest{
 
-			Connection: &remote_connection.Connection{
-				Id:                                   remoteDst.GetId(),
-				NetworkService:                       remoteDst.NetworkService,
-				Context:                              remoteDst.GetContext(),
-				Labels:                               remoteDst.GetLabels(),
-				DestinationNetworkServiceManagerName: endpoint.GetNetworkServiceManager().GetName(),
-				SourceNetworkServiceManagerName:      srv.getNetworkServiceManagerName(),
-				NetworkServiceEndpointName:           endpoint.GetNetworkserviceEndpoint().GetEndpointName(),
-			},
-			MechanismPreferences: remoteM,
-		}
-	} else {
-		message = &remote_networkservice.NetworkServiceRequest{
-			Connection: &remote_connection.Connection{
-				Id:                                   "-",
-				NetworkService:                       requestConnection.GetNetworkService(),
-				Context:                              requestConnection.GetContext(),
-				Labels:                               requestConnection.GetLabels(),
-				DestinationNetworkServiceManagerName: endpoint.GetNetworkServiceManager().GetName(),
-				SourceNetworkServiceManagerName:      srv.getNetworkServiceManagerName(),
-				NetworkServiceEndpointName:           endpoint.GetNetworkserviceEndpoint().GetEndpointName(),
-			},
-			MechanismPreferences: remoteM,
+				Connection: &remote_connection.Connection{
+					Id:                                   remoteDst.GetId(),
+					NetworkService:                       remoteDst.NetworkService,
+					Context:                              remoteDst.GetContext(),
+					Labels:                               remoteDst.GetLabels(),
+					DestinationNetworkServiceManagerName: endpoint.GetNetworkServiceManager().GetName(),
+					SourceNetworkServiceManagerName:      srv.getNetworkServiceManagerName(),
+					NetworkServiceEndpointName:           endpoint.GetNetworkserviceEndpoint().GetEndpointName(),
+				},
+				MechanismPreferences: remoteM,
+			}
 		}
 	}
-	return message
+	return &remote_networkservice.NetworkServiceRequest{
+		Connection: &remote_connection.Connection{
+			Id:                                   "-",
+			NetworkService:                       requestConnection.GetNetworkService(),
+			Context:                              requestConnection.GetContext(),
+			Labels:                               requestConnection.GetLabels(),
+			DestinationNetworkServiceManagerName: endpoint.GetNetworkServiceManager().GetName(),
+			SourceNetworkServiceManagerName:      srv.getNetworkServiceManagerName(),
+			NetworkServiceEndpointName:           endpoint.GetNetworkserviceEndpoint().GetEndpointName(),
+		},
+		MechanismPreferences: remoteM,
+	}
+
 }
 
 func (srv *networkServiceManager) createLocalNSERequest(endpoint *registry.NSERegistration, requestConnection nsm.NSMConnection) *networkservice.NetworkServiceRequest {
