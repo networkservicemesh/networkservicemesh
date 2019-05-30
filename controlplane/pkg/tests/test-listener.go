47fef584ded4e0a525a1993d24f6d3e8eb19e6c5
lobkovilya <ilya.lobkov@xored.com>
2019-05-30 20:44:55 +0700

    Fix concurrent writing to map in test listener
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- controlplane/pkg/tests/test-listener.go
+++ controlplane/pkg/tests/test-listener.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -15,6 +16,7 @@ const (
 )
 
 type testConnectionModelListener struct {
+	sync.RWMutex
 	model.ListenerImpl
 	additions int
 	updates   int
@@ -27,27 +29,42 @@ type testConnectionModelListener struct {
 }
 
 func (impl *testConnectionModelListener) ClientConnectionAdded(clientConnection *model.ClientConnection) {
+	impl.Lock()
+	defer impl.Unlock()
+
 	impl.additions++
 	impl.connections[clientConnection.GetID()] = clientConnection
 	logrus.Infof("ClientConnectionAdded: %v", clientConnection)
 }
 
 func (impl *testConnectionModelListener) ClientConnectionDeleted(clientConnection *model.ClientConnection) {
+	impl.Lock()
+	defer impl.Unlock()
+
 	impl.deletions++
 	logrus.Infof("ClientConnectionDeleted: %v", clientConnection)
 	delete(impl.connections, clientConnection.GetID())
 }
 
 func (impl *testConnectionModelListener) ClientConnectionUpdated(old, new *model.ClientConnection) {
+	impl.Lock()
+	defer impl.Unlock()
+
 	impl.updates++
 	impl.connections[new.GetID()] = new
 	logrus.Infof("ClientConnectionUpdated: %s %v", new.GetID(), impl.textMarshaler.Text(new.Xcon))
 }
 
 func (impl *testConnectionModelListener) EndpointAdded(endpoint *model.Endpoint) {
+	impl.Lock()
+	defer impl.Unlock()
+
 	impl.endpoints++
 }
 func (impl *testConnectionModelListener) EndpointDeleted(endpoint *model.Endpoint) {
+	impl.Lock()
+	defer impl.Unlock()
+
 	impl.endpoints--
 }
 
