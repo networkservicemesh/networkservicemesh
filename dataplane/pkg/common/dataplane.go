0d06f8743fe6b1065beac26fd7db76a2dd3f49b9
Radoslav Dimitrov <dimitrovr@vmware.com>
2019-06-12 01:49:35 +0300

    Refactor forwarding plane variables
    Signed-off-by: Radoslav Dimitrov <dimitrovr@vmware.com>

--- dataplane/pkg/common/dataplane.go
+++ dataplane/pkg/common/dataplane.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"net"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
@@ -27,6 +28,8 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/crossconnect"
+	local "github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/local/connection"
+	remote "github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/remote/connection"
 	monitor_crossconnect "github.com/networkservicemesh/networkservicemesh/controlplane/pkg/monitor/crossconnect"
 	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/monitor_crossconnect_server"
 	"github.com/networkservicemesh/networkservicemesh/dataplane/pkg/apis/dataplane"
@@ -39,12 +42,22 @@ type NSMDataplane interface {
 
 // TODO Convert all the defaults to properly use NsmBaseDir
 const (
-	NSMBaseDirKey                       = "NSM_BASEDIR"
-	NSMBaseDirDefault                   = "/var/lib/networkservicemesh/"
-	DataplaneRegistrarSocketKey         = "DATAPLANE_REGISTRAR_SOCKET"
-	DataplaneRegistrarSocketDefault     = "/var/lib/networkservicemesh/nsm.dataplane-registrar.io.sock"
-	DataplaneRegistrarSocketTypeKey     = "DATAPLANE_REGISTRAR_SOCKET_TYPE"
-	DataplaneRegistrarSocketTypeDefault = "unix"
+	NSMBaseDirKey                        = "NSM_BASEDIR"
+	NSMBaseDirDefault                    = "/var/lib/networkservicemesh/"
+	DataplaneRegistrarSocketKey          = "DATAPLANE_REGISTRAR_SOCKET"
+	DataplaneRegistrarSocketDefault      = "/var/lib/networkservicemesh/nsm.dataplane-registrar.io.sock"
+	DataplaneRegistrarSocketTypeKey      = "DATAPLANE_REGISTRAR_SOCKET_TYPE"
+	DataplaneRegistrarSocketTypeDefault  = "unix"
+	DataplaneMetricsEnabledKey           = "METRICS_COLLECTOR_ENABLED"
+	DataplaneMetricsEnabledDefault       = true
+	DataplaneMetricsRequestPeriodKey     = "METRICS_COLLECTOR_REQUEST_PERIOD"
+	DataplaneMetricsRequestPeriodDefault = time.Second * 2
+	DataplaneNameKey                     = "DATAPLANE_NAME"
+	DataplaneNameDefault                 = "vppagent"
+	DataplaneSocketKey                   = "DATAPLANE_SOCKET"
+	DataplaneSocketDefault               = "/var/lib/networkservicemesh/nsm-vppagent.dataplane.sock"
+	DataplaneSocketTypeKey               = "DATAPLANE_SOCKET_TYPE"
+	DataplaneSocketTypeDefault           = "unix"
 )
 
 type DataplaneConfigBase struct {
@@ -54,6 +67,9 @@ type DataplaneConfigBase struct {
 	RegistrarSocketType string
 	DataplaneSocket     string
 	DataplaneSocketType string
+	Mechanisms          *Mechanisms
+	MetricsEnabled      bool
+	MetricsPeriod       time.Duration
 }
 
 type dataplaneConfig struct {
@@ -63,11 +79,38 @@ type dataplaneConfig struct {
 	listener   net.Listener
 }
 
+// Mechanisms is a message used to communicate any changes in operational parameters and constraints
+type Mechanisms struct {
+	RemoteMechanisms []*remote.Mechanism
+	LocalMechanisms  []*local.Mechanism
+}
+
 func createDataplaneConfig() *dataplaneConfig {
 	dpConfig := &dataplaneConfig{
 		common: &DataplaneConfigBase{},
 	}
 	var ok bool
+
+	dpConfig.common.Name, ok = os.LookupEnv(DataplaneNameKey)
+	if !ok {
+		logrus.Infof("%s not set, using default %s", DataplaneNameKey, DataplaneNameDefault)
+		dpConfig.common.Name = DataplaneNameDefault
+	}
+	logrus.Infof("Starting dataplane - %s", dpConfig.common.Name)
+
+	dpConfig.common.DataplaneSocket, ok = os.LookupEnv(DataplaneSocketKey)
+	if !ok {
+		logrus.Infof("%s not set, using default %s", DataplaneSocketKey, DataplaneSocketDefault)
+		dpConfig.common.DataplaneSocket = DataplaneSocketDefault
+	}
+	logrus.Infof("DataplaneSocket: %s", dpConfig.common.DataplaneSocket)
+
+	dpConfig.common.DataplaneSocketType, ok = os.LookupEnv(DataplaneSocketTypeKey)
+	if !ok {
+		logrus.Infof("%s not set, using default %s", DataplaneSocketTypeKey, DataplaneSocketTypeDefault)
+		dpConfig.common.DataplaneSocketType = DataplaneSocketTypeDefault
+	}
+	logrus.Infof("DataplaneSocketType: %s", dpConfig.common.DataplaneSocketType)
 
 	dpConfig.common.NSMBaseDir, ok = os.LookupEnv(NSMBaseDirKey)
 	if !ok {
@@ -100,7 +143,27 @@ func createDataplaneConfig() *dataplaneConfig {
 	dpConfig.monitor = monitor_crossconnect.NewMonitorServer()
 	crossconnect.RegisterMonitorCrossConnectServer(dpConfig.gRPCserver, dpConfig.monitor)
 	monitor_crossconnect_server.NewMonitorNetNsInodeServer(dpConfig.monitor)
+	/* Metrics */
+	dpConfig.common.MetricsEnabled = DataplaneMetricsEnabledDefault
+	val, ok := os.LookupEnv(DataplaneMetricsEnabledKey)
+	if ok {
+		res, err := strconv.ParseBool(val)
+		if err == nil {
+			dpConfig.common.MetricsEnabled = res
+		}
+	}
+	logrus.Infof("MetricsEnabled: %s", dpConfig.common.MetricsEnabled)
 
+	if dpConfig.common.MetricsEnabled {
+		dpConfig.common.MetricsPeriod = DataplaneMetricsRequestPeriodDefault
+		if val, ok = os.LookupEnv(DataplaneMetricsRequestPeriodKey); ok {
+			parsedPeriod, err := time.ParseDuration(val)
+			if err == nil {
+				dpConfig.common.MetricsPeriod = parsedPeriod
+			}
+		}
+		logrus.Infof("MetricsPeriod: %v ", dpConfig.common.MetricsPeriod)
+	}
 	return dpConfig
 }
 
