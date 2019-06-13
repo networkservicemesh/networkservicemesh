fa4e624bfe773013197aef6c5802beefb51d65ba
denis-tingajkin <denis.tingajkin@xored.com>
2019-06-13 18:53:25 +0700

    Fix 'install: cannot change permissions of ‘/go/bin’: Operation not permitted'
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- scripts/lint-download.sh
+++ scripts/lint-download.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 {
-    wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.16.0
+    wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- v1.16.0
+    sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
-    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b "$(go env GOPATH)"/bin v1.16.0
+    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- v1.16.0
+    sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
     make lint-download
 }
