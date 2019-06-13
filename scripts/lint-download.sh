71703d97a773a5a4890409c26e8371b5216a3650
Nikolay Nikolaev <nnikolay@vmware.com>
2019-06-13 16:22:45 +0300

    Updates and fixes (#1236)
    * Upgrade lint to 1.17.1
    
    * Cleanup helm charts of nodeSelector
    
    * go mod tidy and fixes
    
    * Fix push docker images
    
    We now have only test-common and vpp-test-common
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- scripts/lint-download.sh
+++ scripts/lint-download.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 {
-    wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- v1.16.0
+    wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- v1.17.1
     sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
-    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- v1.16.0
+    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- v1.17.1
     sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
     make lint-download
