f9a0d820a6f46e3ad440375ea9499ed97562ec33
Andrey Sobolev <haiodo@xored.com>
2019-06-19 18:26:45 +0700

    Make shell check happy
    Signed-off-by: Andrey Sobolev <haiodo@xored.com>

--- .cloudtest/gke/init.sh
+++ .cloudtest/gke/init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Authenticate if not yet"
-if $(gcloud auth print-identity-token 2>&1 | grep -q "gcloud config set account ACCOUNT"); then
+if gcloud auth print-identity-token 2>&1 | grep -q 'gcloud config set account ACCOUNT'; then
+    echo "Doing auth"
     echo "$GCLOUD_SERVICE_KEY" | gcloud auth activate-service-account --key-file=-
+else
+    echo "All is ok"
 fi
