e201df3798dd6d934032d7d1e2acaf4eaa658c39
Radoslav Dimitrov <dimitrovr@vmware.com>
2019-06-19 13:57:46 +0300

    Group target variables
    Signed-off-by: Radoslav Dimitrov <dimitrovr@vmware.com>

--- .mk/k8s.mk
+++ .mk/k8s.mk
@@ -15,15 +15,15 @@
 K8S_CONF_DIR = k8s/conf
 
 DEPLOY_ICMP_KERNEL = icmp-responder-nse nsc
-DEPLOY_ICMP_VPP = vppagent-icmp-responder-nse vppagent-nsc
 DEPLOY_TRACING = jaeger
 DEPLOY_WEBHOOK = admission-webhook
 DEPLOY_MONITOR = crossconnect-monitor skydive
 # Set the configured forwarding plane
 ifeq (${FORWARDING_PLANE}, vpp)
   DEPLOY_FORWARDING_PLANE = vppagent-dataplane
-  DEPLOY_ICMP = $(DEPLOY_ICMP_KERNEL) $(DEPLOY_ICMP_VPP)
+  DEPLOY_ICMP_VPP = vppagent-icmp-responder-nse vppagent-nsc
   DEPLOY_VPN = secure-intranet-connectivity vppagent-firewall-nse vppagent-passthrough-nse vpn-gateway-nse vpn-gateway-nsc
+  DEPLOY_ICMP = $(DEPLOY_ICMP_KERNEL) $(DEPLOY_ICMP_VPP)
 endif
 # Need nsmdp and icmp-responder-nse here as well, but missing yaml files
 DEPLOY_NSM = nsmgr $(DEPLOY_FORWARDING_PLANE)
