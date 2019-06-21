0424964f4884b3c35809275f964db44c66e74ecf
denis-tingajkin <denis.tingajkin@xored.com>
2019-06-21 17:43:12 +0700

    Review comments
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- .mk/code_analysis.mk
+++ .mk/code_analysis.mk
@@ -4,11 +4,11 @@ lint-fix:
 
 .PHONY: lint-install
 lint-install:
-	GO111MODULE=on go get -u github.com/golangci/golangci-lint/cmd/golangci-lint@v1.17.1
+	go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.17.1
 
 .PHONY: lint-check-diff
 lint-check-diff:
-	GO111MODULE=on golangci-lint run --new-from-rev=origin/master
+	golangci-lint run --new-from-rev=origin/master
 
 .PHONY: lint-check-all
 lint-check-all:
