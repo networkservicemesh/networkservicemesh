03bdfbbdd98c66d3a324bb274d737f22784ee202
Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
2019-06-25 09:58:23 +0200

    clean and fix minor bugs
    Signed-off-by: Stanislav Chlebec <stanislav.chlebec@pantheon.tech>

--- scripts/install-qemu-user-static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dpkg -l | grep qemu-user-static
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    sudo apt-get install -y qemu-user-static
-fi
-cp /usr/bin/qemu-aarch64-static $1/test/applications/build/qemu-aarch64-static 
-cp /usr/bin/qemu-aarch64-static $1/dataplane/vppagent/conf/vpp/qemu-aarch64-static
