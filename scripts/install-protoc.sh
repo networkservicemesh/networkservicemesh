8b9eb62198de616bed7d7b113551c11df74ed4f3
denis-tingajkin <denis.tingajkin@xored.com>
2019-06-25 16:22:27 +0700

    Checkout install-protoc.sh
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -2,7 +2,7 @@
 set -ex
 pushd ~/
 PROTOC_VER=3.6.1
-PROTOC_ZIP=protoc-$PROTOC_VER-osx-x86_64.zip
+PROTOC_ZIP=protoc-$PROTOC_VER-linux-x86_64.zip
 curl -OL https://github.com/google/protobuf/releases/download/v$PROTOC_VER/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 rm -f $PROTOC_ZIP
