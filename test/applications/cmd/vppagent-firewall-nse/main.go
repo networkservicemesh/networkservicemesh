ac680576599ac1b53255fc8059d5fae86e503c58
Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>
2019-07-02 15:48:46 +0700

    Use ClientConnect, MemifConnect and XConnect in firewall test
    Signed-off-by: Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>

--- test/applications/cmd/vppagent-firewall-nse/main.go
+++ test/applications/cmd/vppagent-firewall-nse/main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/networkservicemesh/networkservicemesh/pkg/tools"
 	"github.com/networkservicemesh/networkservicemesh/sdk/common"
 	"github.com/networkservicemesh/networkservicemesh/sdk/endpoint"
+	"github.com/networkservicemesh/networkservicemesh/sdk/vppagent"
 	"github.com/sirupsen/logrus"
 )
 
@@ -28,7 +29,7 @@ var version string
 
 func main() {
 	logrus.Info("Starting vppagent-firewall-nse...")
-	logrus.Infof("Versiion: %v", version)
+	logrus.Infof("Version: %v", version)
 	// Capture signals to cleanup before exiting
 	c := tools.NewOSSignalChannel()
 
@@ -44,7 +45,10 @@ func main() {
 	composite := endpoint.NewCompositeEndpoint(
 		endpoint.NewMonitorEndpoint(configuration),
 		newVppAgentAclComposite(configuration),
-		newVppAgentXConnComposite(configuration),
+		vppagent.NewFlush(configuration, "localhost:9112"),
+		vppagent.NewXConnect(configuration),
+		vppagent.NewMemifConnect(configuration, vppagent.SOURCE),
+		vppagent.NewClientConnect(configuration),
 		endpoint.NewClientEndpoint(configuration),
 		endpoint.NewConnectionEndpoint(configuration))
 
