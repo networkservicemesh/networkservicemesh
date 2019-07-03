f9c2a0f27faa60f31711a0c242917d14b29634cd
lobkovilya <ilya.lobkov@xored.com>
2019-07-03 18:46:41 +0700

    Add readonly port to kubelet
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- .cloudtest/packet/start-master.sh
+++ .cloudtest/packet/start-master.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 kubeadm init --pod-network-cidr=192.168.0.0/16 --skip-token-print
 
+echo "KUBELET_EXTRA_ARGS= --read-only-port=10255" > /etc/default/kubelet
+service kubelet restart
+
 mkdir -p "$HOME"/.kube
 sudo cp -f /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u):$(id -g)" "$HOME"/.kube/config
