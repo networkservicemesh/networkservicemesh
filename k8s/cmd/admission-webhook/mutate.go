7d0c3302f23d6202d8b639edccf74be70d677875
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-07-11 22:39:19 +0700

    Fix admission webhook ignores wrong pod specs  (#1337)
    * Add check on container duplication. Code cleanup.
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * Add test, code cleanup
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * remove validate handler
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * make format
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * self code review
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

---
+++ k8s/cmd/admission-webhook/mutate.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/admission/v1beta1"
+)
+
+func (s *nsmAdmissionWebhook) mutate(request *v1beta1.AdmissionRequest) *v1beta1.AdmissionResponse {
+	logrus.Infof("AdmissionReview for =%v", request)
+	if !isSupportKind(request) {
+		return okReviewResponse()
+	}
+	metaAndSpec, err := getMetaAndSpec(request)
+	if err != nil {
+		return errorReviewResponse(err)
+	}
+	value, ok := getNsmAnnotationValue(ignoredNamespaces, metaAndSpec)
+	if !ok {
+		logrus.Infof("Skipping validation for %s/%s due to policy check", metaAndSpec.meta.Namespace, metaAndSpec.meta.Name)
+		return okReviewResponse()
+	}
+	if err = validateAnnotationValue(value); err != nil {
+		return errorReviewResponse(err)
+	}
+	if err = checkNsmInitContainerDuplication(metaAndSpec.spec); err != nil {
+		logrus.Error(err)
+		return errorReviewResponse(err)
+	}
+	patch := createInitContainerPatch(value, getInitContainerPatchPath(request))
+	//append another patches
+	patchBytes, err := json.Marshal(patch)
+	if err != nil {
+		return errorReviewResponse(err)
+	}
+	logrus.Infof("AdmissionResponse: patch=%v\n", string(patchBytes))
+	return createReviewResponse(patchBytes)
+}
