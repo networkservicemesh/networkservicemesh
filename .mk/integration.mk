a163fd1270a9ea19fbbdb93e8463fcba0727424f
Nikolay Nikolaev <nnikolay@vmware.com>
2019-07-15 13:14:03 +0300

    Go mod tidy (#1332)
    * Go mod tidy
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Increase test timeouts for local testing
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Protobuf 1.3.2
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * unpin protobuf
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Bump protoc 3.8.0
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Protobuf 1.3.2 in proto files
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * fix go gen paths
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * Remove strict protobuf version dependency
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>
    
    * rever fix go gen paths
    b1bb87af003b1539b9bdfdded613581f79cb60df
    
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- .mk/integration.mk
+++ .mk/integration.mk
@@ -29,4 +29,4 @@ k8s-integration-tests-%: k8s-integration-config
 
 .PHONY: k8s-integration-%-test
 k8s-integration-%-test: k8s-integration-config
-	@GO111MODULE=on BROKEN_TESTS_ENABLED=on NSM_NAMESPACE=${NSM_NAMESPACE_INTEGRATION} go test -v ./test/integration/... -failfast -tags="basic recover usecase" -run $*
+	@GO111MODULE=on BROKEN_TESTS_ENABLED=on NSM_NAMESPACE=${NSM_NAMESPACE_INTEGRATION} go test -v ./test/integration/... -failfast -timeout 60m -tags="basic recover usecase" -run $*
