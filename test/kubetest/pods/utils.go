10bd2aa9ab7a9ed1f9a3968c5d6347f356244cd5
lobkovilya <ilya.lobkov@xored.com>
2019-07-18 01:08:47 +0700

    Activate all clouds for checking
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- test/kubetest/pods/utils.go
+++ test/kubetest/pods/utils.go
@@ -2,7 +2,6 @@ package pods
 
 import (
 	"k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
@@ -25,22 +24,22 @@ func createProbe(path string) *v1.Probe {
 
 func createDefaultResources() v1.ResourceRequirements {
 	return v1.ResourceRequirements{
-		Requests: v1.ResourceList{
-			v1.ResourceCPU: resource.NewScaledQuantity(1, -3).DeepCopy(),
-		},
-		Limits: v1.ResourceList{
-			v1.ResourceCPU: resource.NewScaledQuantity(1, 0).DeepCopy(),
-		},
+		//Requests: v1.ResourceList{
+		//	v1.ResourceCPU: resource.NewScaledQuantity(1, -3).DeepCopy(),
+		//},
+		//Limits: v1.ResourceList{
+		//	v1.ResourceCPU: resource.NewScaledQuantity(1, 0).DeepCopy(),
+		//},
 	}
 }
 
 func createDefaultDataplaneResources() v1.ResourceRequirements {
 	return v1.ResourceRequirements{
-		Requests: v1.ResourceList{
-			v1.ResourceCPU: resource.NewScaledQuantity(1, -3).DeepCopy(),
-		},
-		Limits: v1.ResourceList{
-			v1.ResourceCPU: resource.NewScaledQuantity(1, 0).DeepCopy(),
-		},
+		//Requests: v1.ResourceList{
+		//	v1.ResourceCPU: resource.NewScaledQuantity(1, -3).DeepCopy(),
+		//},
+		//Limits: v1.ResourceList{
+		//	v1.ResourceCPU: resource.NewScaledQuantity(1, 0).DeepCopy(),
+		//},
 	}
 }
