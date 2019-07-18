d0c8830be3a6916b2b3ab5f4f5eae67e9f61f896
Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>
2019-07-18 10:55:47 +0700

    Format code
    Signed-off-by: Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>

--- controlplane/pkg/apis/plugins/constants.go
+++ controlplane/pkg/apis/plugins/constants.go
@@ -2,7 +2,7 @@ package plugins
 
 const (
 	// PluginRegistryPath defines the location of NSM Plugin sockets
-	PluginRegistryPath   = "/var/lib/networkservicemesh/plugins/"
+	PluginRegistryPath = "/var/lib/networkservicemesh/plugins/"
 	// PluginRegistrySocket defines the location of NSM Plugin Registry socket
 	PluginRegistrySocket = PluginRegistryPath + "registry.sock"
 )
