42f2306339a042a8f2a0d3f07453f55c12daeeff
Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>
2019-07-25 11:27:48 +0700

    Use snake_case for file names
    Signed-off-by: Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>

--- controlplane/pkg/apis/plugins/gen.go
+++ controlplane/pkg/apis/plugins/gen.go
@@ -1,3 +1,3 @@
 package plugins
 
-//go:generate bash -c "protoc -I . registry.proto connectionplugin.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/  --proto_path=$( go list -f '{{ .Dir }}' -m github.com/golang/protobuf )"
+//go:generate bash -c "protoc -I . registry.proto connection_plugin.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/  --proto_path=$( go list -f '{{ .Dir }}' -m github.com/golang/protobuf )"
