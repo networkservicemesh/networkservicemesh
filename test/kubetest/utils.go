1c399840159c3395f94e7e9d4f68fc238edfe0ff
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-07-27 02:54:27 +0700

    TestOneToOneConnectionMemif periodically fails on CI (#1383)
    * Fix for issues/1378
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * self code review
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * fix linter issue
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * Review comments
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- test/kubetest/utils.go
+++ test/kubetest/utils.go
@@ -5,6 +5,7 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/apis/nsm"
 	"net"
 	"os"
 	"strings"
@@ -213,6 +214,14 @@ func DeployMonitoringNSC(k8s *K8s, node *v1.Node, name string, timeout time.Dura
 	)
 }
 
+// NoHealNSMgrPodConfig returns config for NSMgr. The config has properties for disabling healing for nse
+func NoHealNSMgrPodConfig(k8s *K8s) []*pods.NSMgrPodConfig {
+	return []*pods.NSMgrPodConfig{
+		noHealNSMgrPodConfig(k8s),
+		noHealNSMgrPodConfig(k8s),
+	}
+}
+
 func icmpCommand(dirty, neighbors, routes, update bool) []string {
 	command := []string{"/bin/icmp-responder-nse"}
 
@@ -251,6 +260,18 @@ func defaultNSCEnv() map[string]string {
 	return map[string]string{
 		"OUTGOING_NSC_LABELS": "app=icmp",
 		"OUTGOING_NSC_NAME":   "icmp-responder",
+	}
+}
+
+func noHealNSMgrPodConfig(k8s *K8s) *pods.NSMgrPodConfig {
+	return &pods.NSMgrPodConfig{
+		Variables: map[string]string{
+			nsmd2.NsmdDeleteLocalRegistry: "true", // Do not use local registry restore for clients/NSEs
+			nsm.NsmdHealDSTWaitTimeout:    "1",    // 1 second
+			nsm.NsmdHealEnabled:           "true",
+		},
+		Namespace:          k8s.GetK8sNamespace(),
+		DataplaneVariables: DefaultDataplaneVariables(k8s.GetForwardingPlane()),
 	}
 }
 
