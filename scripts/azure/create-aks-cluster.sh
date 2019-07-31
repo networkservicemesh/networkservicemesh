d20519e3487f85036b856d616772294aeda58dea
Artem Belov <47595664+artem-belov@users.noreply.github.com>
2019-07-31 18:08:42 +0700

    Start azure cluster with public ip (#1401)
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- scripts/azure/create-aks-cluster.sh
+++ scripts/azure/create-aks-cluster.sh
@@ -11,6 +11,21 @@ if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
     exit 1
 fi
 
+function register_feature {
+    echo "Registering feature $1 in namespace $2..."
+    REQUEST="az feature register --namespace $2 -n $1 --query properties.state -o tsv"
+    while [ "$($REQUEST)" == "Registering" ]; do
+        sleep 5
+    done
+    echo "Registered"
+}
+
+# Prepare azure for Public IP enabling
+az extension add --name aks-preview
+register_feature VMSSPreview Microsoft.ContainerService
+register_feature NodePublicIPPreview Microsoft.ContainerService
+az provider register -n Microsoft.ContainerService
+
 echo -n "Creating AKS cluster '$AZURE_CLUSTER_NAME'..."
 
 if (az aks show --resource-group "$AZURE_RESOURCE_GROUP" --name "$AZURE_CLUSTER_NAME" > /dev/null 2>&1); then
@@ -24,18 +39,39 @@ else
             --node-count 2 \
             --node-vm-size Standard_B2s \
             --generate-ssh-keys \
+            --debug \
             --no-wait && \
         echo "done" || exit 1
     else
-	     az aks create \
-            --resource-group "$AZURE_RESOURCE_GROUP" \
-            --name "$AZURE_CLUSTER_NAME" \
-            --node-count 2 \
-            --node-vm-size Standard_B2s \
-            --generate-ssh-keys \
-            --no-wait \
-            --service-principal "$AZURE_SERVICE_PRINCIPAL" \
-            --client-secret "$AZURE_SERVICE_PRINCIPAL_SECRET" && \
+        # Temporary while az aks create does not support api-version=2019-06-01 required for enableNodePublicIP option
+	    az rest -m put -u "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/$AZURE_RESOURCE_GROUP/providers/Microsoft.ContainerService/managedClusters/$AZURE_CLUSTER_NAME?api-version=2019-06-01" \
+            --headers Content-Type="application/json" Accept="application/json" accept-language="en-US" -b "
+            {
+                \"location\": \"centralus\",
+                \"properties\":
+                {
+                    \"kubernetesVersion\": \"\",
+                    \"dnsPrefix\": \"${AZURE_CLUSTER_NAME::10}-${AZURE_RESOURCE_GROUP}\",
+                    \"agentPoolProfiles\":
+                    [{
+                        \"name\": \"nodepool1\",
+                        \"count\": 2,
+                        \"vmSize\": \"Standard_B2s\",
+                        \"osType\": \"Linux\",
+                        \"enableNodePublicIP\": true,
+                        \"type\": \"VirtualMachineScaleSets\"
+                    }],
+                    \"servicePrincipalProfile\":
+                    {
+                        \"adminUsername\": \"azureuser\",
+                        \"osType\": \"Linux\",
+                        \"clientId\": \"$AZURE_SERVICE_PRINCIPAL\",
+                        \"secret\": \"$AZURE_SERVICE_PRINCIPAL_SECRET\"
+                    },
+                    \"addonProfiles\": {},
+                    \"enableRBAC\": true
+                }
+            }" && \
         echo "done" || exit 1
     fi
 fi
@@ -47,10 +83,26 @@ az aks wait  \
 	--created > /dev/null && \
 echo "done" || exit 1
 
+echo "Creating Inbound traffic rule"
+NODE_RESOURCE_GROUP=$(az aks show -g "$AZURE_RESOURCE_GROUP" -n "$AZURE_CLUSTER_NAME" --query nodeResourceGroup -o tsv)
+NSG_NAME=$(az network nsg list -g "$NODE_RESOURCE_GROUP" --query "[].name" -o tsv)
+az network nsg rule create --name "${NSG_NAME}-rule" \
+    --nsg-name "$NSG_NAME" \
+    --priority 100 \
+    --resource-group "$NODE_RESOURCE_GROUP" \
+    --access Allow \
+    --description "Allow All Inbound Internet traffic" \
+    --destination-address-prefixes '*' \
+    --destination-port-ranges '*' \
+    --direction Inbound \
+    --protocol '*' \
+    --source-address-prefixes Internet \
+    --source-port-ranges '*' && \
+echo "done" || exit 1
+
 mkdir -p "$(dirname "$AZURE_CREDENTIALS_PATH")"
 az aks get-credentials \
    	--name "$AZURE_CLUSTER_NAME" \
    	--resource-group "$AZURE_RESOURCE_GROUP" \
    	--file "$AZURE_CREDENTIALS_PATH" \
    	--overwrite-existing || exit 1
-
