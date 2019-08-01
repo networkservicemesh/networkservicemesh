b5a110c064a0503cec99e395b49820dfe404e5a0
Nikolay Nikolaev <nnikolay@vmware.com>
2019-08-01 19:10:56 +0300

    Re-factor the helm.mk
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- .mk/helm.mk
+++ .mk/helm.mk
@@ -13,41 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include .mk/gke.mk
-include .mk/aws.mk
-include .mk/k8s.mk
+CHARTS=$(shell ls deployments/helm)
+INSTALL_CHARTS=$(addprefix helm-install-,$(CHARTS))
+DELETE_CHARTS=$(addprefix helm-delete-,$(CHARTS))
 
-.PHONY: helm-init-nsm
-helm-init-nsm:
-	@helm install deployments/helm/nsm
+.PHONY: $(INSTALL_CHARTS)
+$(INSTALL_CHARTS): export CHART=$(subst helm-install-,,$@)
+$(INSTALL_CHARTS):
+	helm install --name=${CHART} deployments/helm/${CHART}
 
-.PHONY: helm-icmp
-helm-icmp: helm-init-nsm
-	@helm install deployments/helm/icmp-responder
-
-.PHONY: helm-vpp-icmp
-helm-vpp-icmp: helm-init-nsm
-	@helm install deployments/helm/vpp-icmp-responder
-
-.PHONY: helm-vpn
-helm-vpn: helm-init-nsm
-	@helm install deployments/helm/vpn
-
-.PHONY: helm-nsmd-monitoring
-helm-nsmd-monitoring: helm-init-nsm
-	@helm install deployments/helm/nsmd-monitoring
-
-.PHONY: helm-gke
-helm-gke:
-	@source ./.env/gke.env
-	gke-start helm-init-nsm gcloud-check
-	# not sure if it's more effective to test separately
-	helm-icmp helm-vpp-icmp helm-vpn helm-nsmd-monitoring
-	gke-delete
-
-.PHONY: helm-aws
-helm-aws: make aws-init
-	@source ./.env/aws.env
-	aws-start k8s-config helm-init-nsm
-	helm-icmp helm-vpp-icmp helm-vpn helm-nsmd-monitoring
-	aws-delete
+.PHONY: $(DELETE_CHARTS)
+$(DELETE_CHARTS): export CHART=$(subst helm-delete-,,$@)
+$(DELETE_CHARTS):
+	helm delete ${CHART}
