bf836e4105bf5145236b2d2ca4448cadd0b96669
Victoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>
2019-08-02 12:51:46 +0700

    Enable linter for tests (#1427)
    Signed-off-by: Viktoria Dlugopolskaya <viktoria.dlugopolskaya@xored.com>

--- scripts/lint-download.sh
+++ scripts/lint-download.sh
@@ -4,8 +4,8 @@
     wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- v1.17.1
     sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
-    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- v1.17.1
+    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- v1.17.1
     sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
-    make lint-download
+    make lint-install
 }
