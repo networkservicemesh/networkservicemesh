8547873ee447d11fb5b68c5818503864992364eb
Nikolay Nikolaev <nnikolay@vmware.com>
2019-08-02 08:54:53 +0300

    Move helm init to cloud specific start scripts
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -7,9 +7,3 @@ HELM_RELEASE=v2.14.3
 wget -c https://storage.googleapis.com/kubernetes-helm/helm-${HELM_RELEASE}-linux-amd64.tar.gz
 tar zxf helm-${HELM_RELEASE}-linux-amd64.tar.gz --strip 1 linux-amd64/helm
 mv helm "${HOME}"/bin/helm
-
-helm init
-
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
