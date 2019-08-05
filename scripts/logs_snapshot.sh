3bc62db23d67b2b47e6d44e9deb5503e46bddf93
denis-tingajkin <denis.tingajkin@xored.com>
2019-08-05 18:17:17 +0700

    fix vet issues
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- scripts/logs_snapshot.sh
+++ scripts/logs_snapshot.sh
@@ -7,15 +7,15 @@ if [[ ${STORE_POD_LOGS_IN_FILES} == true ]]; then
   if [[ ${STORE_POD_LOGS_DIR} ]]; then
     path=${STORE_POD_LOGS_DIR}
   fi
-  mkdir -p ${path}/pod
+  mkdir -p "${path}"/pod
   echo "Created folder ${path}/pod"
 fi
 
 for pod in $(${kubectl} -o=name get pods); do
-  echo ${pod}
+  echo "${pod}"
   if [[ ${STORE_POD_LOGS_IN_FILES} == true ]]; then
     filePath=${path}/${pod}.log
-    ${kubectl} logs --all-containers=true "${pod}" >> ${filePath}
+    ${kubectl} logs --all-containers=true "${pod}" >> "${filePath}"
     echo "Saved logs for ${pod} in ${filePath}"
   else
     echo "Start logs of ${pod}"
