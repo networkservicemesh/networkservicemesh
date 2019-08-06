bfa499d1957ec1fd93fada4d72c2ad1ad5f6db3a
Nikolay Nikolaev <nnikolay@vmware.com>
2019-08-06 10:57:15 +0300

    Update client-go and bump to 1.14.5
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- k8s/cmd/admission-webhook/init.go
+++ k8s/cmd/admission-webhook/init.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,7 +20,7 @@ var (
 func init() {
 	runtimeScheme := runtime.NewScheme()
 	_ = corev1.AddToScheme(runtimeScheme)
-	_ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1.AddToScheme(runtimeScheme)
 	// TODO: remove workaround if issue will be fixed
 	// defaulting with webhooks:
 	// https://github.com/kubernetes/kubernetes/issues/57982
