b172a9a0ebb746fba2d02a7e2facd3d204be6b7e
Nikolay Nikolaev <nnikolay@vmware.com>
2019-08-09 14:35:25 +0300

    Docker build all images
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- .mk/docker.mk
+++ .mk/docker.mk
@@ -16,6 +16,7 @@ BUILD_CONTAINERS=nsmd nsmdp nsmd-k8s vppagent-dataplane vppagent-dataplane-dev
 BUILD_CONTAINERS+=devenv crossconnect-monitor
 BUILD_CONTAINERS+=nsc test-nse monitoring-nsc
 BUILD_CONTAINERS+=vppagent-firewall-nse
+BUILD_CONTAINERS+=vppagent-icmp-responder-nse vppagent-nsc admission-webhook
 RUN_CONTAINERS=$(BUILD_CONTAINERS)
 KILL_CONTAINERS=$(BUILD_CONTAINERS)
 LOG_CONTAINERS=$(KILL_CONTAINERS)
