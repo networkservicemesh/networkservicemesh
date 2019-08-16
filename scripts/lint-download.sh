7385d5ea5e498399f24fe5fc6043e7aac3c3d7d8
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-08-16 16:11:58 +0700

    Downgrade version of linter to 1.16.0 to reduce resource class of linter job (#1483)
    * Use golangci-lint v1.16.0. Use default resource class for linter job
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * use medium+ resource class
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- scripts/lint-download.sh
+++ scripts/lint-download.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+version=1.16.0
 {
-    wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- v1.17.1
+    wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- v${version}
     sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
-    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- v1.17.1
+    wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- v${version}
     sudo cp ./bin/golangci-lint "$(go env GOPATH)"/bin/
 } || {
     make lint-install
