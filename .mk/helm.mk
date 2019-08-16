a66eda2087817e3527b644f258e45e2801f12aab
lobkovilya <ilya.lobkov@xored.com>
2019-08-16 17:58:41 +0700

    Wait for tiller's deployment patch correctly applied
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- .mk/helm.mk
+++ .mk/helm.mk
@@ -19,10 +19,7 @@ DELETE_CHARTS=$(addprefix helm-delete-,$(CHARTS))
 
 .PHONY: helm-init
 helm-init:
-	helm init --wait
-	kubectl create serviceaccount --namespace kube-system tiller
-	kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-	kubectl patch deploy --namespace kube-system tiller-deploy -p "{\"spec\":{\"template\":{\"spec\":{\"serviceAccount\":\"tiller\"}}}}"
+	helm init --wait && ./scripts/helm-patch-tiller.sh
 
 .PHONY: $(INSTALL_CHARTS)
 $(INSTALL_CHARTS): export CHART=$(subst helm-install-,,$@)
---
+++ scripts/helm-patch-tiller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p "{\"spec\":{\"template\":{\"spec\":{\"serviceAccount\":\"tiller\"}}}}"
+tillerPod=$(kubectl get pod -l app=helm -n kube-system | grep -v Terminating | awk 'NR>1 {print $1}')
+kubectl wait -n kube-system --timeout=150s --for condition=Ready pods/${tillerPod}
