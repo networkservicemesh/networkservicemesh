78a19cc8d04af61429b0a9d43e8a38a9eda69b90
lobkovilya <ilya.lobkov@xored.com>
2019-08-21 02:48:22 +0700

    Updating CI for spire helm chart
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- .mk/spire.mk
+++ .mk/spire.mk
@@ -12,44 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SPIRE_DIR = k8s/conf/spire
-SPIRE_NAMESPACE_CONF = spire-namespace
-SPIRE_SERVER = server-account server-configmap server-secrets server-service spire-roles server-statefulset
-SPIRE_AGENT = agent-account agent-configmap agent-daemonset
-SPIRE_AGENT_AZURE = agent-account agent-configmap-azure agent-daemonset
+SPIRE_NAMESPACE=spire
 
-.PHONY: spire-start
-spire-start: spire-server-start spire-agent-start
+.PHONY: spire-install
+spire-install:
+	helm install --name=spire \
+	--wait --timeout 300 \
+	--namespace="${SPIRE_NAMESPACE}" \
+	deployments/helm/nsm/charts/spire
+
+# temporary workaround for azure
+.PHONY: spire-install-azure
+spire-install-azure:
+	helm install --name=spire \
+	--wait --timeout 300 \
+	--set azure.enabled=true \
+	--namespace="${SPIRE_NAMESPACE}" \
+	deployments/helm/nsm/charts/spire
 
 .PHONY: spire-delete
-spire-delete: spire-agent-delete spire-server-delete
-
-.PHONY: spire-namespace-create
-spire-namespace-create: $(addsuffix -apply,$(addprefix spire-,$(SPIRE_NAMESPACE_CONF)))
-
-.PHONY: spire-namespace-delete
-spire-namespace-delete: $(addsuffix -delete,$(addprefix spire-,$(SPIRE_NAMESPACE_CONF)))
-
-.PHONY: spire-server-start
-spire-server-start: spire-namespace-create $(addsuffix -apply,$(addprefix spire-,$(SPIRE_SERVER)))
-
-.PHONY: spire-agent-start
-spire-agent-start: spire-namespace-create $(addsuffix -apply,$(addprefix spire-,$(SPIRE_AGENT)))
-
-.PHONY: spire-server-delete
-spire-server-delete: $(addsuffix -delete,$(addprefix spire-,$(SPIRE_SERVER))) spire-namespace-delete
-
-.PHONY: spire-agent-delete
-spire-agent-delete: $(addsuffix -delete,$(addprefix spire-,$(SPIRE_AGENT))) spire-namespace-delete
-
-.PHONY: spire-%-apply
-spire-%-apply:
-	@sed "s;\(image:[ \t]*\)\(networkservicemesh\)\(/[^:]*\).*;\1${CONTAINER_REPO}\3$${COMMIT/$${COMMIT}/:$${COMMIT}};" ${SPIRE_DIR}/$*.yaml | $(kubectl) apply -n spire -f -
-
-.PHONY: spire-%-delete
-spire-%-delete:
-	@echo "Deleting ${SPIRE_DIR}/$*.yaml"
-	@$(kubectl) delete -n spire -f ${SPIRE_DIR}/$*.yaml > /dev/null 2>&1 || echo "$* does not exist and thus cannot be deleted"
-
-.PHONY: spire-agent-start-azure
-spire-agent-start-azure: spire-namespace-create $(addsuffix -apply,$(addprefix spire-,$(SPIRE_AGENT_AZURE)))
+spire-delete:
+	helm delete --purge spire
