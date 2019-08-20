e4e3365cb8ee640880c1809bdb783709f7290597
lobkovilya <ilya.lobkov@xored.com>
2019-08-20 16:38:36 +0700

    Create helm chart for spire
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- security/scripts/registration.sh
+++ security/scripts/registration.sh
@@ -8,7 +8,7 @@ register() {
         -selector k8s_sat:agent_sa:spire-agent
 
     /opt/spire/bin/spire-server entry create \
-        -data /opt/spire/registration.json \
+        -data /run/spire/entries/registration.json \
         -registrationUDSPath /tmp/spire-registration.sock
 }
 
@@ -20,9 +20,6 @@ cleanup() {
 /opt/spire/bin/spire-server run -config /run/spire/config/server.conf -registrationUDSPath /tmp/spire-registration.sock &
 
 cleanup
-
-sleep 2
-/opt/spire/bin/spire-server entry show
 
 while ! register; do
     echo "One more try"
