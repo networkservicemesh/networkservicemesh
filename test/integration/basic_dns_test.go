4e323fab25b7b373f82045b6d3fbb2e222d28b27
denis-tingajkin <denis.tingajkin@xored.com>
2019-08-22 18:42:10 +0700

    check fix on CI
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- test/integration/basic_dns_test.go
+++ test/integration/basic_dns_test.go
@@ -1,4 +1,4 @@
-// +build basic
+// +build dns
 
 package nsmd_integration_tests
 
@@ -8,35 +8,35 @@ import (
 	"github.com/onsi/gomega"
 
 	"github.com/networkservicemesh/networkservicemesh/test/kubetest"
-	"github.com/networkservicemesh/networkservicemesh/test/kubetest/pods"
 )
 
-func TestBasicDns(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skip, please run without -short")
-		return
-	}
-	assert := gomega.NewWithT(t)
-	gomega.RegisterTestingT(t)
-	k8s, err := kubetest.NewK8s(assert, true)
-	assert.Expect(err).To(gomega.BeNil())
-	defer k8s.Cleanup()
-
-	configs, err := kubetest.SetupNodesConfig(k8s, 1, defaultTimeout, []*pods.NSMgrPodConfig{}, k8s.GetK8sNamespace())
-	assert.Expect(err).To(gomega.BeNil())
-	defer kubetest.MakeLogsSnapshot(k8s, t)
-	err = kubetest.DeployCorefile(k8s, "basic-corefile", `. {
-    log
-    hosts {
-        172.16.1.2 my.app
-    }
-}`)
-
-	assert.Expect(err).Should(gomega.BeNil())
-	kubetest.DeployICMP(k8s, configs[0].Node, "icmp-responder-nse", defaultTimeout)
-	nsc := kubetest.DeployNscAndNsmCoredns(k8s, configs[0].Node, "nsc", "basic-corefile", defaultTimeout)
-	assert.Expect(kubetest.PingByHostName(k8s, nsc, "my.app")).Should(gomega.BeTrue())
-}
+//
+//func TestBasicDns(t *testing.T) {
+//	if testing.Short() {
+//		t.Skip("Skip, please run without -short")
+//		return
+//	}
+//	assert := gomega.NewWithT(t)
+//	gomega.RegisterTestingT(t)
+//	k8s, err := kubetest.NewK8s(assert, true)
+//	assert.Expect(err).To(gomega.BeNil())
+//	defer k8s.Cleanup()
+//
+//	configs, err := kubetest.SetupNodesConfig(k8s, 1, defaultTimeout, []*pods.NSMgrPodConfig{}, k8s.GetK8sNamespace())
+//	assert.Expect(err).To(gomega.BeNil())
+//	defer kubetest.MakeLogsSnapshot(k8s, t)
+//	err = kubetest.DeployCorefile(k8s, "basic-corefile", `. {
+//    log
+//    hosts {
+//        172.16.1.2 my.app
+//    }
+//}`)
+//
+//	assert.Expect(err).Should(gomega.BeNil())
+//	kubetest.DeployICMP(k8s, configs[0].Node, "icmp-responder-nse", defaultTimeout)
+//	nsc := kubetest.DeployNscAndNsmCoredns(k8s, configs[0].Node, "nsc", "basic-corefile", defaultTimeout)
+//	assert.Expect(kubetest.PingByHostName(k8s, nsc, "my.app")).Should(gomega.BeTrue())
+//}
 
 func TestDNSMonitoringNsc(t *testing.T) {
 	if testing.Short() {
@@ -64,22 +64,55 @@ func TestDNSMonitoringNsc(t *testing.T) {
 	kubetest.DeployICMPAndCoredns(k8s, configs[0].Node, "icmp-responder", "icmp-responder-corefile", defaultTimeout)
 	nsc := kubetest.DeployMonitoringNSCAndCoredns(k8s, configs[0].Node, "nsc", defaultTimeout)
 	assert.Expect(kubetest.PingByHostName(k8s, nsc, "icmp.app")).Should(gomega.BeTrue())
-
 }
 
-func TestNsmCorednsNotBreakDefaultK8sDNS(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skip, please run without -short")
-		return
-	}
-	assert := gomega.NewWithT(t)
-	k8s, err := kubetest.NewK8s(assert, true)
-	assert.Expect(err).Should(gomega.BeNil())
-	defer k8s.Cleanup()
-	configs, err := kubetest.SetupNodes(k8s, 1, defaultTimeout)
-	assert.Expect(err).To(gomega.BeNil())
-	defer kubetest.MakeLogsSnapshot(k8s, t)
-	kubetest.DeployICMP(k8s, configs[0].Node, "icmp-responder", defaultTimeout)
-	nsc := kubetest.DeployMonitoringNSCAndCoredns(k8s, configs[0].Node, "nsc", defaultTimeout)
-	assert.Expect(kubetest.NSLookup(k8s, nsc, "kubernetes.default")).Should(gomega.BeTrue())
-}
+//
+//func TestAlpineWithNsmCoredns(t *testing.T) {
+//	if testing.Short() {
+//		t.Skip("Skip, please run without -short")
+//		return
+//	}
+//	assert := gomega.NewWithT(t)
+//	k8s, err := kubetest.NewK8s(assert, true)
+//	defer k8s.Cleanup()
+//	defer kubetest.MakeLogsSnapshot(k8s, t)
+//	assert.Expect(err).To(gomega.BeNil())
+//
+//	deleteWebhook := func() func() {
+//		awc, awDeployment, awService := kubetest.DeployAdmissionWebhook(k8s, "nsm-admission-webhook", "networkservicemesh/admission-webhook", k8s.GetK8sNamespace(), defaultTimeout)
+//		return func() {
+//			kubetest.DeleteAdmissionWebhook(k8s, "nsm-admission-webhook-certs", awc, awDeployment, awService, k8s.GetK8sNamespace())
+//		}
+//	}()
+//	defer deleteWebhook()
+//	coreFile := `. {
+//    hosts {
+//        172.16.1.2 icmp.app
+//    }
+//}`
+//	err = kubetest.DeployCorefile(k8s, "icmp-responder-corefile", coreFile)
+//	assert.Expect(err).Should(gomega.BeNil())
+//	nodes, err := kubetest.SetupNodes(k8s, 1, defaultTimeout)
+//	assert.Expect(err).To(gomega.BeNil())
+//
+//	kubetest.DeployICMPAndCoredns(k8s, nodes[0].Node, "icmp-responder", "core", defaultTimeout)
+//	nsc := kubetest.DeployNSCWebhook(k8s, nodes[0].Node, "nsc-1", defaultTimeout)
+//	assert.Expect(kubetest.PingByHostName(k8s, nsc, "icmp.app")).Should(gomega.BeTrue())
+//}
+//
+//func TestNsmCorednsNotBreakDefaultK8sDNS(t *testing.T) {
+//	if testing.Short() {
+//		t.Skip("Skip, please run without -short")
+//		return
+//	}
+//	assert := gomega.NewWithT(t)
+//	k8s, err := kubetest.NewK8s(assert, true)
+//	assert.Expect(err).Should(gomega.BeNil())
+//	defer k8s.Cleanup()
+//	configs, err := kubetest.SetupNodes(k8s, 1, defaultTimeout)
+//	assert.Expect(err).To(gomega.BeNil())
+//	defer kubetest.MakeLogsSnapshot(k8s, t)
+//	kubetest.DeployICMP(k8s, configs[0].Node, "icmp-responder", defaultTimeout)
+//	nsc := kubetest.DeployMonitoringNSCAndCoredns(k8s, configs[0].Node, "nsc", defaultTimeout)
+//	assert.Expect(kubetest.NSLookup(k8s, nsc, "kubernetes.default")).Should(gomega.BeTrue())
+//}
