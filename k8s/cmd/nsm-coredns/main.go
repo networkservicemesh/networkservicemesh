3d6f82261690471852e19f3c3a6426da2dd6f403
denis-tingajkin <denis.tingajkin@xored.com>
2019-08-22 18:39:36 +0700

    fix problem
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- k8s/cmd/nsm-coredns/main.go
+++ k8s/cmd/nsm-coredns/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
-
+	"github.com/caddyserver/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/coremain"
 	_ "github.com/coredns/coredns/plugin/bind"
 	_ "github.com/coredns/coredns/plugin/hosts"
 	_ "github.com/coredns/coredns/plugin/log"
 	_ "github.com/coredns/coredns/plugin/reload"
+	"os"
+	"time"
 
 	_ "github.com/networkservicemesh/networkservicemesh/k8s/cmd/nsm-coredns/plugin/fanout"
 )
@@ -20,6 +24,35 @@ func init() {
 }
 
 func main() {
+	err := waitForCorefile()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("Version: %v\n", version)
 	coremain.Run()
+}
+
+func waitForCorefile() error {
+	const timeoutDuration = time.Second * 5
+	timeout := time.After(timeoutDuration)
+	path := pathToCorefile()
+	for {
+		_, err := os.Stat(path)
+		if os.IsExist(err) {
+			return nil
+		}
+		select {
+		case <-timeout:
+			return errors.New(fmt.Sprintf("corefile not found. Path: %v", path))
+		case <-time.After(time.Millisecond * 500):
+			break
+		}
+	}
+}
+
+func pathToCorefile() string {
+	cl := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	path := cl.String("cof", caddy.DefaultConfigFile, "")
+	cl.Parse(os.Args[1:])
+	return *path
 }
