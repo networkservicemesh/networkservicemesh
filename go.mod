0608f1d10e4a993ac5cff4ce47326dafa25da690
Nikolay Nikolaev <nnikolay@vmware.com>
2019-08-22 05:11:41 +0300

    Add docs and fix make check for switch_k8s_version.sh
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/networkservicemesh/networkservicemesh
 
+// Use ./scripts/switch_k8s_version.sh to change k8s version
+
 require (
 	github.com/aws/aws-sdk-go v1.22.0
 	github.com/caddyserver/caddy v1.0.1
--- scripts/switch_k8s_version.sh
+++ scripts/switch_k8s_version.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+# Original script by Andy Bursavich:
+# https://github.com/kubernetes/kubernetes/issues/79384#issuecomment-521493597
+
 set -euo pipefail
 
 VERSION=${1#"v"}
 if [ -z "$VERSION" ]; then
-    echo "Must specify version!"
+    printf "Usage (assuming k8s 1.16.0): \n %s 1.16.0\n" "${0}"
     exit 1
 fi
-MODS=($(
-    curl -sS https://raw.githubusercontent.com/kubernetes/kubernetes/v${VERSION}/go.mod |
-    sed -n 's|.*k8s.io/\(.*\) => ./staging/src/k8s.io/.*|k8s.io/\1|p'
-))
+
+MODS=()
+while IFS='' read -r line
+do
+    MODS+=("$line")
+done < <( curl -sS https://raw.githubusercontent.com/kubernetes/kubernetes/v"${VERSION}"/go.mod | sed -n 's|.*k8s.io/\(.*\) => ./staging/src/k8s.io/.*|k8s.io/\1|p')
+
 for MOD in "${MODS[@]}"; do
     V=$(
         go mod download -json "${MOD}@kubernetes-${VERSION}" |
@@ -18,3 +25,4 @@ for MOD in "${MODS[@]}"; do
     go mod edit "-replace=${MOD}=${MOD}@${V}"
 done
 go get "k8s.io/kubernetes@v${VERSION}"
+go mod tidy
