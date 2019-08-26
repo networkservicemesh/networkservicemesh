440ebf9f582b6f2420825270f22226123179c0cb
lobkovilya <ilya.lobkov@xored.com>
2019-08-26 15:23:46 +0200

    vet check fix
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- scripts/helm-patch-tiller.sh
+++ scripts/helm-patch-tiller.sh
@@ -6,5 +6,5 @@ kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 tillerBefore=$(kubectl get pod -l app=helm -n kube-system | awk 'NR>1 {print $1}')
 kubectl patch deploy --namespace kube-system tiller-deploy -p "{\"spec\":{\"template\":{\"spec\":{\"serviceAccount\":\"tiller\"}}}}"
-tillerPod=$(kubectl get pod -l app=helm -n kube-system | grep -v $tillerBefore | awk 'NR>1 {print $1}')
+tillerPod=$(kubectl get pod -l app=helm -n kube-system | grep -v "$tillerBefore" | awk 'NR>1 {print $1}')
 kubectl wait -n kube-system --timeout=150s --for condition=Ready pods/"${tillerPod}"
