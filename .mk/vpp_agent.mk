c545eaab686170128ec7776b1a52ced598ea5dd4
Artem Belov <artem.belov@xored.com>
2019-09-02 09:23:55 +0700

    Update from v2.2.0-beta to v2.2.0
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- .mk/vpp_agent.mk
+++ .mk/vpp_agent.mk
@@ -1,11 +1,11 @@
 ARCH ?= $(shell uname -m)
 ifeq (${ARCH}, x86_64)
-  export VPP_AGENT=ligato/vpp-agent:v2.2.0-beta
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.2.0-beta
+  export VPP_AGENT=ligato/vpp-agent:v2.2.0
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.2.0
 endif
 ifeq (${ARCH}, aarch64)
-  export VPP_AGENT=ligato/vpp-agent-arm64:v2.2.0-beta
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent-arm64:v2.2.0-beta
+  export VPP_AGENT=ligato/vpp-agent-arm64:v2.2.0
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent-arm64:v2.2.0
 endif
 
 .PHONY: docker-vppagent-dataplane-dev-build
