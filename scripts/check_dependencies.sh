821860ae0181cf2bbffcb7770396b1ef4b2aa82b
denis-tingajkin <denis.tingajkin@xored.com>
2019-09-09 12:58:37 +0700

    Add check for go.mod and go.sum after go build to avoid potential unexpected problems
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

---
+++ scripts/check_dependencies.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function master_branch() {
+    if [[ $1 == "" ]] ; then
+        echo "origin/master"
+    fi
+    echo "$1"
+}
+
+function check_git() {
+    if ! [[ $(command -v git) ]] ; then 
+        echo "git not found."
+        exit 1
+    fi 
+}
+
+function check_deps() {
+    check_git
+    local master=$(master_branch $1)
+    echo "master branch: ${master}"
+    for file in $(git diff --name-only ${master}); do
+        echo "${file}"
+        if [[ ${file} == *"go.mod" ]] || [[ ${file} == *"go.sum" ]]; then
+            echo "ERROR: go.mod or go.sum have changes after go build..."
+            git diff ${file}
+            exit 2
+        fi
+    done
+}
+
+check_deps "$1"
