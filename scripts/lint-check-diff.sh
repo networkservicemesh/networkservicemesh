a25ee0cc4540b5ad1e8b2be6a0f87b3a5817b13c
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-09-16 05:23:31 +0700

    Stabilization of linter (#1610)
    * stabilization of linter
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * add GOGC
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * fix bash linter issues
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

---
+++ scripts/lint-check-diff.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function check_diff() {
+    GOGC=50 golangci-lint run --new-from-rev=origin/master
+}
+
+attempts=10
+result=""
+for (( i=0; i < "${attempts}"; i++ ))
+do
+    echo "Attempt $i"
+    result="$(check_diff)"
+    if ! [[ "${result}" == *"failed to load program with go/packages"* ]] ; then
+        break
+    fi
+    if [[ "$((i+1))" == "${attempts}" ]] ; then
+        echo "can not load program with go/packages"
+        exit 1
+    fi
+done
+echo "${result}"
