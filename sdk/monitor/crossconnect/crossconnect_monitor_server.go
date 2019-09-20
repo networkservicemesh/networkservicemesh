5ba757194698bcad82fc75b2df27fbb18c5a829c
Ed Warnicke <hagbard@gmail.com>
2019-09-20 11:16:16 -0500

    Move metrics from controlplane to sdk/monitor/metrics
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- sdk/monitor/crossconnect/crossconnect_monitor_server.go
+++ sdk/monitor/crossconnect/crossconnect_monitor_server.go
@@ -4,8 +4,8 @@ import (
 	"github.com/golang/protobuf/ptypes/empty"
 
 	"github.com/networkservicemesh/networkservicemesh/controlplane/api/crossconnect"
-	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/metrics"
 	"github.com/networkservicemesh/networkservicemesh/sdk/monitor"
+	"github.com/networkservicemesh/networkservicemesh/sdk/monitor/metrics"
 )
 
 // MonitorServer is a monitor.Server for crossconnect GRPC API
