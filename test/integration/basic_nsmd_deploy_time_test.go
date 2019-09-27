12906dd798e35d799eb580236cbbaface693faf9
Andrey Sobolev <haiodo@xored.com>
2019-09-28 01:27:25 +0700

    Add warmup for test
    Signed-off-by: Andrey Sobolev <haiodo@xored.com>

--- test/integration/basic_nsmd_deploy_time_test.go
+++ test/integration/basic_nsmd_deploy_time_test.go
@@ -23,18 +23,27 @@ func TestNSMDDeploy(t *testing.T) {
 	logrus.Print("Running NSMgr Deploy test")
 
 	k8s, err := kubetest.NewK8s(g, true)
-	defer k8s.Cleanup()
-	defer kubetest.MakeLogsSnapshot(k8s, t)
 	g.Expect(err).To(BeNil())
 
+	// Warmup
+
+	_, err = kubetest.SetupNodes(k8s, 1, defaultTimeout)
+	g.Expect(err).To(BeNil())
+	k8s.Cleanup()
+	
 	st := time.Now()
+	k8s, err = kubetest.NewK8s(g, true)
+	defer k8s.Cleanup()
+	defer kubetest.MakeLogsSnapshot(k8s, t)
 	_, err = kubetest.SetupNodes(k8s, 1, defaultTimeout)
 	g.Expect(err).To(BeNil())
 	deploy := time.Now()
 	k8s.Cleanup()
 	destroy := time.Now()
+	
+	
 	logrus.Infof("Pods Start time: %v", deploy.Sub(st))
-	g.Expect(deploy.Sub(st) < time.Second*15).To(Equal(true))
+	g.Expect(deploy.Sub(st) < time.Second*30).To(Equal(true))
 	logrus.Infof("Pods Cleanup time: %v", destroy.Sub(deploy))
-	g.Expect(destroy.Sub(deploy) < time.Second*25).To(Equal(true))
+	g.Expect(destroy.Sub(deploy) < time.Second*30).To(Equal(true))
 }
