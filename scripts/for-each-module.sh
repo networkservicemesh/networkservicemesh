ec83d11e11c58e3f0e3c5970b00a98ed4ca39433
Ed Warnicke <hagbard@gmail.com>
2019-09-28 22:20:44 -0500

    Fix master breakage on for-each excluding vendor (#1681)
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- scripts/for-each-module.sh
+++ scripts/for-each-module.sh
@@ -15,6 +15,6 @@ exec_command () {
 }
 
 export -f exec_command
-find . -name "go.mod" -o -iname "vendor*" \( -exec bash -c "exec_command \"${cmd}\" " {} \; \)
+find . -name "go.mod" -not -path "./vendor/*" -exec bash -c "exec_command \"${cmd}\" " {} \;
 
 exit $res
