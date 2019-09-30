c8b5bb7fe0cf19cea097c6ffc7a376d5a70dd2fc
Artem Belov <artem.belov@xored.com>
2019-09-30 19:34:47 +0700

    Fix gke load images script (#1689)
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- scripts/gke/load-images.sh
+++ scripts/gke/load-images.sh
@@ -12,12 +12,12 @@ then
     exit 1
 fi
 
-GKE_GROUP=$(gcloud container clusters describe dev --project="$GKE_PROJECT_ID" --zone "$GKE_CLUSTER_ZONE" --format=text | grep "^instanceGroupUrls\[0\]" | sed 's/^instanceGroupUrls\[0\]:.*\///g')
+GKE_GROUP=$(gcloud container clusters describe "$GKE_CLUSTER_NAME" --project="$GKE_PROJECT_ID" --zone "$GKE_CLUSTER_ZONE" --format=text | grep "^instanceGroupUrls\[0\]" | sed 's/^instanceGroupUrls\[0\]:.*\///g')
 GKE_INSTANCES=$(gcloud compute instance-groups list-instances --project="$GKE_PROJECT_ID" --zone "$GKE_CLUSTER_ZONE" "$GKE_GROUP" | tail -n +2 | sed 's/ .*//g')
 echo "gke instances: $GKE_INSTANCES"
 for NODE in $GKE_INSTANCES 
 do
   echo "Loading image $1 to $NODE:"
-  gcloud compute scp --zone="$GKE_CLUSTER_ZONE" "scripts/vagrant/images/$1" "$NODE:$HOME/"
+  gcloud compute scp --zone="$GKE_CLUSTER_ZONE" "scripts/vagrant/images/$1" "$NODE:~/"
   gcloud compute ssh --zone="$GKE_CLUSTER_ZONE" "$NODE" --command="sudo docker load -i ~/$1"
 done
