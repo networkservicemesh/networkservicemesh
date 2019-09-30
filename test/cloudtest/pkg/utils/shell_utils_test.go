e48d02afa176ad470ea735b5bfab21aba250b19f
denis-tingajkin <denis.tingajkin@xored.com>
2019-09-30 08:25:30 +0700

    reduce the time for the test to 1 sec
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- test/cloudtest/pkg/utils/shell_utils_test.go
+++ test/cloudtest/pkg/utils/shell_utils_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/onsi/gomega"
 )
@@ -13,7 +14,8 @@ import (
 func TestProcessOutputShouldNotLostOutput(t *testing.T) {
 	assert := gomega.NewWithT(t)
 	const expected = "output..."
-	for i := 0; i < 1000; i++ {
+	start := time.Now()
+	for time.Now().Sub(start) < time.Second {
 		output, err := RunCommand(context.Background(), fmt.Sprintf("echo \"%v\"", expected), "", func(s string) {}, bufio.NewWriter(&strings.Builder{}), nil, nil, true)
 		assert.Expect(err).Should(gomega.BeNil())
 		assert.Expect(strings.TrimSpace(output)).Should(gomega.Equal(expected))
