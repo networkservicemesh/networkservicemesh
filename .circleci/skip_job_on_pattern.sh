0059fc3b4990e7f33ebae05cd0b986e2c58a9655
Ed Warnicke <hagbard@gmail.com>
2019-10-03 03:07:38 -0500

    Skip CI (prelude to skipping CI on doc only changes) (#1702)
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ .circleci/skip_job_on_pattern.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eo pipefail
+BRANCH="${1}"
+PATTERN=".md$|.circleci/skip_job_on_pattern.sh"
+
+FILES_CHANGED=$(git diff "${BRANCH}"... --name-only)
+NOT_MATCHING_PATTERN_FILES_CHANGED=$(git diff "${BRANCH}"... --name-only| grep -E -v "${PATTERN}" || true)
+if [ -z "${NOT_MATCHING_PATTERN_FILES_CHANGED}" ]; then
+  echo "Halting Job because only files matching \"${PATTERN}\" found:"
+  echo "FILE_CHANGED: ${FILES_CHANGED}"
+  circleci-agent step halt
+else
+  echo "Running Job because files not matching \"${PATTERN}\" found:"
+  echo "NOT_MATCHING_PATTERN_FILES_CHANGED: ${NOT_MATCHING_PATTERN_FILES_CHANGED}"
+fi
