e162a1fda628a376373b1b3405c3eb1f8f44a373
Alexander Yustus <alexander.yustus@xored.com>
2019-10-04 12:21:13 +0700

    Add env variable to set output directory for unit test results
    Signed-off-by: Alexander Yustus <alexander.yustus@xored.com>

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 CUR_DIR=$(dirname "$0")
 TEST_LIST=$("$CUR_DIR"/for-each-module.sh "go list ./..." | grep -v -e "sample" | grep -e "^github")
-# shellcheck disable=SC2086
-gotestsum --junitfile ~/junit/unit-tests.xml -- -short $TEST_LIST
+
+: "${JUNITDIR=~/junit}"
+mkdir -p "$JUNITDIR"
+
+# shellcheck disable=SC2086 # disable warning for $TEST_LIST word expansion
+gotestsum --junitfile "$JUNITDIR/unit-tests.xml" -- -short $TEST_LIST
