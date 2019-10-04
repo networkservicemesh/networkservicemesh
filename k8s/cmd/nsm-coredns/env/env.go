fcc37c3835b757626e761b0132a241ec6f27ce42
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-10-04 16:27:03 +0700

    Fix: With admission-webhook, nsm-coredns assumes kubedns service IP is 10.96.0.10  (#1684)
    * add possible to use custom dns config for client pods
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * make format
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * fix automerge issues
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * code cleanup
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * fix format
    
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- k8s/cmd/nsm-coredns/env/env.go
+++ k8s/cmd/nsm-coredns/env/env.go
@@ -7,6 +7,4 @@ const (
 	UseUpdateAPIEnv = utils.EnvVar("USE_UPDATE_API")
 	//UpdateAPIClientSock means path to client socket for dns context update server
 	UpdateAPIClientSock = utils.EnvVar("UPDATE_API_CLIENT_SOCKET")
-	//DefaultDNSServerIPList using for configuring default config
-	DefaultDNSServerIPList = utils.EnvVar("UPDATE_API_DEFAULT_DNS_SERVER")
 )
