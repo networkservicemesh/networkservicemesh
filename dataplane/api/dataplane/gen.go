131cc0ba486c1b8abeba9d2af716f54d6c0775ac
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2019-10-07 19:07:41 +0700

    Rename all 'doc.go' files to 'gen.go' for better name convention (#1721)
    * add a check for verifying generated code
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>
    
    * rename doc.go to gen.go for better name convention
    
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- dataplane/api/dataplane/gen.go
+++ dataplane/api/dataplane/gen.go
@@ -1,3 +1,22 @@
+// Copyright 2018 Red Hat, Inc.
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package core manages the lifecycle of all plugins (start, graceful
+// shutdown) and defines the core lifecycle SPI. The core lifecycle SPI
+// must be implemented by each plugin.
+
 package dataplane
 
 //go:generate bash -c "protoc -I . dataplane.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/  --proto_path=$( go list -f '{{ .Dir }}' -m github.com/golang/protobuf )"
