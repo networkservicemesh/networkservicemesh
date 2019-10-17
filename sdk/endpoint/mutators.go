aa5815869b2c0b789b915a62da7aa3e45853a4f9
Ivana Atanasova <iyovcheva@vmware.com>
2019-10-17 10:42:47 +0300

    Add pod name to cross connect labels (#1613)
    * Add pod name to cross connect client labels
    
    This adds pod name taken from env var `POD_NAME` as first priority
    and `/etc/hostname` if env var is not set, to cross connect labels
    in order to make cross-connects and metrics more descriptive.
    
    * Add endpoint pod name to cross-connect
    
    This adds pod name taken from /etc/hostname and namespace to cross-connect endpoint labels in order to make cross-connects
    and metrics more  descriptive
    
    * Add namespace to cross-connect data
    
    Identifying src/dest with pod name is not reliable out of a single
    namespace, as there might be name collisions. This is why adding
    namespace is also required
    
    Signed-off-by: Ivana Yovcheva <iyovcheva@vmware.com>

--- sdk/endpoint/mutators.go
+++ sdk/endpoint/mutators.go
@@ -3,8 +3,12 @@ package endpoint
 import (
 	"context"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connectioncontext"
 	"github.com/networkservicemesh/networkservicemesh/controlplane/api/local/connection"
+	"github.com/networkservicemesh/networkservicemesh/pkg/tools"
+	"github.com/networkservicemesh/networkservicemesh/sdk/common"
 )
 
 //CreateRouteMutator - Creates an instance of ConnectionMutator with routes mutating
@@ -17,4 +21,18 @@ func CreateRouteMutator(routes []string) ConnectionMutator {
 		}
 		return nil
 	}
+}
+
+func CreatePodNameMutator() ConnectionMutator {
+	return func(ctc context.Context, c *connection.Connection) error {
+		podName, err := tools.GetCurrentPodNameFromHostname()
+		if err != nil {
+			logrus.Infof("failed to get current pod name from hostname: %v", err)
+		} else {
+			c.Labels[connection.PodNameKey] = podName
+			c.Labels[connection.NamespaceKey] = common.GetNamespace()
+		}
+		return nil
+	}
+
 }
