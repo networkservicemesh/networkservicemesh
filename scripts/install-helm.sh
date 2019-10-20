b2c12e65bdaf8ed6afa34a639dc3c3d59bab7e25
Nikolay Nikolaev <nnikolay@vmware.com>
2019-10-20 22:20:46 +0300

    Upgrade helm in packet.net to 2.15.0
    Signed-off-by: Nikolay Nikolaev <nnikolay@vmware.com>

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-HELM_RELEASE=v2.14.3
+HELM_RELEASE=v2.15.0
 
 wget -c https://storage.googleapis.com/kubernetes-helm/helm-${HELM_RELEASE}-linux-amd64.tar.gz
 tar zxf helm-${HELM_RELEASE}-linux-amd64.tar.gz --strip 1 linux-amd64/helm
