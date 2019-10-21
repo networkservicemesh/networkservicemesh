3c6e6d31c7f3167c346ea9e8045393d47c2c2177
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2019-10-21 02:19:46 -0700

    Remove unused constants (#1787)
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>

--- controlplane/api/connection/mechanisms/memif/constants.go
+++ controlplane/api/connection/mechanisms/memif/constants.go
@@ -11,10 +11,6 @@ const (
 	// InterfaceNameKey - interface name mechanism property key
 	InterfaceNameKey = "name"
 	// InterfaceDescriptionKey - interface description mechanism property key
-	InterfaceDescriptionKey = "description"
-	// LinuxIfMaxLength - Linux has a limit of 15 characters for an interface name
-	LinuxIfMaxLength = 15
-	// SocketFilename - socket filename memif mechanism property key
 	SocketFilename = "socketfile"
 	// Master - NSMgr name
 	Master = "master"
@@ -22,10 +18,4 @@ const (
 	Slave = "slave"
 	// Workspace - NSM workspace location mechanism property key
 	Workspace = "workspace"
-	// WorkspaceNSEName - NSE workspace name mechanism property key
-	WorkspaceNSEName = "workspaceNseName"
-	// MemifSocket - memif socket filename
-	MemifSocket = "memif.sock"
-	// NsmBaseDirEnv - NSM location directory
-	NsmBaseDirEnv = "NSM_BASE_DIR"
 )
