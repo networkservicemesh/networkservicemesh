b9c2de8cb059a01b2536b5d8153c2470218cf6f5
denis-tingajkin <denis.tingajkin@xored.com>
2019-10-21 15:51:35 +0700

    set race flag for a while
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -2,4 +2,4 @@
 CUR_DIR=$(dirname "$0")
 TEST_LIST=$("$CUR_DIR"/for-each-module.sh "go list ./..." | grep -v -e "sample" | grep -e "^github")
 # shellcheck disable=SC2086
-gotestsum --junitfile ~/junit/unit-tests.xml -- -short $TEST_LIST
+gotestsum --junitfile ~/junit/unit-tests.xml -- -short -race     $TEST_LIST
