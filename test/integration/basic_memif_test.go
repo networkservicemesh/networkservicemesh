946e2ee77a7850c880f9730488496d14445b1551
denis-tingajkin <denis.tingajkin@xored.com>
2019-10-30 22:44:08 +0700

    broke test
    Signed-off-by: denis-tingajkin <denis.tingajkin@xored.com>

--- test/integration/basic_memif_test.go
+++ test/integration/basic_memif_test.go
@@ -12,7 +12,6 @@ import (
 
 func TestSimpleMemifConnection(t *testing.T) {
 	g := NewWithT(t)
-
 	if testing.Short() {
 		t.Skip("Skip, please run without -short")
 		return
@@ -30,4 +29,5 @@ func TestSimpleMemifConnection(t *testing.T) {
 	kubetest.DeployVppAgentICMP(k8s, nodes[0].Node, "icmp-responder", defaultTimeout)
 	vppagentNsc := kubetest.DeployVppAgentNSC(k8s, nodes[0].Node, "vppagent-nsc", defaultTimeout)
 	g.Expect(true, kubetest.IsVppAgentNsePinged(k8s, vppagentNsc))
+	t.FailNow() // just for test
 }
