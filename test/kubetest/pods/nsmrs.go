7915b988aa5bea3a9da659c9fd79013482f58833
Artem Belov <artem.belov@xored.com>
2019-11-01 14:04:33 +0700

    Copyright headers
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- test/kubetest/pods/nsmrs.go
+++ test/kubetest/pods/nsmrs.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2019 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package pods
 
 import (
@@ -9,8 +23,6 @@ import (
 func DefaultNSMRS() map[string]string {
 	return map[string]string{}
 }
-
-const ()
 
 // NSMRSPodConfig - configuration required for NSMRS Pod creating (environment variables)
 type NSMRSPodConfig struct {
