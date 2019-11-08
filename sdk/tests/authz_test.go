14966816fff6d6e7361a4c2bb38a0fa13ebd0c82
Ilya Lobkov <ilya.lobkov@xored.com>
2019-11-08 11:21:59 +0700

    Properly rename, update headers
    Signed-off-by: Ilya Lobkov <ilya.lobkov@xored.com>

--- sdk/tests/authz_test.go
+++ sdk/tests/authz_test.go
@@ -1,3 +1,19 @@
+// Copyright (c) 2019 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tests
 
 import (
@@ -32,7 +48,7 @@ func TestAuthzEndpoint_Positive(t *testing.T) {
 		rego.Module("example.com", policy)).PrepareForEval(context.Background())
 	g.Expect(err).To(gomega.BeNil())
 
-	srv := endpoint.NewAuthEndpoint(p)
+	srv := endpoint.NewAuthzEndpoint(p)
 
 	request := &networkservice.NetworkServiceRequest{
 		Connection: &connection.Connection{
@@ -64,7 +80,7 @@ func TestAuthzEndpoint_Negative(t *testing.T) {
 
 	g.Expect(err).To(gomega.BeNil())
 
-	srv := endpoint.NewAuthEndpoint(p)
+	srv := endpoint.NewAuthzEndpoint(p)
 
 	request := &networkservice.NetworkServiceRequest{
 		Connection: &connection.Connection{
