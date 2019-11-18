f3b67fcbc1c8b1245a6dd203c2594230531b1bd0
Andrey Sobolev <haiodo@users.noreply.github.com>
2019-11-19 04:16:50 +0700

    Update vagrant to use 19.04 and 2Gb (#1907)
    Signed-off-by: Andrey Sobolev <haiodo@xored.com>

--- scripts/vagrant/scripts/install_kubernetes.sh
+++ scripts/vagrant/scripts/install_kubernetes.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Install kubernetes
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-apt-get update
 echo KUBERNETES_VERSION: "${KUBERNETES_VERSION}"
 apt-get install -qy kubelet="${KUBERNETES_VERSION}" kubectl="${KUBERNETES_VERSION}" kubeadm="${KUBERNETES_VERSION}"
 
---
+++ scripts/vagrant/scripts/install_sources.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get install -y apt-transport-https
+
+# Install docker images
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Install kubernetes
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+echo "Perform update"
+sudo apt-get update
