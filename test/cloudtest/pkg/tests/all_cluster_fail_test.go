3825ad810365c8e3a53cda6e692d9c402e861035
Andrey Sobolev <haiodo@users.noreply.github.com>
2019-12-04 00:43:24 +0700

    CloudTest should properly cleanup multi cloud tests (#1954)
    + Make logs to be less vertical space consuming.
    
    Signed-off-by: Andrey Sobolev <haiodo@xored.com>

--- test/cloudtest/pkg/tests/all_cluster_fail_test.go
+++ test/cloudtest/pkg/tests/all_cluster_fail_test.go
@@ -151,3 +151,61 @@ func TestClusterInstancesOnFailShellRunner(t *testing.T) {
 	}
 	g.Expect(foundFailTest).Should(BeTrue())
 }
+
+func TestClusterInstancesOnFailShellRunnerInterdomain(t *testing.T) {
+	g := NewWithT(t)
+
+	testConfig := &config.CloudTestConfig{}
+
+	testConfig.Timeout = 300
+
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "cloud-test-temp")
+	defer utils.ClearFolder(tmpDir, false)
+	g.Expect(err).To(BeNil())
+
+	testConfig.ConfigRoot = tmpDir
+	ap := createProvider(testConfig, "a_provider")
+	ap.Scripts["config"] = "echo ./.tests/config.a"
+	bp := createProvider(testConfig, "b_provider")
+	bp.Scripts["config"] = "echo ./.tests/config.b"
+	testConfig.Executions = append(testConfig.Executions, &config.ExecutionConfig{
+		Name:            "pass",
+		Timeout:         15,
+		ClusterCount:    2,
+		ClusterSelector: []string{"a_provider", "b_provider"},
+		Kind:            "shell",
+		Run:             "echo pass",
+		OnFail:          `echo >>>Running on fail script with ${KUBECONFIG} <<<`,
+	})
+	testConfig.Executions = append(testConfig.Executions, &config.ExecutionConfig{
+		Name:            "fail",
+		Timeout:         15,
+		ClusterCount:    2,
+		ClusterSelector: []string{"a_provider", "b_provider"},
+		Kind:            "shell",
+		Run:             "make_all_happy()",
+		OnFail:          `echo >>>Running on fail script with ${KUBECONFIG} <<<`,
+	})
+	testConfig.Reporting.JUnitReportFile = JunitReport
+
+	logKeeper := utils.NewLogKeeper()
+	defer logKeeper.Stop()
+
+	report, err := commands.PerformTesting(testConfig, &testValidationFactory{}, &commands.Arguments{})
+	g.Expect(err.Error()).To(Equal("there is failed tests 1"))
+	foundFailTest := false
+
+	for _, t := range report.Suites[0].TestCases {
+		if t.Name == "_a_provider_b_provider_fail" {
+			g.Expect(t.Failure).NotTo(Equal(BeNil()))
+			g.Expect(strings.Contains(t.Failure.Contents, ">>>Running on fail script with ./.tests/config.a <<<")).To(Equal(true))
+			g.Expect(strings.Contains(t.Failure.Contents, ">>>Running on fail script with ./.tests/config.b <<<")).To(Equal(true))
+			foundFailTest = true
+		} else {
+			g.Expect(t.Failure).Should(BeNil())
+		}
+	}
+	g.Expect(foundFailTest).Should(BeTrue())
+
+	g.Expect(logKeeper.MessageCount("OnFail: running on fail script operations with")).To(Equal(2))
+}
