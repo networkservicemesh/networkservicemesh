aaff06278773958933c8b09c44246a3ba7197277
Ilya Lobkov <ilya.lobkov@xored.com>
2019-12-06 20:34:09 +0700

    use v2.9.1
    Signed-off-by: Ilya Lobkov <ilya.lobkov@xored.com>

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-HELM_RELEASE=v2.14.0
+HELM_RELEASE=v2.9.1
 
 wget -c https://storage.googleapis.com/kubernetes-helm/helm-${HELM_RELEASE}-linux-amd64.tar.gz
 tar zxf helm-${HELM_RELEASE}-linux-amd64.tar.gz --strip 1 linux-amd64/helm
