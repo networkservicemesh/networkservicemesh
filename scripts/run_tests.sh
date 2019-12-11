c6b3a7dfc42534ded759a4c55bafbbda8c939674
Denis Tingajkin <denis.tingajkin@xored.com>
2019-12-11 18:28:56 +0700

    fix sanity check issues
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,6 +2,5 @@
 FLAGS=$1
 CUR_DIR=$(dirname "$0")
 TEST_LIST=$("$CUR_DIR"/for-each-module.sh "go list ./..." | grep -v -e "sample" | grep -e "^github")
-echo $TEST_LIST
 # shellcheck disable=SC2086
 gotestsum --junitfile ~/junit/unit-tests.xml -- $FLAGS $TEST_LIST
