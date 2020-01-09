3d33143da936a74583fc41fdee6e43705e7d5164
Denis Tingajkin <denis.tingajkin@xored.com>
2020-01-09 14:44:19 +0700

    fix issue 2007
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

---
+++ controlplane/pkg/nsmd/remote_peer_descriptor.go
@@ -0,0 +1,91 @@
+// Copyright (c) 2020 Cisco Systems, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package nsmd
+
+import (
+	"context"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/registry"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/model"
+)
+
+//RemotePeerDescriptor represents network service manager remote peer
+type RemotePeerDescriptor interface {
+	AddConnection(connection *model.ClientConnection)
+	RemoveConnection(connection *model.ClientConnection)
+	Cancel()
+	Context() context.Context
+	Reset()
+	IsCanceled() bool
+	HasConnection() bool
+	RemoteNsm() *registry.NetworkServiceManager
+}
+
+type remotePeerDescriptor struct {
+	connections map[string]*model.ClientConnection
+	cancel      context.CancelFunc
+	canceled    bool
+	remoteNsm   *registry.NetworkServiceManager
+	context     context.Context
+}
+
+func (r *remotePeerDescriptor) HasConnection() bool {
+	return len(r.connections) > 0
+}
+
+func (r *remotePeerDescriptor) Context() context.Context {
+	return r.context
+}
+
+func (r *remotePeerDescriptor) AddConnection(connection *model.ClientConnection) {
+	r.connections[connection.ConnectionID] = connection
+}
+
+func (r *remotePeerDescriptor) RemoveConnection(connection *model.ClientConnection) {
+	delete(r.connections, connection.ConnectionID)
+}
+
+func (r *remotePeerDescriptor) Cancel() {
+	if r.cancel != nil {
+		r.cancel()
+	}
+	r.canceled = true
+}
+
+func (r *remotePeerDescriptor) Reset() {
+	r.context, r.cancel = context.WithCancel(context.Background())
+	r.canceled = false
+}
+
+func (r *remotePeerDescriptor) IsCanceled() bool {
+	return r.canceled
+}
+
+func (r *remotePeerDescriptor) RemoteNsm() *registry.NetworkServiceManager {
+	return r.remoteNsm
+}
+
+//NewRemotePeerDescriptor represents network service manager remote peer
+func NewRemotePeerDescriptor(remoteNsm *registry.NetworkServiceManager) RemotePeerDescriptor {
+	result := &remotePeerDescriptor{
+		remoteNsm:   remoteNsm,
+		connections: make(map[string]*model.ClientConnection),
+	}
+	result.Reset()
+	return result
+}
