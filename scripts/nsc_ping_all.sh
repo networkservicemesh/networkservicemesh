04fe2435ce17039bdc33a4cbbbd27f7c23d7ebe4
Alexander Yustus <alexander.yustus@xored.com>
2020-01-13 14:06:43 +0700

    Refactor NSC check script
    Signed-off-by: Alexander Yustus <alexander.yustus@xored.com>

--- scripts/nsc_ping_all.sh
+++ scripts/nsc_ping_all.sh
@@ -3,92 +3,92 @@
 kubectl="kubectl -n ${NSM_NAMESPACE:-default}"
 
 echo "Search and ping NSCs..."
-NSCs=$(${kubectl} get pods -o=name | grep -E "icmp-responder-nsc|vpp-icmp-responder-nsc" | sed 's@.*/@@')
+NSCs=$(${kubectl} get pods -o=name | grep "icmp-responder-nsc" | sed 's@.*/@@')
 if [ -z "$NSCs" ]; then
   echo "Zero NSCs found, nothing to ping!"
-  exit
+  exit 1
 fi
 echo "NSCs found:"
 echo "$NSCs"
 
-#  Ping all the things!
-EXIT_VAL=0
+ip_prefix=172.16.1
+vpp_ip_prefix=10.30.1
+
+ERR_COUNT=0
+
 for nsc in $NSCs; do
+    echo
     echo "===== >>>>> PROCESSING ${nsc}  <<<<< ==========="
-    if [[ ${nsc} == vpp-* ]]; then
-        for ip in $(${kubectl} exec "${nsc}" -- vppctl show int addr | grep L3 | awk '{print $2}'); do
-            if [[ "${ip}" == 172.16.1.* ]];then
-                lastSegment=$(echo "${ip}" | cut -d . -f 4 | cut -d / -f 1)
-                nextOp=$((lastSegment + 1))
-                targetIp="172.16.1.${nextOp}"
-                endpointName="icmp-responder-nse"
-            elif [[ "${ip}" == 10.30.1.* ]];then
-                lastSegment=$(echo "${ip}" | cut -d . -f 4 | cut -d / -f 1)
-                nextOp=$((lastSegment + 1))
-                targetIp="10.30.1.${nextOp}"
-                endpointName="vpp-icmp-responder-nse"
-            fi
+    is_vpp_nsc() { [[ ${nsc} == vpp-* ]]; }
+    nsc_exec="${kubectl} exec ${nsc}"
 
-            if [ -n "${targetIp}" ]; then
-                # Prime the pump, its normal to get a packet loss due to arp
-                ${kubectl} exec "${nsc}" -- vppctl ping "${targetIp}" repeat 1 > /dev/null 2>&1
-                OUTPUT=$(${kubectl} exec "${nsc}" -- vppctl ping "${targetIp}" repeat 3)
-                echo "${OUTPUT}"
-                RESULT=$(echo "${OUTPUT}"| grep "packet loss" | awk '{print $6}')
-                if [ "${RESULT}" = "0%" ]; then
-                    echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} successful"
-                    PingSuccess="true"
-                else
-                    echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} unsuccessful"
-                    EXIT_VAL=1
-                fi
-                unset targetIp
-                unset endpointName
-            fi
-        done
+    if is_vpp_nsc; then
+        list_ip_cmd="vppctl show int addr | grep L3 | awk '{print \$2}' | grep ${vpp_ip_prefix}"
     else
-        for ip in $(${kubectl} exec "${nsc}" -- ip addr| grep inet | awk '{print $2}'); do
-            if [[ "${ip}" == 172.16.1.* ]];then
-                lastSegment=$(echo "${ip}" | cut -d . -f 4 | cut -d / -f 1)
-                nextOp=$((lastSegment + 1))
-                targetIp="172.16.1.${nextOp}"
-                endpointName="icmp-responder-nse"
-            elif [[ "${ip}" == 10.30.1.* ]];then
-                lastSegment=$(echo "${ip}" | cut -d . -f 4 | cut -d / -f 1)
-                nextOp=$((lastSegment + 1))
-                targetIp="10.30.1.${nextOp}"
-                endpointName="vpp-icmp-responder-nse"
-            fi
-
-            if [ -n "${targetIp}" ]; then
-                if ${kubectl} exec "${nsc}" -- ping -c 1 "${targetIp}" ; then
-                    echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} successful"
-                    PingSuccess="true"
-                else
-                    echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} unsuccessful"
-                    EXIT_VAL=1
-                fi
-                unset targetIp
-                unset endpointName
-            fi
-        done
+        list_ip_cmd="ip addr | grep inet | awk '{print \$2}' | grep ${ip_prefix}"
     fi
-    if [ -z ${PingSuccess} ]; then
-        EXIT_VAL=1
-        echo "+++++++==ERROR==ERROR=============================================================================+++++"
-        echo "NSC ${nsc} failed to connect to an icmp-responder NetworkService"
+    # make few tries to fetch NSC's IP adresses
+    for ((i=3; i > 0; --i)); do
+      nsc_ips=$(eval "${nsc_exec} -- ${list_ip_cmd}" | tr -d '\r')
+      [ -n "$nsc_ips" ] && echo "Successfully fetched IP addresses of NSC" && break
+      echo "Failed to list IP addresses of NSC"
+      # give NSC some time to initialize unless it's last try
+      ((i > 1)) && sleep 5
+    done
+
+    for ip in $nsc_ips; do
+        echo "Checking ${ip}:"
+        lastSegment=$(echo "${ip}" | cut -d . -f 4 | cut -d / -f 1)
+        nextOp=$((lastSegment + 1))
+        if [[ "${ip}" == $ip_prefix.* ]];then
+            targetIp="$ip_prefix.${nextOp}"
+            endpointName="icmp-responder-nse"
+        else
+            targetIp="$vpp_ip_prefix.${nextOp}"
+            endpointName="vpp-icmp-responder-nse"
+        fi
+
+        if is_vpp_nsc; then
+            # Prime the pump, its normal to get a packet loss due to arp
+            ${nsc_exec} -- vppctl ping "${targetIp}" repeat 3 > /dev/null 2>&1
+            OUTPUT=$(${nsc_exec} -- vppctl ping "${targetIp}" repeat 3)
+            echo "${OUTPUT}"
+            # for VPP-specific tests, one packet received is enough to consider the ping a success
+            if [[ "$OUTPUT" != *"sent, 0 received,"* ]]; then
+                echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} successful"
+                PingSuccess="true"
+            else
+                echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} unsuccessful"
+            fi
+        else
+            if ${nsc_exec} -- ping -c 1 "${targetIp}" ; then
+                echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} successful"
+                PingSuccess="true"
+            else
+                echo "NSC ${nsc} with IP ${ip} pinging ${endpointName} TargetIP: ${targetIp} unsuccessful"
+            fi
+        fi
+    done
+
+    if [ -z "${PingSuccess}" ]; then
+        ((++ERR_COUNT))
+        echo "===========  ERROR  ========================================================"
+        echo "NSC ${nsc} check failed, trace info:"
         ${kubectl} get pod "${nsc}" -o wide
         echo "POD ${nsc} Network dump -------------------------------"
-        if [[ ${nsc} == vpp-* ]]; then
-            ${kubectl} exec "${nsc}" -- vppctl show int
-            ${kubectl} exec "${nsc}" -- vppctl show int addr
-            ${kubectl} exec "${nsc}" -- vppctl show memif
+        if is_vpp_nsc; then
+            ${nsc_exec} -- vppctl show int
+            ${nsc_exec} -- vppctl show int addr
+            ${nsc_exec} -- vppctl show memif
         else
-            ${kubectl} exec "${nsc}" -- ip addr
-            ${kubectl} exec "${nsc}" ip route
+            ${nsc_exec} -- ip addr
+            ${nsc_exec} -- ip route
         fi
-        echo "+++++++==ERROR==ERROR=============================================================================+++++"
+        echo "===========  END OF ERROR  ================================================="
     fi
     unset PingSuccess
 done
-exit ${EXIT_VAL}
+
+echo
+echo "Done, $ERR_COUNT errors"
+exit ${ERR_COUNT}
