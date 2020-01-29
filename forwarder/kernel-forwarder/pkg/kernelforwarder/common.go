af73a1bf17887b1a94f028875b7194ca6ccf06f3
Artem Belov <artem.belov@xored.com>
2020-01-29 14:01:12 +0700

    Finish wireguard mechanism; Kernel forwarder integration tests
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- forwarder/kernel-forwarder/pkg/kernelforwarder/common.go
+++ forwarder/kernel-forwarder/pkg/kernelforwarder/common.go
@@ -13,21 +13,86 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package local
+package kernelforwarder
 
 import (
-	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connectioncontext"
 	"net"
 
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection/mechanisms/common"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connectioncontext"
+	"github.com/networkservicemesh/networkservicemesh/utils/fs"
 )
 
-const (
-	/* VETH pairs are used only for local connections(same node), so we can use a larger MTU size as there's no multi-node connection */
-	cVETHMTU    = 16000
-)
+// CreateInterface - creates interface to remote connection
+func SetupInterface(ifaceName string, conn *connection.Connection, isDst bool) (string, error) {
+	netNsInode := conn.GetMechanism().GetParameters()[common.NetNsInodeKey]
+	neighbors := conn.GetContext().GetIpContext().GetIpNeighbors()
+	var ifaceIP string
+	var routes []*connectioncontext.Route
+	if isDst {
+		ifaceIP = conn.GetContext().GetIpContext().GetDstIpAddr()
+		routes = conn.GetContext().GetIpContext().GetSrcRoutes()
+	} else {
+		ifaceIP = conn.GetContext().GetIpContext().GetSrcIpAddr()
+		routes = conn.GetContext().GetIpContext().GetDstRoutes()
+	}
+
+	/* Get namespace handler - source */
+	nsHandle, err := fs.GetNsHandleFromInode(netNsInode)
+	if err != nil {
+		logrus.Errorf("local: failed to get source namespace handle - %v", err)
+		return netNsInode, err
+	}
+	/* If successful, don't forget to close the handler upon exit */
+	defer func() {
+		if err = nsHandle.Close(); err != nil {
+			logrus.Error("local: error when closing source handle: ", err)
+		}
+		logrus.Debug("local: closed source handle: ", nsHandle, netNsInode)
+	}()
+	logrus.Debug("local: opened source handle: ", nsHandle, netNsInode)
+
+	/* Setup interface - source namespace */
+	if err = setupLinkInNs(nsHandle, ifaceName, ifaceIP, routes, neighbors, true); err != nil {
+		logrus.Errorf("local: failed to setup interface - source - %q: %v", ifaceName, err)
+		return netNsInode, err
+	}
+
+	return netNsInode, nil
+}
+
+// CreateInterface - deletes interface to remote connection
+func ClearInterfaceSetup(ifaceName string, conn *connection.Connection) (string, error) {
+	netNsInode := conn.GetMechanism().GetParameters()[common.NetNsInodeKey]
+	ifaceIP := conn.GetContext().GetIpContext().GetSrcIpAddr()
+
+	/* Get namespace handler - source */
+	nsHandle, err := fs.GetNsHandleFromInode(netNsInode)
+	if err != nil {
+		return "", errors.Errorf("failed to get source namespace handle - %v", err)
+	}
+	/* If successful, don't forget to close the handler upon exit */
+	defer func() {
+		if err = nsHandle.Close(); err != nil {
+			logrus.Error("local: error when closing source handle: ", err)
+		}
+		logrus.Debug("local: closed source handle: ", nsHandle, netNsInode)
+	}()
+	logrus.Debug("local: opened source handle: ", nsHandle, netNsInode)
+
+	/* Extract interface - source namespace */
+	if err = setupLinkInNs(nsHandle, ifaceName, ifaceIP, nil, nil, false); err != nil {
+		return "", errors.Errorf("failed to extract interface - source - %q: %v", ifaceName, err)
+	}
+
+	return netNsInode, nil
+}
 
 // setupLinkInNs is responsible for configuring an interface inside a given namespace - assigns IP address, routes, etc.
 func setupLinkInNs(containerNs netns.NsHandle, ifaceName, ifaceIP string, routes []*connectioncontext.Route, neighbors []*connectioncontext.IpNeighbor, inject bool) error {
@@ -166,16 +231,4 @@ func addNeighbors(link netlink.Link, neighbors []*connectioncontext.IpNeighbor) 
 		}
 	}
 	return nil
-}
-
-// NewVETH returns a VETH interface instance
-func NewVETH(srcName, dstName string) *netlink.Veth {
-	/* Populate the VETH interface configuration */
-	return &netlink.Veth{
-		LinkAttrs: netlink.LinkAttrs{
-			Name: srcName,
-			MTU:  cVETHMTU,
-		},
-		PeerName: dstName,
-	}
 }
