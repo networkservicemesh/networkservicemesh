72ca3b9a1d4ece78f26704f1f73d78a09b409acd
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2020-01-30 15:24:23 +0700

    fix version of generation tools (#2087)
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- scripts/prepare-generate.sh
+++ scripts/prepare-generate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-GO111MODULE=off go get -u k8s.io/code-generator/cmd/deepcopy-gen
-GO111MODULE=off go get -u github.com/golang/protobuf/protoc-gen-go
-GO111MODULE=off go get -u golang.org/x/tools/cmd/stringer
-
+tmp="$(mktemp -d)"
+cd "${tmp}" || exit 1
+go get k8s.io/code-generator/cmd/deepcopy-gen@v0.17.2
+go get github.com/golang/protobuf/protoc-gen-go@v1.3.3
+go get golang.org/x/tools/cmd/stringer@v0.0.0-20200130002326-2f3ba24bd6e7
+rm -rf "${tmp}"
