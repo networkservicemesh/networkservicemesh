afeccae670e35146675104f80d11d8fb20673305
Ilya Lobkov <ilya.lobkov@xored.com>
2020-02-11 18:29:51 +0700

    fix proto
    Signed-off-by: Ilya Lobkov <ilya.lobkov@xored.com>

--- applications/federation-server/api/gen.go
+++ applications/federation-server/api/gen.go
@@ -1,3 +1,3 @@
 package federation
 
-//go:generate bash -c "protoc -I . federation.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/  --proto_path=$( go list -f '{{ .Dir }}' -m github.com/golang/protobuf )"
+//go:generate bash -c "protoc -I . federation.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src/ --proto_path=$GOPATH/pkg/mod/"
