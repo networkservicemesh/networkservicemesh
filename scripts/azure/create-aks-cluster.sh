c406a1407d60b083ba9eb8eb7adeb13e76771b8e
Andrey Sobolev <haiodo@xored.com>
2020-09-23 23:18:22 +0700

    Bump AKS kubernetes version
    Bump AKS kubernetes version
    
    Signed-off-by: Andrey Sobolev <haiodo@xored.com>

--- scripts/azure/create-aks-cluster.sh
+++ scripts/azure/create-aks-cluster.sh
@@ -44,13 +44,13 @@ else
                 \"location\": \"centralus\",
                 \"properties\":
                 {
-                    \"kubernetesVersion\": \"1.15.11\",
+                    \"kubernetesVersion\": \"1.19.0\",
                     \"dnsPrefix\": \"${AZURE_CLUSTER_NAME::10}-${AZURE_RESOURCE_GROUP}\",
                     \"agentPoolProfiles\":
                     [{
                         \"name\": \"nodepool1\",
                         \"count\": 2,
-                        \"orchestratorVersion\": \"1.15.11\",
+                        \"orchestratorVersion\": \"1.19.0\",
                         \"vmSize\": \"Standard_B2s\",
                         \"osType\": \"Linux\",
                         \"enableNodePublicIP\": true,
