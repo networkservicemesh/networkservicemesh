54ab0cadbdb0603a5e5f69214336bbc36633ae65
Denis Tingaikin <49399980+denis-tingajkin@users.noreply.github.com>
2020-10-01 13:14:55 +0700

    increase connect time for TestInterdomainKernelForwarderWireguard (#2199)
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- sdk/client/client.go
+++ sdk/client/client.go
@@ -40,7 +40,7 @@ import (
 
 const (
 	// ConnectTimeout - a default connection timeout
-	ConnectTimeout = 15 * time.Second
+	ConnectTimeout = time.Minute
 	// ConnectionRetry - A number of retries for establish a network service, default == 10
 	ConnectionRetry = 10
 	// RequestDelay - A delay between attempts, default = 5sec
